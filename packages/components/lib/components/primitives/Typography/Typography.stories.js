f852d3b1f4db7c2c836ebca8f80c75bdb0e99bc0
Pau Teruel <93.pau.teruel@gmail.com>
2020-06-30 14:42:28 +0200

    feat: create lib build

--- packages/components/lib/components/primitives/Typography/Typography.stories.js
+++ packages/components/lib/components/primitives/Typography/Typography.stories.js
@@ -3,7 +3,7 @@
 Object.defineProperty(exports, "__esModule", {
   value: true
 });
-exports.Typographies = exports.default = void 0;
+exports.Typographies = exports.Headlines = exports.default = void 0;
 
 var _react = _interopRequireDefault(require("react"));
 
@@ -20,6 +20,52 @@ var _default = {
   component: _.Typography
 };
 exports.default = _default;
+
+const Headlines = () => /*#__PURE__*/_react.default.createElement("div", null, /*#__PURE__*/_react.default.createElement("div", {
+  className: "flex flex-row items-center my-4"
+}, /*#__PURE__*/_react.default.createElement("p", {
+  className: "mr-4"
+}, "Headline1"), /*#__PURE__*/_react.default.createElement(_.Typography, {
+  tag: "h1",
+  type: _Typography.TypographyTypes.Headline1
+}, "The quick brown fox jumps over the lazy dog.")), /*#__PURE__*/_react.default.createElement("div", {
+  className: "flex flex-row items-center my-4"
+}, /*#__PURE__*/_react.default.createElement("p", {
+  className: "mr-4"
+}, "Headline2"), /*#__PURE__*/_react.default.createElement(_.Typography, {
+  tag: "h1",
+  type: _Typography.TypographyTypes.Headline2
+}, "The quick brown fox jumps over the lazy dog.")), /*#__PURE__*/_react.default.createElement("div", {
+  className: "flex flex-row items-center my-4"
+}, /*#__PURE__*/_react.default.createElement("p", {
+  className: "mr-4"
+}, "Headline3"), /*#__PURE__*/_react.default.createElement(_.Typography, {
+  tag: "h1",
+  type: _Typography.TypographyTypes.Headline3
+}, "The quick brown fox jumps over the lazy dog.")), /*#__PURE__*/_react.default.createElement("div", {
+  className: "flex flex-row items-center my-4"
+}, /*#__PURE__*/_react.default.createElement("p", {
+  className: "mr-4"
+}, "Headline4"), /*#__PURE__*/_react.default.createElement(_.Typography, {
+  tag: "h1",
+  type: _Typography.TypographyTypes.Headline4
+}, "The quick brown fox jumps over the lazy dog.")), /*#__PURE__*/_react.default.createElement("div", {
+  className: "flex flex-row items-center my-4"
+}, /*#__PURE__*/_react.default.createElement("p", {
+  className: "mr-4"
+}, "Headline5"), /*#__PURE__*/_react.default.createElement(_.Typography, {
+  tag: "h1",
+  type: _Typography.TypographyTypes.Headline5
+}, "The quick brown fox jumps over the lazy dog.")), /*#__PURE__*/_react.default.createElement("div", {
+  className: "flex flex-row items-center my-4"
+}, /*#__PURE__*/_react.default.createElement("p", {
+  className: "mr-4"
+}, "Headline6"), /*#__PURE__*/_react.default.createElement(_.Typography, {
+  tag: "h1",
+  type: _Typography.TypographyTypes.Headline6
+}, "The quick brown fox jumps over the lazy dog.")));
+
+exports.Headlines = Headlines;
 var tagOptions;
 
 (function (tagOptions) {

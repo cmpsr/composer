51b7d7558b36324eb1c9c9a96277a632743ca167
Dawin Valenzuela <dawin.valenzuela@gmail.com>
2020-07-06 22:37:39 -0500

    Change path implementation

--- packages/components/src/components/primitives/Icon/icons/content/contentIcons.tsx
+++ packages/components/src/components/primitives/Icon/icons/content/contentIcons.tsx
@@ -642,20 +642,17 @@ export const content_copy = ({ type }: Props) => {
   const path = {
     filled:
       'M16 1H4c-1.1 0-2 .9-2 2v14h2V3h12V1zm3 4H8c-1.1 0-2 .9-2 2v14c0 1.1.9 2 2 2h11c1.1 0 2-.9 2-2V7c0-1.1-.9-2-2-2zm0 16H8V7h11v14z',
-    outlined:
-      'M16 1H4c-1.1 0-2 .9-2 2v14h2V3h12V1zm3 4H8c-1.1 0-2 .9-2 2v14c0 1.1.9 2 2 2h11c1.1 0 2-.9 2-2V7c0-1.1-.9-2-2-2zm0 16H8V7h11v14z',
     rounded:
       'M15 1H4c-1.1 0-2 .9-2 2v13c0 .55.45 1 1 1s1-.45 1-1V4c0-.55.45-1 1-1h10c.55 0 1-.45 1-1s-.45-1-1-1zm4 4H8c-1.1 0-2 .9-2 2v14c0 1.1.9 2 2 2h11c1.1 0 2-.9 2-2V7c0-1.1-.9-2-2-2zm-1 16H9c-.55 0-1-.45-1-1V8c0-.55.45-1 1-1h9c.55 0 1 .45 1 1v12c0 .55-.45 1-1 1z',
   };
 
   return (
     <>
-      {type === 'filled' || type === 'outlined' ? (
-        <path d="M0 0h24v24H0V0z" fill="none" />
-      ) : (
-        <path d="M0 0h24v24H0z" fill="none" />
-      )}
-      <path d={path[type]} />
+      <path
+        d={type === 'rounded' ? 'M0 0h24v24H0z' : 'M0 0h24v24H0V0z'}
+        fill="none"
+      />
+      <path d={type === 'rounded' ? path[type] : path.filled} />
     </>
   );
 };

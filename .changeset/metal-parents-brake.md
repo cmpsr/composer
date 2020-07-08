f4138fbecc643ba89c9844014f18599eb5423628
Dawin Valenzuela <55356515+dawinValenzuela@users.noreply.github.com>
2020-07-08 02:27:26 -0500

    Content copy icon (#62)
    * Adding content_copy icon
    
    * changeset file
    
    * Change path implementation
    
    * move function to alphabetical order
    
    * adding build files.

---
+++ .changeset/metal-parents-brake.md
@@ -0,0 +1,5 @@
+---
+"@cmpsr/components": patch
+---
+
+Add content_copy_icon
--- packages/components/lib/components/primitives/Icon/icons/content/contentIcons.js
+++ packages/components/lib/components/primitives/Icon/icons/content/contentIcons.js
@@ -3,7 +3,7 @@
 Object.defineProperty(exports, "__esModule", {
   value: true
 });
-exports.where_to_vote = exports.weekend = exports.waves = exports.undo = exports.unarchive = exports.text_format = exports.sort = exports.send = exports.select_all = exports.save_alt = exports.save = exports.report_off = exports.report = exports.reply_all = exports.reply = exports.remove_circle_outline = exports.remove_circle = exports.remove = exports.redo = exports.outlined_flag = exports.next_week = exports.move_to_inbox = exports.markunread = exports.mail = exports.low_priority = exports.link_off = exports.link = exports.inbox = exports.how_to_vote = exports.how_to_reg = exports.gesture = exports.forward = exports.font_download = exports.flag = exports.filter_list = exports.file_copy = exports.drafts = exports.delete_sweep = exports.create = exports.clear = exports.block = exports.ballot = exports.backspace = exports.archive = exports.add_circle_outline = exports.add_circle = exports.add_box = exports.add = void 0;
+exports.where_to_vote = exports.weekend = exports.waves = exports.undo = exports.unarchive = exports.text_format = exports.sort = exports.send = exports.select_all = exports.save_alt = exports.save = exports.report_off = exports.report = exports.reply_all = exports.reply = exports.remove_circle_outline = exports.remove_circle = exports.remove = exports.redo = exports.outlined_flag = exports.next_week = exports.move_to_inbox = exports.markunread = exports.mail = exports.low_priority = exports.link_off = exports.link = exports.inbox = exports.how_to_vote = exports.how_to_reg = exports.gesture = exports.forward = exports.font_download = exports.flag = exports.filter_list = exports.file_copy = exports.drafts = exports.delete_sweep = exports.create = exports.content_copy = exports.clear = exports.block = exports.ballot = exports.backspace = exports.archive = exports.add_circle_outline = exports.add_circle = exports.add_box = exports.add = void 0;
 
 var _react = _interopRequireDefault(require("react"));
 
@@ -143,6 +143,23 @@ const clear = ({
 };
 
 exports.clear = clear;
+
+const content_copy = ({
+  type
+}) => {
+  const path = {
+    filled: 'M16 1H4c-1.1 0-2 .9-2 2v14h2V3h12V1zm3 4H8c-1.1 0-2 .9-2 2v14c0 1.1.9 2 2 2h11c1.1 0 2-.9 2-2V7c0-1.1-.9-2-2-2zm0 16H8V7h11v14z',
+    rounded: 'M15 1H4c-1.1 0-2 .9-2 2v13c0 .55.45 1 1 1s1-.45 1-1V4c0-.55.45-1 1-1h10c.55 0 1-.45 1-1s-.45-1-1-1zm4 4H8c-1.1 0-2 .9-2 2v14c0 1.1.9 2 2 2h11c1.1 0 2-.9 2-2V7c0-1.1-.9-2-2-2zm-1 16H9c-.55 0-1-.45-1-1V8c0-.55.45-1 1-1h9c.55 0 1 .45 1 1v12c0 .55-.45 1-1 1z'
+  };
+  return /*#__PURE__*/_react.default.createElement(_react.default.Fragment, null, /*#__PURE__*/_react.default.createElement("path", {
+    d: type === 'rounded' ? 'M0 0h24v24H0z' : 'M0 0h24v24H0V0z',
+    fill: "none"
+  }), /*#__PURE__*/_react.default.createElement("path", {
+    d: type === 'rounded' ? path[type] : path.filled
+  }));
+};
+
+exports.content_copy = content_copy;
 
 const create = ({
   type

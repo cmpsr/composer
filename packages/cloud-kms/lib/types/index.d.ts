28966f4aec99d541ee719f086394c4604c08cf54
Takeichi Kanzaki Cabrera <tkanzakic@hey.com>
2020-07-28 14:19:39 +0200

    cloud-kms: remove lib from ignored files

---
+++ packages/cloud-kms/lib/types/index.d.ts
@@ -0,0 +1,19 @@
+export interface KmsConfig {
+    projectId: string;
+    keyFilename?: string;
+    credentials?: {
+        client_email?: string;
+        private_key?: string;
+    };
+    key: {
+        keyRingId: string;
+        cryptoKeyId: string;
+        cryptoKeyVersionId: string;
+        locationId?: string;
+    };
+}
+export interface IEncryptor {
+    encrypt: (data: string | object, excludedKeys?: string[]) => Promise<string | object>;
+    decrypt: (data: string | object, excludedKeys?: string[]) => Promise<string | object>;
+}
+//# sourceMappingURL=index.d.ts.map

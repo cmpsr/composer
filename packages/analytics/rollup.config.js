e5f932ed4a342a51f4209fbebec32b5f4716e9c0
Ryan Hefner <hi@ryanhefner.com>
2020-07-29 16:45:30 -0400

    Add Rollup config and build script to analytics package

---
+++ packages/analytics/rollup.config.js
@@ -0,0 +1,43 @@
+import babel from '@rollup/plugin-babel';
+import commonjs from '@rollup/plugin-commonjs';
+import json from '@rollup/plugin-json';
+import resolve from '@rollup/plugin-node-resolve';
+import { terser } from 'rollup-plugin-terser';
+import pkg from './package.json';
+
+const config = {
+  input: './lib/index.js',
+  output: {
+    name: 'analytics',
+    file: './dist/index.js',
+    format: 'umd',
+    globals: {
+      'react': 'React',
+      'react-dom': 'ReactDOM',
+    },
+    banner: `/*! ${pkg.name} v${pkg.version} | (c) ${new Date().getFullYear()} Composer | ${pkg.license} License | https://github.com/${pkg.repository} !*/`,
+    footer: '/* tbd */',
+  },
+  external: [
+    'react',
+    'react-dom',
+    /@babel\/runtime/,
+  ],
+  plugins: [
+    babel({
+      babelHelpers: process.env.BABEL_ENV === 'umd' ? 'bundled' : 'runtime',
+      exclude: 'node_modules/**',
+    }),
+    resolve(),
+    commonjs({
+      include: /node_modules/,
+    }),
+    json(),
+  ],
+};
+
+if (process.env.NODE_ENV === 'production') {
+  config.plugins.push(terser());
+}
+
+export default config;

17ff0b8c95cfd153564294cb5d512e62bc607c40
Ryan Hefner <hi@ryanhefner.com>
2020-07-30 07:59:17 -0400

    Add Rollup config and build script to plaid package

---
+++ packages/plaid/tools/build.js
@@ -0,0 +1,47 @@
+const fs = require('fs');
+const execSync = require('child_process').execSync;
+const prettyBytes = require('pretty-bytes');
+const gzipSize = require('gzip-size');
+
+const exec = (command, extraEnv) => {
+  execSync(command, {
+    stdio: 'inherit',
+    env: Object.assign({}, process.env, extraEnv),
+  });
+};
+
+console.log(`Convert / Babel-ify Typescript files ...`);
+
+exec('babel src -d lib --extensions .ts,tsx --ignore src/__mocks__,__tests__,**/*.test.* && tsc -d --declarationDir lib --declarationMap --emitDeclarationOnly');
+
+console.log('\nBuilding CommonJS modules ...');
+
+exec('babel lib -d cjs --ignore src/__mocks__,__tests__,**/*.test.*', {
+  BABEL_ENV: 'cjs',
+});
+
+console.log('\nBuilding ES modules ...');
+
+exec('babel lib -d es --ignore src/__mocks__,__tests__,**/*.test.*', {
+  BABEL_ENV: 'es',
+});
+
+console.log('\nBuilding plaid.js ...');
+
+exec('rollup -c -f umd -o umd/plaid.js', {
+  BABEL_ENV: 'umd',
+  NODE_ENV: 'development',
+});
+
+console.log('\nBuilding plaid.min.js ...');
+
+exec('rollup -c -f umd -o umd/plaid.min.js', {
+  BABEL_ENV: 'umd',
+  NODE_ENV: 'production',
+});
+
+const size = gzipSize.sync(
+  fs.readFileSync('umd/plaid.min.js')
+);
+
+console.log('\ngzipped, the UMD build is %s', prettyBytes(size));

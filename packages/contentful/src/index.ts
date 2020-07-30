fbf4e1fcd39e55a107bb7f72b870fc5f25c7b3cf
Ryan Hefner <hi@ryanhefner.com>
2020-07-29 22:35:28 -0400

    Add src index to contentful package

---
+++ packages/contentful/src/index.ts
@@ -0,0 +1,15 @@
+import * as client from './client';
+import * as config from './config';
+import * as constants from './constants';
+import * as context from './context';
+import * as types from './types';
+import * as utils from './utils';
+
+export {
+  client,
+  config,
+  constants,
+  context,
+  types,
+  utils,
+};

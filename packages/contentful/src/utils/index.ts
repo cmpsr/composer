26239c4ad5076ea63defeb4e2ce605001e200fdf
Ryan Hefner <hi@ryanhefner.com>
2020-07-29 22:35:08 -0400

    Add utils index to contentful package

---
+++ packages/contentful/src/utils/index.ts
@@ -0,0 +1,23 @@
+import * as getContent from './getContent';
+import * as getContentModel from './getContentModel';
+import * as getFastlyABAllocationsFromHeader from './getFastlyABAllocationsFromHeader';
+import * as getRoute from './getRoute';
+import * as getRouteVariant from './getRouteVariant';
+import * as getRouteVariantByQuery from './getRouteVariantByQuery';
+import * as getVariantByQueryParams from './getVariantByQueryParams';
+import * as randomTrafficSelector from './randomTrafficSelector';
+import * as renderFromComponentMap from './renderFromComponentMap';
+import * as renderFromContentfulModel from './renderFromContentfulModel';
+
+export {
+  getContent,
+  getContentModel,
+  getFastlyABAllocationsFromHeader,
+  getRoute,
+  getRouteVariant,
+  getRouteVariantByQuery,
+  getVariantByQueryParams,
+  randomTrafficSelector,
+  renderFromComponentMap,
+  renderFromContentfulModel,
+};

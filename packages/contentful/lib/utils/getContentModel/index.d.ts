4796c050feded8345e49522e5821cb80cb319bc0
Ryan Hefner <hi@ryanhefner.com>
2020-07-29 22:34:32 -0400

    Remove lib directory from repo for contentful package

--- packages/contentful/lib/utils/getContentModel/index.d.ts
+++
@@ -1,4 +0,0 @@
-import { ReactElement } from 'react';
-import { ChildProps } from 'types/ChildProps';
-export declare const getContentModel: ({ id, type }: ChildProps) => ReactElement[];
-//# sourceMappingURL=index.d.ts.map
--- packages/contentful/lib/utils/getVariantByQueryParams/index.d.ts
+++
@@ -1,2 +0,0 @@
-export * from './getVariantByQueryParams';
-//# sourceMappingURL=index.d.ts.map

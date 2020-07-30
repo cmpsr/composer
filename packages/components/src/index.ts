f6207e03937ac87bf8d729e6733db19675680e92
Ryan Hefner <hi@ryanhefner.com>
2020-07-29 22:20:16 -0400

    Add main index file to components package

---
+++ packages/components/src/index.ts
@@ -0,0 +1,11 @@
+import * as components from './components';
+import * as styles from './styles';
+import * as types from './types';
+import * as utils from './utils';
+
+export {
+  components,
+  styles,
+  types,
+  utils,
+};

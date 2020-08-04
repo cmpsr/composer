620bc451f1d71cb1fcd79f4f846097176bb3f803
Ryan Hefner <hi@ryanhefner.com>
2020-08-04 19:26:35 -0400

    Delete index files added to components package

--- packages/components/src/components/index.ts
+++
@@ -1,9 +0,0 @@
-import * as containers from './containers';
-import * as layouts from './layouts';
-import * as primitives from './primitives';
-
-export {
-  containers,
-  layouts,
-  primitives,
-};

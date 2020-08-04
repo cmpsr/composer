08e5fac0976eef4387c91b42c0fc2dd1ae4bbb98
Ryan Hefner <hi@ryanhefner.com>
2020-08-04 17:48:55 -0400

    Update sms index in twilio package

--- packages/twilio/src/sms/index.ts
+++ packages/twilio/src/sms/index.ts
@@ -1,10 +1,10 @@
 import { TwilioSms } from './TwilioSms';
 
-export const useTwilioSms = (
+const useTwilioSms = (
   accountSid = process.env.TWILIO_ACCOUNT_SID,
   authToken = process.env.TWILIO_AUTH_TOKEN
 ): TwilioSms => {
   return new TwilioSms({ accountSid, authToken });
 };
 
-export { TwilioSms };
+export { TwilioSms, useTwilioSms };

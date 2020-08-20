79a0c6f997b7b4904c1239f2d75919cb6ad2eeb5
Dawin Valenzuela <dawin.valenzuela@gmail.com>
2020-08-20 16:50:50 -0500

    adding knobs to badge

--- packages/components/src/components/primitives/Badges/IconicBadge/stories/IconicBadge.stories.tsx
+++ packages/components/src/components/primitives/Badges/IconicBadge/stories/IconicBadge.stories.tsx
@@ -1,38 +1,15 @@
 import React from 'react';
 import { IconicBadge, BadgeColor } from '..';
 import { search as Search } from '../../../Icon/icons/action';
+import { select } from '@storybook/addon-knobs';
 
 export default {
   title: 'Composer/Primitive/Badges/IconicBadge',
   component: IconicBadge,
 };
 
-export const withIcon = () => <IconicBadge icon={<Search type="filled" />} />;
+export const basic = () => <IconicBadge icon={<Search type="filled" />} color={select('color', BadgeColor, BadgeColor.Primary)} />;
 
-withIcon.story = {
-  name: 'With icon',
-};
-
-export const withPrimaryColor = () => (
-  <IconicBadge color={BadgeColor.Primary} icon={<Search type="filled" />} />
-);
-
-withPrimaryColor.story = {
-  name: 'With primary color',
-};
-
-export const withSecondaryColor = () => (
-  <IconicBadge color={BadgeColor.Secondary} icon={<Search type="filled" />} />
-);
-
-withSecondaryColor.story = {
-  name: 'With secondary color',
-};
-
-export const withErrorColor = () => (
-  <IconicBadge color={BadgeColor.Error} icon={<Search type="filled" />} />
-);
-
-withErrorColor.story = {
-  name: 'With error color',
+basic.story = {
+  name: 'Basic',
 };
--- packages/components/src/components/primitives/Badges/NumericBadge/stories/NumericBadge.stories.tsx
+++ packages/components/src/components/primitives/Badges/NumericBadge/stories/NumericBadge.stories.tsx
@@ -1,45 +1,20 @@
 import React from 'react';
 import { NumericBadge, BadgeColor } from '..';
+import { select, number } from '@storybook/addon-knobs';
 
 export default {
   title: 'Composer/Primitive/Badges/NumericBadge',
   component: NumericBadge,
 };
 
-export const withNumber = () => <NumericBadge number={3} />;
-
-withNumber.story = {
-  name: 'With number',
-};
-
-export const withMaximumNumber = () => (
-  <NumericBadge number={12} maximumNumber={9} />
+export const basic = () => (
+  <NumericBadge
+    number={number('number', 3)}
+    maximumNumber={number('maxiNumber', 9)}
+    color={select('color', BadgeColor, BadgeColor.Primary)}
+  />
 );
 
-withMaximumNumber.story = {
-  name: 'With maximum number',
-};
-
-export const withPrimaryColor = () => (
-  <NumericBadge color={BadgeColor.Primary} number={5} />
-);
-
-withPrimaryColor.story = {
-  name: 'With primary color',
-};
-
-export const withSecondaryColor = () => (
-  <NumericBadge color={BadgeColor.Secondary} number={5} />
-);
-
-withSecondaryColor.story = {
-  name: 'With secondary color',
-};
-
-export const withErrorColor = () => (
-  <NumericBadge color={BadgeColor.Error} number={5} />
-);
-
-withErrorColor.story = {
-  name: 'With error color',
+basic.story = {
+  name: 'Basic',
 };

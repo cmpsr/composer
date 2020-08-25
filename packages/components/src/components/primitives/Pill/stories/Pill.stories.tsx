3d760a8a6658487ac48d385168a9d7b87c37aa6a
Dawin Valenzuela <dawin.valenzuela@gmail.com>
2020-08-25 11:53:05 -0500

    adding knobs for Pill component

--- packages/components/src/components/primitives/Pill/stories/Pill.stories.tsx
+++ packages/components/src/components/primitives/Pill/stories/Pill.stories.tsx
@@ -1,12 +1,13 @@
 import React from 'react';
 import { Pill } from '..';
+import { text } from '@storybook/addon-knobs';
 
 export default {
   title: 'Composer/Primitive/Pill',
   component: Pill,
 };
 
-export const basic = () => <Pill label="TODAY" />;
+export const basic = () => <Pill label={text('Label', "TODAY")} />;
 
 basic.story = {
   name: 'Basic',

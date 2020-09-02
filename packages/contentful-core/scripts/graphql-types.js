2937233cb28fe94bbb14a4eec0ed449bb7819462
Ryan Hefner <hi@ryanhefner.com>
2020-09-01 21:41:02 -0400

    Add environment support to graphql-types script

--- packages/contentful-core/scripts/graphql-types.js
+++ packages/contentful-core/scripts/graphql-types.js
@@ -4,7 +4,7 @@ require('dotenv').config();
 
 const { exec } = require('child_process');
 
-exec(`apollo client:codegen ${process.env.TYPES_DIR || 'src/types'} --outputFlat --includes "**/*.graphql" --endpoint https://graphql.contentful.com/content/v1/spaces/${process.env.CONTENTFUL_SPACE_ID} --target typescript --globalTypesFile ${process.env.GLOBAL_TYPES_FILE || './src/types/global.ts'} --no-addTypename --header "Authorization: Bearer ${process.env.CONTENTFUL_ACCESS_TOKEN}"`, (error, stdout, stderr) => {
+exec(`apollo client:codegen ${process.env.TYPES_DIR || 'src/types'} --outputFlat --includes "**/*.graphql" --endpoint https://graphql.contentful.com/content/v1/spaces/${process.env.CONTENTFUL_SPACE_ID}/environments/${process.env.CONTENTFUL_ENVIRONMENT || 'master'} --target typescript --globalTypesFile ${process.env.GLOBAL_TYPES_FILE || './src/types/global.ts'} --no-addTypename --header "Authorization: Bearer ${process.env.CONTENTFUL_ACCESS_TOKEN}"`, (error, stdout, stderr) => {
   if (error) {
     console.log(`error: ${error}`);
     return;

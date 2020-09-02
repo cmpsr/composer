6f8ed46fa0d1630f33fc4140d6aed0a77607f7b6
Ryan Hefner <hi@ryanhefner.com>
2020-09-01 21:38:17 -0400

    Add environment support to graphql-schema script

--- packages/contentful-core/scripts/graphql-schema.js
+++ packages/contentful-core/scripts/graphql-schema.js
@@ -5,15 +5,19 @@ require('dotenv').config();
 const fetch = require('node-fetch');
 const fs = require('fs');
 
-fetch(`https://graphql.contentful.com/content/v1/spaces/${process.env.CONTENTFUL_SPACE_ID}`, {
-  method: 'POST',
-  headers: {
-    'Content-Type': 'application/json',
-    Authorization: `Bearer ${process.env.CONTENTFUL_ACCESS_TOKEN}`,
-  },
-  body: JSON.stringify({
-    variables: {},
-    query: `
+fetch(
+  `https://graphql.contentful.com/content/v1/spaces/${
+    process.env.CONTENTFUL_SPACE_ID
+  }/environments/${process.env.CONTENTFUL_ENVIRONMENT || 'master'}`,
+  {
+    method: 'POST',
+    headers: {
+      'Content-Type': 'application/json',
+      Authorization: `Bearer ${process.env.CONTENTFUL_ACCESS_TOKEN}`,
+    },
+    body: JSON.stringify({
+      variables: {},
+      query: `
       {
         __schema {
           types {
@@ -26,22 +30,26 @@ fetch(`https://graphql.contentful.com/content/v1/spaces/${process.env.CONTENTFUL
         }
       }
     `,
-  }),
-})
-  .then(result => result.json())
-  .then(result => {
+    }),
+  }
+)
+  .then((result) => result.json())
+  .then((result) => {
     // here we're filtering out any type information unrelated to unions or interfaces
     const filteredData = result.data.__schema.types.filter(
-      type => type.possibleTypes !== null
+      (type) => type.possibleTypes !== null
     ); // eslint-disable-line no-underscore-dangle
+
     result.data.__schema.types = filteredData; // eslint-disable-line no-underscore-dangle, no-param-reassign
+
     if (!fs.existsSync(`${process.env.SCHEMA_DIR || './src/schema'}`)) {
       fs.mkdirSync(`${process.env.SCHEMA_DIR || './src/schema'}`);
     }
+
     fs.writeFile(
       `${process.env.SCHEMA_DIR || './src/schema'}/fragmentTypes.json`,
       JSON.stringify(result.data),
-      err => {
+      (err) => {
         if (err) {
           console.error('Error writing fragmentTypes file', err); // eslint-disable-line no-console
         } else {

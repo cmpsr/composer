0eb088db7263795107fa59573bc39a826fbaaf7d
Takeichi Kanzaki Cabrera <tkanzakic@hey.com>
2020-10-23 12:19:46 +0200

    add size to storybook playground

--- packages/styled-components/src/components/primitives/Badges/NumericBadge/NumericBadge.stories.tsx
+++ packages/styled-components/src/components/primitives/Badges/NumericBadge/NumericBadge.stories.tsx
@@ -2,6 +2,7 @@ import React from 'react';
 import { NumericBadge } from '.';
 import { number, select, text } from '@storybook/addon-knobs';
 import { ThemeProvider } from 'styled-components';
+import theme from 'styles/theme';
 
 export default {
   title: 'Primitives/Badges/NumericBadge',
@@ -27,9 +28,20 @@ export const ErrorColor = () => (
 );
 
 export const Playground = () => (
-  <NumericBadge
-    color={select('Color', NumericBadge.Color, NumericBadge.Color.Primary)}
-    number={number('Number', 5)}
-    maximumNumber={number('Maximum number allowed', 9)}
-  />
+  <ThemeProvider
+    theme={{
+      ...theme,
+      NumericBadge: {
+        height: text('Height', '1.25rem'),
+        width: text('Width', '1.25rem'),
+        padding: text('Padding', '0.15rem'),
+      },
+    }}
+  >
+    <NumericBadge
+      color={select('Color', NumericBadge.Color, NumericBadge.Color.Primary)}
+      number={number('Number', 5)}
+      maximumNumber={number('Maximum number allowed', 9)}
+    />
+  </ThemeProvider>
 );

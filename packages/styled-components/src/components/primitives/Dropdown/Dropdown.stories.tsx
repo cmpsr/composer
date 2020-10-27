a20f5614962f8256d92195d2ca24beccc363c414
Pau Teruel <93.pau.teruel@gmail.com>
2020-10-27 11:22:45 +0100

    feat: apply feedback

--- packages/styled-components/src/components/primitives/Dropdown/Dropdown.stories.tsx
+++ packages/styled-components/src/components/primitives/Dropdown/Dropdown.stories.tsx
@@ -1,17 +1,86 @@
 import React from 'react';
 import { Dropdown } from '.';
 import { text } from '@storybook/addon-knobs';
+import { LinkItem } from 'components/primitives/List/Items';
+import { favorite as Favorite, Icon } from 'components/primitives/Icon';
+import styled from 'styled-components';
 
 export default {
   title: 'Primitives/Dropdown',
   component: Dropdown,
 };
 
-export const Title = () => (
+export const Titles = () => (
   <>
-    <h1>Title</h1>
+    <h1>Titles</h1>
     <Dropdown title="Title">
-      <span>Children</span>
+      <LinkItem title="title" />
+      <LinkItem title="title" />
+      <LinkItem title="title" />
+      <LinkItem title="title" />
+    </Dropdown>
+  </>
+);
+
+export const Icons = () => (
+  <>
+    <h1>Icons</h1>
+    <Dropdown title="Title">
+      <LinkItem title="title" icon={<Favorite type={Icon.Types.Filled} />} />
+      <LinkItem title="title" icon={<Favorite type={Icon.Types.Filled} />} />
+      <LinkItem title="title" icon={<Favorite type={Icon.Types.Filled} />} />
+      <LinkItem title="title" icon={<Favorite type={Icon.Types.Filled} />} />
+    </Dropdown>
+  </>
+);
+
+const StyledFlex = styled.div`
+  display: flex;
+`;
+export const Columns = () => (
+  <>
+    <h1>Columns</h1>
+    <Dropdown title="Title">
+      <StyledFlex>
+        <div>
+          {' '}
+          <LinkItem
+            title="title"
+            icon={<Favorite type={Icon.Types.Filled} />}
+          />
+          <LinkItem
+            title="title"
+            icon={<Favorite type={Icon.Types.Filled} />}
+          />
+          <LinkItem
+            title="title"
+            icon={<Favorite type={Icon.Types.Filled} />}
+          />
+          <LinkItem
+            title="title"
+            icon={<Favorite type={Icon.Types.Filled} />}
+          />
+        </div>
+        <div>
+          {' '}
+          <LinkItem
+            title="title"
+            icon={<Favorite type={Icon.Types.Filled} />}
+          />
+          <LinkItem
+            title="title"
+            icon={<Favorite type={Icon.Types.Filled} />}
+          />
+          <LinkItem
+            title="title"
+            icon={<Favorite type={Icon.Types.Filled} />}
+          />
+          <LinkItem
+            title="title"
+            icon={<Favorite type={Icon.Types.Filled} />}
+          />
+        </div>
+      </StyledFlex>
     </Dropdown>
   </>
 );

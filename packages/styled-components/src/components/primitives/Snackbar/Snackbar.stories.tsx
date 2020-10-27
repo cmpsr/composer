211e1b5c1e8f35bab157a472f21fa31513d773c1
Pau Teruel <93.pau.teruel@gmail.com>
2020-10-27 10:33:47 +0100

    test: add test for snackbar

--- packages/styled-components/src/components/primitives/Snackbar/Snackbar.stories.tsx
+++ packages/styled-components/src/components/primitives/Snackbar/Snackbar.stories.tsx
@@ -1,5 +1,5 @@
 import React, { useEffect } from 'react';
-import { Snackbar, showSnackbar, hideSnackbar } from '.';
+import { Snackbar, showSnackbar } from '.';
 import { Icon, favorite as Favorite } from 'components/primitives/Icon';
 import { select, text, number } from '@storybook/addon-knobs';
 import { action } from '@storybook/addon-actions';
@@ -9,10 +9,96 @@ export default {
   component: Snackbar,
 };
 
-export const withBase = () => {
-  const props = {
-    message: text('Message', 'Prompt Option'),
-    description: text('Description', 'Detail Info'),
+export const Message = () => {
+  const config = {
+    message: 'Message',
+  };
+
+  useEffect(() => {
+    showSnackbar(config);
+  }, []);
+
+  return <Snackbar />;
+};
+
+export const Description = () => {
+  const config = {
+    message: 'Message',
+    description: 'Description',
+  };
+
+  useEffect(() => {
+    showSnackbar(config);
+  }, []);
+
+  return <Snackbar />;
+};
+
+export const TextPositions = () => {
+  useEffect(() => {
+    Object.keys(Snackbar.TextPositions).map((textPosition) => {
+      showSnackbar({
+        message: textPosition,
+        textPosition: Snackbar.TextPositions[textPosition],
+        action: {
+          childAction: (
+            <Icon>
+              <Favorite type={Icon.Types.Filled} />
+            </Icon>
+          ),
+        },
+      });
+    });
+  }, []);
+
+  return (
+    <>
+      <h1>Text positions</h1>
+      <Snackbar />
+    </>
+  );
+};
+
+export const Positions = () => {
+  useEffect(() => {
+    Object.keys(Snackbar.Positions).map((position) => {
+      showSnackbar({
+        message: position,
+        position: Snackbar.Positions[position],
+      });
+    });
+  }, []);
+
+  return (
+    <>
+      <h1>Positions</h1>
+      <Snackbar />
+    </>
+  );
+};
+
+export const Types = () => {
+  useEffect(() => {
+    Object.keys(Snackbar.Types).map((type) => {
+      showSnackbar({
+        message: type,
+        type: Snackbar.Types[type],
+      });
+    });
+  }, []);
+
+  return (
+    <>
+      <h1>Types</h1>
+      <Snackbar />
+    </>
+  );
+};
+
+export const Playground = () => {
+  const config = {
+    message: text('Message', 'Message'),
+    description: text('Description', 'Description'),
     textPosition: select(
       'Text Position',
       Snackbar.TextPositions,
@@ -37,83 +123,8 @@ export const withBase = () => {
   };
 
   useEffect(() => {
-    showSnackbar(props);
-  }, [props]);
+    showSnackbar(config);
+  }, [config]);
 
-  return (
-    <div>
-      <Snackbar />
-      <button onClick={hideSnackbar}>Hide snackbar</button>
-    </div>
-  );
+  return <Snackbar />;
 };
-
-withBase.story = {
-  name: 'Base',
-};
-
-// export const withIcon = () => {
-//   const props = {
-//     className: 'text-fill-primary-900',
-//     message: 'Prompt Option',
-//     description: 'Detail Info',
-//     textPosition: SnackbarTextPosition.Right,
-//     type: SnackbarType.Default,
-//     position: SnackbarPosition.TopCenter,
-//     autoClose: 5000,
-//     onClose: action('On close'),
-//     action: {
-//       childAction: (
-//         <Icon className="text-fill-white fill-current">
-//           <Close type="filled" />
-//         </Icon>
-//       ),
-//       onClickAction: action('On click action'),
-//     },
-//   };
-
-//   useEffect(() => {
-//     showSnackbar(props);
-//   }, []);
-
-//   return (
-//     <div className="p-48">
-//       <Snackbar />
-//     </div>
-//   );
-// };
-
-// withIcon.story = {
-//   name: 'Icon',
-// };
-
-// export const withButton = () => {
-//   const props = {
-//     className: 'text-fill-primary-900',
-//     message: 'Prompt Option',
-//     description: 'Detail Info',
-//     textPosition: SnackbarTextPosition.Left,
-//     type: SnackbarType.Default,
-//     position: SnackbarPosition.TopCenter,
-//     autoClose: 5000,
-//     onClose: action('On close'),
-//     action: {
-//       childAction: <div className="text-fill-white">buttonContent</div>,
-//       onClickAction: action('On click action'),
-//     },
-//   };
-
-//   useEffect(() => {
-//     showSnackbar(props);
-//   }, []);
-
-//   return (
-//     <div className="p-48">
-//       <Snackbar />
-//     </div>
-//   );
-// };
-
-// withButton.story = {
-//   name: 'Button',
-// };

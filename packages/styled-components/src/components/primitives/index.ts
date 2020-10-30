245dbac24e67093db186444e123c77d338dee930
Pau Teruel <93.pau.teruel@gmail.com>
2020-10-30 13:43:36 +0100

    Styled TextField (#185)
    * feat: create textfield
    
    * define type TextField Type
    - support forwardRef and custom attributes for Shapes and Modes
    
    * feat: update textfield to use hoc
    
    * feat: add proper types to styled
    
    Co-authored-by: Takeichi Kanzaki Cabrera <tkanzakic@hey.com>

--- packages/styled-components/src/components/primitives/index.ts
+++ packages/styled-components/src/components/primitives/index.ts
@@ -9,6 +9,7 @@ export * from './Dropdown';
 export * from './Form/ErrorField';
 export * from './Form/RadioButton';
 export * from './Form/TextArea';
+export * from './Form/TextField';
 export * from './Icon';
 export * from './Image';
 export * from './Link';

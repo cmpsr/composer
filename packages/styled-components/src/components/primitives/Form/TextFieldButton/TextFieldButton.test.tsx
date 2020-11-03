77b3d2d8e88d5779bdda97f621e5044efd429f16
Pau Teruel <93.pau.teruel@gmail.com>
2020-11-03 17:12:33 +0100

    feat: add missing import

--- packages/styled-components/src/components/primitives/Form/TextFieldButton/TextFieldButton.test.tsx
+++ packages/styled-components/src/components/primitives/Form/TextFieldButton/TextFieldButton.test.tsx
@@ -1,6 +1,7 @@
 import React from 'react';
 import { render, fireEvent, screen } from '@testing-library/react';
 import { TextFieldButton } from './TextFieldButton';
+import 'jest-styled-components';
 
 describe('TextFieldButtonButton', () => {
   const testId = 'textFieldButton';

c58cba4d3aa9784bd28d38d3a7e7ccdc717771b6
Pau Teruel <93.pau.teruel@gmail.com>
2020-11-05 14:52:06 +0100

    Styled DropdownFieldMulti (#200)
    * feat: create dropdownnativeselect
    
    * feat: create dropdownfield
    
    * feat: dropdownmulti
    
    * feat: apply feedback
    
    * feat: create dropdownfield multi
    
    * feat: apply feedback

---
+++ packages/styled-components/src/components/primitives/Form/DropdownFieldMulti/DropdownFieldMulti.stories.tsx
@@ -0,0 +1,65 @@
+import React from 'react';
+import { DropdownFieldMulti } from '.';
+import { action } from '@storybook/addon-actions';
+import { text, boolean } from '@storybook/addon-knobs';
+
+export default {
+  title: 'Primitives/Form/DropdownFieldMulti',
+  component: DropdownFieldMulti,
+};
+
+const options = [
+  { value: 1, label: 'Option 1' },
+  { value: 2, label: 'Option 2' },
+  { value: 3, label: 'Option 3' },
+];
+
+export const Placeholder = () => (
+  <>
+    <h1>DropdownField placeholder</h1>
+    <DropdownFieldMulti
+      placeholder={'Select an item'}
+      onItemChange={action('On click icon')}
+      options={options}
+    />
+  </>
+);
+
+export const InitialOption = () => (
+  <>
+    <h1>DropdownField initial option</h1>
+    <DropdownFieldMulti
+      placeholder={'Select an item'}
+      onItemChange={action('On click icon')}
+      options={options}
+      initialSelectedOptions={[options[2]]}
+    />
+  </>
+);
+
+export const MultipleInitialOptions = () => (
+  <>
+    <h1>DropdownField multiple initial option</h1>
+    <DropdownFieldMulti
+      placeholder={'Select an item'}
+      onItemChange={action('On click icon')}
+      options={options}
+      initialSelectedOptions={[options[2], options[1]]}
+    />
+  </>
+);
+
+export const Playground = () => (
+  <>
+    <h1>Playground</h1>
+    <DropdownFieldMulti
+      placeholder={text('Placeholder', 'placeholder')}
+      name={text('Name', 'name')}
+      disabled={boolean('Disabled', false)}
+      invalid={boolean('Invalid', false)}
+      onItemChange={action('On click icon')}
+      options={options}
+      customCss={text('Custom css', 'border: 1px dashed blue')}
+    />
+  </>
+);

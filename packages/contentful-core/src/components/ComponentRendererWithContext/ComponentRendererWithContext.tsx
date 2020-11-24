efe7c288f934d97a4d1efd4b0684ca993d496671
Ryan Hefner <hi@ryanhefner.com>
2020-11-24 14:58:43 -0500

    update ComponentRendererWithContext to pass along context to ComponentRendererWithQuery

--- packages/contentful-core/src/components/ComponentRendererWithContext/ComponentRendererWithContext.tsx
+++ packages/contentful-core/src/components/ComponentRendererWithContext/ComponentRendererWithContext.tsx
@@ -5,6 +5,7 @@ import { ComponentRendererWithQuery } from '../ComponentRendererWithQuery';
 export type ChildProps = {
   className?: string;
   componentMap: any;
+  context?: any;
   queryMap: any;
   type: string;
   variables: { [key: string]: string | boolean | number };
@@ -14,12 +15,14 @@ export const ComponentRendererWithContext = ({
   className,
   variables,
   componentMap,
+  context,
   queryMap,
-  type
+  type,
 }: ChildProps): ReactElement | null => (
     <ContentfulProvider componentMap={componentMap} queryMap={queryMap}>
       <ComponentRendererWithQuery
         className={className}
+        context={context}
         type={type}
         variables={variables}
       />

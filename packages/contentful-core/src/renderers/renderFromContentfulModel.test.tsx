3f28063e6b0a1ad423879927024ee961ef6f6a09
Ryan Hefner <hi@ryanhefner.com>
2020-12-03 15:56:01 -0500

    expand renderFromContentfulModel coverage to account for new warnings and rendering

--- packages/contentful-core/src/renderers/renderFromContentfulModel.test.tsx
+++ packages/contentful-core/src/renderers/renderFromContentfulModel.test.tsx
@@ -11,29 +11,29 @@ const Link = ({ title, url }) => (
 );
 
 const componentMap = {
-  Link: (item): ReactElement => <Link {...item} />
+  Link: (item): ReactElement => <Link {...item} />,
 };
 
 const FAKE_ASSET = {
   contentType: 'image',
   title: 'fake asset title',
-  url: 'impulsum.vc'
+  url: 'impulsum.vc',
 };
 
 const FAKE_SYS = {
-  id: 'fake sys id'
+  id: 'fake sys id',
 };
 
 const FAKE_ROUTE = {
-  slug: '/'
+  slug: '/',
 };
 
 const FAKE_LINK_STYLE = {
   background: {
     default: {
-      bg: 'fill-primary-900'
-    }
-  }
+      bg: 'fill-primary-900',
+    },
+  },
 };
 
 const FAKE_LINK = {
@@ -45,7 +45,7 @@ const FAKE_LINK = {
   embeddedType: null,
   route: FAKE_ROUTE,
   style: FAKE_LINK_STYLE,
-  url: 'impulsum.vc'
+  url: 'impulsum.vc',
 };
 
 // Mock the graphql file calls
@@ -56,14 +56,51 @@ jest.mock(
 
 // Mock useQuery
 jest.mock('@apollo/client', () => ({
-  useQuery: jest.fn(() => ({ error: null, loading: null }))
+  useQuery: jest.fn(() => ({ error: null, loading: null })),
 }));
 
+// eslint-disable-next-line @typescript-eslint/no-empty-function
+jest.spyOn(global.console, 'warn').mockImplementation(() => {});
+
 describe('renderContentItemFromType', () => {
+  beforeEach(() => {
+    jest.resetAllMocks();
+  });
+
   it('renders a Link', () => {
     const { getByText } = render(
       renderFromContentfulModel({ componentMap }, FAKE_LINK)
     );
     getByText(FAKE_LINK.title);
+  });
+
+  it('warns if __typename is not specified in the incoming data', () => {
+    render(renderFromContentfulModel({ componentMap }, {}));
+    expect(global.console.warn).toHaveBeenCalledTimes(1);
+  });
+
+  it('renders nothing if __typename is not specified', () => {
+    const { container } = render(renderFromContentfulModel({ componentMap }, {}));
+    expect(container.innerHTML).toBe('');
+  });
+
+  it('warns if specified type does not exist in componentMap', () => {
+    render(renderFromContentfulModel({ componentMap }, { __typename: 'Test' }));
+    expect(global.console.warn).toHaveBeenCalledTimes(1);
+  });
+
+  it('renders nothing if specified type not found in componentMap', () => {
+    const { container } = render(renderFromContentfulModel({ componentMap }, { __typename: 'Test' }));
+    expect(container.innerHTML).toBe('');
+  });
+
+  it('warns if a sys.id has not been set on the incoming item', () => {
+    render(renderFromContentfulModel({ componentMap }, { __typename: 'Link' }));
+    expect(global.console.warn).toHaveBeenCalledTimes(1);
+  });
+
+  it('renders nothing if sys.id is not set in item data', () => {
+    const { container } = render(renderFromContentfulModel({ componentMap }, { __typename: 'Link' }));
+    expect(container.innerHTML).toBe('');
   });
 });

578acd9244e5b2b45790a685ace8b10897df30e5
David Torbeck <davetorbeck@gmail.com>
2021-03-04 10:52:40 -0500

    Allow for loading component on ComponentRendererWithQuery

--- packages/contentful-core/src/components/ComponentRendererWithQuery/ComponentRendererWithQuery.tsx
+++ packages/contentful-core/src/components/ComponentRendererWithQuery/ComponentRendererWithQuery.tsx
@@ -10,6 +10,7 @@ export type ChildProps = {
   queryMap?: any;
   type: string;
   variables: { [key: string]: string | boolean | number };
+  loadingPlaceholder?: ReactElement;
 };
 
 export const ComponentRendererWithQuery = ({
@@ -19,31 +20,39 @@ export const ComponentRendererWithQuery = ({
   queryMap: queryMapOverride,
   type,
   variables,
+  loadingPlaceholder,
 }: ChildProps): ReactElement | null => {
   try {
-    const {
-      componentMap: componentMapDefault,
-      queryMap: queryMapDefault,
-    } = useContext(ContentfulContext);
+    const { componentMap: componentMapDefault, queryMap: queryMapDefault } = useContext(
+      ContentfulContext,
+    );
 
     const query = (queryMapOverride || queryMapDefault)[type]();
-    const { data } = useQuery(query, {
+    const { data, loading } = useQuery(query, {
       variables,
       context,
     });
+
+    if (loadingPlaceholder && loading) {
+      return loadingPlaceholder;
+    }
+
     if (!data) return null;
 
     const key = Object.keys(data)[0];
+
     if (!key) return null;
 
-    const dataToParse =
-      key.indexOf('Collection') >= 0 ? data[key].items[0] : data[key];
+    const dataToParse = key.indexOf('Collection') >= 0 ? data[key].items[0] : data[key];
+
     return (
-      <ComponentRenderer
-        className={className}
-        data={dataToParse}
-        componentMap={componentMapOverride || componentMapDefault}
-      />
+      <>
+        <ComponentRenderer
+          className={className}
+          data={dataToParse}
+          componentMap={componentMapOverride || componentMapDefault}
+        />
+      </>
     );
   } catch (e) {
     console.log('error', e);

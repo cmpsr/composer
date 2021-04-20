1ecccf69bba75fe20526f09c5bad2b64446369f5
Dawin Valenzuela <55356515+dawinValenzuela@users.noreply.github.com>
2021-04-20 04:16:21 -0500

    feat: adding forwardRef to the TextArea component (#243)
    * adding forwardRef to the TextArea component
    
    * Add changeset
    
    * adding types

--- packages/components/src/components/primitives/Form/TextArea/TextArea.tsx
+++ packages/components/src/components/primitives/Form/TextArea/TextArea.tsx
@@ -1,33 +1,39 @@
-import React from 'react';
+import React, { forwardRef } from 'react';
 import cn from 'classnames';
-import { Props } from './TextArea.types';
+import { Props, Type } from './TextArea.types';
 import { Shapes } from 'components/Components.types';
 import { StyledTextArea } from './TextArea.styled';
 
-export const TextArea = ({
-  className,
-  value = 'placeholder',
-  name,
-  disabled = false,
-  invalid: isInvalid = false,
-  required,
-  placeholder,
-  onChange,
-  shape,
-  customCss,
-  testId = 'textArea',
-}: Props) => (
-  <StyledTextArea
-    data-testid={testId}
-    css={customCss}
-    className={cn(className, shape, { invalid: isInvalid })}
-    disabled={disabled}
-    defaultValue={value}
-    placeholder={placeholder}
-    name={name}
-    required={required}
-    onChange={onChange}
-  />
+export const TextArea: Type = forwardRef<HTMLTextAreaElement, Props>(
+  (
+    {
+      className,
+      value = '',
+      name,
+      disabled = false,
+      invalid: isInvalid = false,
+      required,
+      placeholder,
+      onChange,
+      shape,
+      customCss,
+      testId = 'textArea',
+    }: Props,
+    ref
+  ) => (
+    <StyledTextArea
+      ref={ref}
+      data-testid={testId}
+      css={customCss}
+      className={cn(className, shape, { invalid: isInvalid })}
+      disabled={disabled}
+      defaultValue={value}
+      placeholder={placeholder}
+      name={name}
+      required={required}
+      onChange={onChange}
+    />
+  )
 );
 
 TextArea.Shapes = Shapes;
--- packages/components/src/components/primitives/Form/TextArea/TextArea.types.ts
+++ packages/components/src/components/primitives/Form/TextArea/TextArea.types.ts
@@ -1,4 +1,9 @@
-import { ChangeEvent } from 'react';
+import {
+  ChangeEvent,
+  ForwardRefExoticComponent,
+  RefAttributes,
+  PropsWithoutRef,
+} from 'react';
 import { CssCustomizableProps, Shapes } from 'components/Components.types';
 
 export type Props = CssCustomizableProps & {
@@ -10,4 +15,10 @@ export type Props = CssCustomizableProps & {
   placeholder: string;
   onChange?: (event: ChangeEvent<HTMLTextAreaElement>) => void;
   shape?: Shapes;
+};
+
+export type Type = ForwardRefExoticComponent<
+  PropsWithoutRef<Props> & RefAttributes<HTMLTextAreaElement>
+> & {
+  Shapes?: typeof Shapes;
 };

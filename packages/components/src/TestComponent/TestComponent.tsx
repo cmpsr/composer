34768f77a6c3a8252279985729821c94aa8b97d5
Takeichi Kanzaki Cabrera <tkanzakic@hey.com>
2021-11-04 18:16:46 +0100

    add missing chakra dependencies

--- packages/components/src/TestComponent/TestComponent.tsx
+++ packages/components/src/TestComponent/TestComponent.tsx
@@ -1,9 +1,10 @@
 import React, { FunctionComponent } from 'react';
+import { Button } from '@chakra-ui/react';
 
 interface Props {
   children: React.ReactNode;
 }
 
 export const TestComponent: FunctionComponent<Props> = ({ children }) => {
-  return <div>{children}</div>;
+  return <Button>{children}</Button>;
 };

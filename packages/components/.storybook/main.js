f084fb9fd1754079a4fe4e55f7800067188965db
Takeichi Kanzaki Cabrera <tkanzakic@hey.com>
2021-11-05 19:02:33 +0100

    remove unnecessary dependencies - add rules for mjs files

--- packages/components/.storybook/main.js
+++ packages/components/.storybook/main.js
@@ -1,10 +1,13 @@
 module.exports = {
   stories: ['../src/**/*.stories.mdx', '../src/**/*.stories.@(js|jsx|ts|tsx)'],
-  addons: [
-    '@storybook/addon-links',
-    '@storybook/addon-essentials',
-    '@storybook/addon-a11y',
-    '@storybook/addon-docs',
-    '@storybook/addon-knobs',
-  ],
+  addons: ['@storybook/addon-links', '@storybook/addon-essentials'],
+  webpackFinal: (config) => {
+    config.module.rules.push({
+      test: /\.mjs$/,
+      include: /node_modules/,
+      type: 'javascript/auto',
+    });
+
+    return config;
+  },
 };

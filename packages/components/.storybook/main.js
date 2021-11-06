1a5bc121da1db6bc31ba14c547d9ba616f7a7958
Takeichi Kanzaki Cabrera <tkanzakic@hey.com>
2021-11-06 11:30:54 +0100

    fix chakra styles in storybook

--- packages/components/.storybook/main.js
+++ packages/components/.storybook/main.js
@@ -1,13 +1,23 @@
 module.exports = {
   stories: ['../src/**/*.stories.mdx', '../src/**/*.stories.@(js|jsx|ts|tsx)'],
   addons: ['@storybook/addon-links', '@storybook/addon-essentials'],
-  webpackFinal: (config) => {
+  webpackFinal: async (config) => {
     config.module.rules.push({
       test: /\.mjs$/,
       include: /node_modules/,
       type: 'javascript/auto',
     });
 
-    return config;
+    return {
+      ...config,
+      resolve: {
+        ...config.resolve,
+        alias: {
+          ...config.resolve.alias,
+          '@emotion/core': '@emotion/react',
+          'emotion-theming': '@emotion/react',
+        },
+      },
+    };
   },
 };

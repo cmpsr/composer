52ed380ff0686ad8ea7ad82154308f9a01f39e29
Takeichi Kanzaki Cabrera <tkanzakic@hey.com>
2021-11-10 14:32:00 +0100

    extract theme componentsdefinition

--- packages/components/src/theme/ComposerTheme/theme.ts
+++ packages/components/src/theme/ComposerTheme/theme.ts
@@ -1,14 +1,10 @@
 import { Theme } from './types';
 import { defaultColors } from './Colors';
-import { Text } from './Components/Text';
-import { TextPairing } from './Components/TextPairing';
+import { components } from './Components';
 import { textStyles } from './styles';
 
 export const theme: Theme = {
   colors: defaultColors,
-  components: {
-    Text,
-    TextPairing,
-  },
+  components,
   textStyles,
 };

dc6abce52ec15d9a199f227c93158d0665aad05e
Pau Teruel <93.pau.teruel@gmail.com>
2021-11-18 09:07:07 +0100

    feat: add spacing primitives (#262)
    * feat: add spacing primitives
    
    * feat: show units beside spacing bar
    
    * feat: remove table caption

--- packages/components/src/theme/ComposerTheme/types.ts
+++ packages/components/src/theme/ComposerTheme/types.ts
@@ -1,9 +1,11 @@
 import { SystemStyleObject } from '@chakra-ui/react';
 import { ComponentStyleConfig } from '@chakra-ui/theme';
 import { Colors } from './Colors';
+import { Spacing } from './Spacing';
 
 export interface Theme {
   colors: Colors;
+  space: Spacing;
   components: {
     [key: string]: ComponentStyleConfig;
   };

21dc6c3d6287497c20d59cf548fd5ca38ff62228
Pau Teruel Soldevila <93.pau.teruel@gmail.com>
2021-11-18 08:58:35 +0100

    feat: disable background addon to avoid light and dark contrast in texts

--- packages/components/.storybook/main.js
+++ packages/components/.storybook/main.js
@@ -1,6 +1,14 @@
 module.exports = {
   stories: ['../src/**/*.stories.mdx', '../src/**/*.stories.@(js|jsx|ts|tsx)'],
-  addons: ['@storybook/addon-links', '@storybook/addon-essentials'],
+  addons: [
+    '@storybook/addon-links',
+    {
+      name: '@storybook/addon-essentials',
+      options: {
+        backgrounds: false,
+      },
+    },
+  ],
   webpackFinal: async (config) => {
     config.module.rules.push({
       test: /\.mjs$/,

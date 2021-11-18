75276b045a1032fd7eddfd13aa9de822ef63e468
Pau Teruel Soldevila <93.pau.teruel@gmail.com>
2021-11-18 09:00:04 +0100

    feat: create elevations primitive

--- packages/components/src/theme/ComposerTheme/theme.ts
+++ packages/components/src/theme/ComposerTheme/theme.ts
@@ -1,10 +1,12 @@
 import { Theme } from './types';
 import { defaultColors } from './Colors';
+import { defaultShadows } from './Shadows';
 import { components } from './Components';
 import { textStyles } from './styles';
 
 export const theme: Theme = {
   colors: defaultColors,
+  shadows: defaultShadows,
   components,
   textStyles,
 };
--- packages/components/src/theme/ComposerTheme/types.ts
+++ packages/components/src/theme/ComposerTheme/types.ts
@@ -1,9 +1,11 @@
 import { SystemStyleObject } from '@chakra-ui/react';
 import { ComponentStyleConfig } from '@chakra-ui/theme';
 import { Colors } from './Colors';
+import { Shadows } from './Shadows';
 
 export interface Theme {
   colors: Colors;
+  shadows: Shadows;
   components: {
     [key: string]: ComponentStyleConfig;
   };

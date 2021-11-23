a5e36c99e1622bafdad114d1ce472ebda75e202c
Takeichi Kanzaki Cabrera <tkanzakic@hey.com>
2021-11-23 12:27:32 +0100

    implement Section layout component

---
+++ packages/components/src/components/layout/Section/Section.test.tsx
@@ -0,0 +1,14 @@
+import React from 'react';
+import { screen, renderWithProviders } from 'tests/renderWithProviders';
+import { Section } from '.';
+
+describe('Section', () => {
+  const givenComponentRendered = (children: JSX.Element) =>
+    renderWithProviders(<Section>{children}</Section>);
+
+  test('should render children', () => {
+    const content = 'Composer rocks!';
+    givenComponentRendered(<p>{content}</p>);
+    screen.getByText(content);
+  });
+});

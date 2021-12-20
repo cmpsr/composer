71f9b108510b25dc6dc3ceb0bbd932d7c1634b40
Takeichi Kanzaki Cabrera <tkanzakic@hey.com>
2021-12-20 18:31:24 +0100

    feat: add button text color new props - fix button disabled states

--- packages/components/src/theme/ComposerTheme/Components/Button.ts
+++ packages/components/src/theme/ComposerTheme/Components/Button.ts
@@ -1,58 +1,66 @@
 import { ComponentStyleConfig } from '@chakra-ui/theme';
+import { transparentize } from '@chakra-ui/theme-tools';
 
-const generateButton = (color: string) => ({
-  backgroundColor: `${color}-default`,
-  _disabled: {
+const generateButton = (color: string, textColor?: string) => {
+  const _disabled = {
     backgroundColor: `${color}-disabled`,
-  },
-  _hover: {
-    backgroundColor: `${color}-hover`,
-    _disabled: {
-      backgroundColor: `${color}-disabled`,
+    opacity: 1,
+    color: transparentize(`text-button-${textColor || color}`, 0.5),
+  };
+  return {
+    color: `text-button-${textColor || color}`,
+    backgroundColor: `${color}-default`,
+    _disabled,
+    _hover: {
+      backgroundColor: `${color}-hover`,
+      _disabled,
     },
-  },
-  _focus: {
-    backgroundColor: `${color}-hover`,
-    boxShadow: `0 0 0 3px var(--chakra-colors-${color}-focus)`,
-  },
-  _pressed: {
-    backgroundColor: `${color}-pressed`,
-  },
-  _active: {
-    backgroundColor: `${color}-pressed`,
-  },
-});
+    _focus: {
+      backgroundColor: `${color}-hover`,
+      boxShadow: `0 0 0 3px var(--chakra-colors-${color}-focus)`,
+    },
+    _pressed: {
+      backgroundColor: `${color}-pressed`,
+    },
+    _active: {
+      backgroundColor: `${color}-pressed`,
+    },
+  };
+};
 
-const generateAltButton = (color: string) => ({
-  backgroundColor: 'background-action-default',
-  color: `text-link-${color}-default`,
-  border: '1px solid',
-  borderColor: `text-link-${color}-default`,
-  _disabled: {
+const generateAltButton = (color: string) => {
+  const _disabled = {
     backgroundColor: 'background-container-disabled',
-  },
-  _hover: {
-    backgroundColor: 'background-action-hover',
-    _disabled: {
+    opacity: 1,
+    color: transparentize(`text-link-${color}-default`, 0.5),
+    borderColor: transparentize(`text-link-${color}-default`, 0.5),
+  };
+  return {
+    backgroundColor: 'background-action-default',
+    color: `text-link-${color}-default`,
+    border: '1px solid',
+    borderColor: `text-link-${color}-default`,
+    _disabled,
+    _hover: {
       backgroundColor: 'background-action-hover',
+      _disabled,
     },
-  },
-  _focus: {
-    backgroundColor: 'background-action-hover',
-    boxShadow: `0 0 0 3px var(--chakra-colors-${color}-focus)`,
-  },
-  _pressed: {
-    backgroundColor: 'background-action-pressed',
-  },
-  _active: {
-    backgroundColor: 'background-action-pressed',
-  },
-});
+    _focus: {
+      backgroundColor: 'background-action-hover',
+      boxShadow: `0 0 0 3px var(--chakra-colors-${color}-focus)`,
+    },
+    _pressed: {
+      backgroundColor: 'background-action-pressed',
+    },
+    _active: {
+      backgroundColor: 'background-action-pressed',
+    },
+  };
+};
 
 export const Button: ComponentStyleConfig = {
   baseStyle: {
     borderRadius: '0.375rem',
-    color: 'text-light',
   },
   sizes: {
     xs: {
@@ -82,7 +90,7 @@ export const Button: ComponentStyleConfig = {
     secondary: generateButton('secondary'),
     'primary-alt': generateAltButton('primary'),
     'secondary-alt': generateAltButton('secondary'),
-    destroy: generateButton('alert-error'),
+    destroy: generateButton('alert-error', 'alert'),
   },
   defaultProps: {
     size: 'md',

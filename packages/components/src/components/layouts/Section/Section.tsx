03db8a35018964493f20fb43eaa1bc9a15e742dd
Takeichi Kanzaki Cabrera <tkanzakic@hey.com>
2021-12-22 15:26:18 +0100

    feat: update chakra to latest version (#276)
    * feat: update chakra to latest version
    
    * chore: use node version 16
    
    * chore: use node version 16 in docker
    
    * chore: remove unnecessary background color
    
    * chore: use node 16 in chromatic action

--- packages/components/src/components/layouts/Section/Section.tsx
+++ packages/components/src/components/layouts/Section/Section.tsx
@@ -3,5 +3,5 @@ import React, { FC } from 'react';
 import { SectionProps } from '.';
 
 export const Section: FC<SectionProps> = (props) => (
-  <Box {...props} as="section" />
+  <Box as="section" {...props} />
 );

4f925cf788025f903fe857b79e6fb498185a83ce
Takeichi Kanzaki Cabrera <tkanzakic@hey.com>
2021-12-23 09:34:43 +0100

    feat: wrap Flex component

---
+++ packages/components/src/components/layouts/Flex/Flex.stories.tsx
@@ -0,0 +1,44 @@
+import React from 'react';
+import { Meta } from '@storybook/react';
+import { Flex } from './Flex';
+import { Box, Text } from 'components';
+
+export default {
+  component: Flex,
+  title: 'Components/Layouts/Flex',
+  argTypes: {
+    direction: {
+      options: ['column', 'row'],
+      control: { type: 'select' },
+    },
+    wrap: {
+      options: ['wrap', 'wrap-reverse', 'nowrap'],
+      control: { type: 'select' },
+    },
+  },
+} as Meta;
+
+const Template = ({ children, ...rest }) => (
+  <Flex {...rest}>
+    {[1, 2, 3, 4, 5].map((index) => (
+      <Box
+        key={index}
+        margin="spacer-1"
+        padding="spacer-4"
+        border="1px solid var(--chakra-colors-accent-default)"
+      >
+        <Text variant="text-body-bold">
+          {children}:{` ${index}`}
+        </Text>
+      </Box>
+    ))}
+  </Flex>
+);
+export const Playground = Template.bind({});
+Playground.args = {
+  children: "I'm a Flex container with some child",
+  direction: 'row',
+  border: '1px solid var(--chakra-colors-primary-default)',
+  wrap: 'wrap',
+  padding: 'spacer-2',
+};

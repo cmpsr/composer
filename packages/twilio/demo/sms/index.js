693f7a0cefd9193d88b2a810fc6b72a3d6e14b6b
TheEnmanuel23 <enmisac@gmail.com>
2022-01-11 11:58:19 -0600

    add endpoints to sms demo API

--- packages/twilio/demo/sms/index.js
+++ packages/twilio/demo/sms/index.js
@@ -7,11 +7,33 @@ app.use(express.json());
 
 const twilioSms = useTwilioSms();
 
+app.get('/available-phones', async (req, res) => {
+  const { areaCode, country, mmsEnabled, smsEnabled, limit } = req.query;
+  const phones = await twilioSms.availablePhones(
+    areaCode,
+    country,
+    mmsEnabled,
+    smsEnabled,
+    limit
+  );
+  res.json(phones);
+});
+
 app.post('/send-sms', async (req, res) => {
   const { message, from, to } = req.body;
   const response = await twilioSms.send(message, from, to);
-  console.log(response);
   res.json(response);
+});
+
+app.post('/buy-phone-number', async (req, res) => {
+  const { phoneNumber, friendlyName, smsUrl, smsMethod } = req.body;
+  const phone = await twilioSms.buy(
+    phoneNumber,
+    friendlyName,
+    smsUrl,
+    smsMethod
+  );
+  res.json(phone);
 });
 
 app.listen(3000, () => console.log('Server running on port 3000'));

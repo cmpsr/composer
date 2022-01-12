3f7a83a7c227e48d4185c613c9c73672efc13ac6
Angel Gonzalez <41210262+Angele91@users.noreply.github.com>
2022-01-12 12:18:01 -0300

    [COM-131] Implementation of Badge component (#285)
    * feat: implementation of badge component
    
    * fix: adding re-export at the parent folder for Badge component
    
    * fix: fix alphabetical order of composer theme components object

--- packages/components/src/theme/ComposerTheme/Components/index.ts
+++ packages/components/src/theme/ComposerTheme/Components/index.ts
@@ -3,8 +3,10 @@ import { Spinner } from './Spinner';
 import { Tabs } from './Tabs';
 import { Text } from './Text';
 import { TextPairing } from './TextPairing';
+import { Badge } from './Badge';
 
 export const components = {
+  Badge,
   Button,
   Spinner,
   Tabs,

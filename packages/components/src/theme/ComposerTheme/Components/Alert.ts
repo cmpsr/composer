999f8d490c5941a28ddfef3d095ae4041a5ec5f6
Angel Gonzalez <angele.gm91@gmail.com>
2022-01-14 13:22:50 -0300

    feat: implement theming for alert colors

--- packages/components/src/theme/ComposerTheme/Components/Alert.ts
+++ packages/components/src/theme/ComposerTheme/Components/Alert.ts
@@ -1,4 +1,82 @@
 import { ComponentStyleConfig } from '@chakra-ui/react';
+import { alertAnatomy as parts } from '@chakra-ui/anatomy';
+import { PartsStyleFunction } from '@chakra-ui/theme-tools';
+
+const defaultContainerColorScheme = {
+  success: {
+    backgroundColor: 'background-success',
+  },
+  error: {
+    backgroundColor: 'background-error',
+  },
+  warning: {
+    backgroundColor: 'background-warning',
+  },
+  info: {
+    backgroundColor: 'background-inform',
+  },
+};
+
+const containerColorScheme = {
+  solid: {
+    success: {
+      backgroundColor: 'alert-success-default',
+    },
+    error: {
+      backgroundColor: 'alert-error-default',
+    },
+    warning: {
+      backgroundColor: 'alert-warning-default',
+    },
+    info: {
+      backgroundColor: 'alert-info-default',
+    },
+  },
+  subtle: defaultContainerColorScheme,
+  'left-accent': defaultContainerColorScheme,
+};
+
+const variantSolid: PartsStyleFunction<typeof parts> = (props) => {
+  const { status, variant } = props;
+
+  return {
+    container: {
+      ...(containerColorScheme[variant]?.[status] || {}),
+    },
+    title: {
+      color: 'text-light',
+    },
+    description: {
+      color: 'text-light',
+    },
+    icon: {
+      status: {
+        success: {
+          color: 'text-light',
+        },
+        warning: {
+          color: 'text-light',
+        },
+        error: {
+          color: 'text-light',
+        },
+        info: {
+          color: 'text-light',
+        },
+      },
+    },
+  };
+};
+
+const generateOtherVariants: PartsStyleFunction<typeof parts> = (props) => {
+  const { status, variant } = props;
+
+  return {
+    container: {
+      ...(containerColorScheme[variant]?.[status] || {}),
+    },
+  };
+};
 
 export const Alert: ComponentStyleConfig = {
   baseStyle: {
@@ -38,29 +116,8 @@ export const Alert: ComponentStyleConfig = {
     variant: 'solid',
   },
   variants: {
-    solid: {
-      title: {
-        color: 'text-light',
-      },
-      description: {
-        color: 'text-light',
-      },
-      icon: {
-        status: {
-          success: {
-            color: 'text-light',
-          },
-          warning: {
-            color: 'text-light',
-          },
-          error: {
-            color: 'text-light',
-          },
-          info: {
-            color: 'text-light',
-          },
-        },
-      },
-    },
+    solid: variantSolid,
+    subtle: generateOtherVariants,
+    'left-accent': generateOtherVariants,
   },
 };

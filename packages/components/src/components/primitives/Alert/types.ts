f868d32176986bed315d3dc86f9a788388bb4453
Angel Gonzalez <41210262+Angele91@users.noreply.github.com>
2022-01-17 12:52:26 -0300

    [COM-129] Implement Alert Component (#294)
    * feat: implementation of alert component
    
    * fix: adding missing components for Alert component implementation
    
    * feat: implement theming for alert colors
    
    * fix: add default state to Alert
    
    * fix: changed testIds prefix to 'cmpsr.', converted px to rem and removed export from AlertIcon
    
    * fix: converting px to rem

---
+++ packages/components/src/components/primitives/Alert/types.ts
@@ -0,0 +1,21 @@
+import { AlertProps as ChakraAlertProps } from '@chakra-ui/react';
+
+export const alertStates = ['success', 'warning', 'error', 'info'] as const;
+export const alertTitleAlignments = ['none', 'left', 'top'] as const;
+export const alertVariants = ['solid', 'subtle', 'left-accent'] as const;
+
+export type AlertState = typeof alertStates[number];
+export type AlertTitleAlignment = typeof alertTitleAlignments[number];
+export type AlertStyle = typeof alertVariants[number];
+
+export type AlertStyles = {
+  icon: { status: { [key: string]: { color: string } } };
+};
+
+export interface AlertProps extends ChakraAlertProps {
+  description?: string;
+  state?: AlertState;
+  titleAlignment?: AlertTitleAlignment;
+  title?: string;
+  variant?: AlertStyle;
+}

ed2439d175a7c496f62daab255dd0da8e48c7b2a
Agustín Tisera <agustint@nimble.la>
2022-01-19 11:59:16 -0300

    ADD: Unit tests

--- packages/components/src/components/primitives/Link/Link.test.tsx
+++ packages/components/src/components/primitives/Link/Link.test.tsx
@@ -1,5 +1,55 @@
+import React from 'react';
+import { screen, renderWithProviders } from 'tests/renderWithProviders';
+import { Link } from './Link';
+
 describe('Link component', () => {
-  it('Should render', () => {
-    expect(0).toBe(0);
+  const icon = <i data-testid="testId" />;
+  const textElement = <span data-testid="testId">span component</span>;
+
+  it.skip('Should render', () => {
+    const { getByTestId } = renderWithProviders(<Link data-testid="testId" />);
+    getByTestId('testid');
+  });
+
+  it('Should render a link with an icon on left side as default', () => {
+    renderWithProviders(
+      <Link role="link" data-testid="testId" icon={icon}>
+        {textElement}
+      </Link>
+    );
+
+    const linkComponent = screen.queryAllByTestId('testId');
+    const [parentNode, firstChild, secondChild] = linkComponent;
+    expect(parentNode.nodeName).toBe('A');
+    expect(firstChild.nodeName).toBe('I');
+    expect(secondChild.nodeName).toBe('SPAN');
+  });
+
+  it('Should render a link with an icon on the right side', () => {
+    renderWithProviders(
+      <Link iconPosition="right" role="link" data-testid="testId" icon={icon}>
+        {textElement}
+      </Link>
+    );
+
+    const linkComponent = screen.queryAllByTestId('testId');
+    const [parentNode, firstChild, secondChild] = linkComponent;
+    expect(parentNode.nodeName).toBe('A');
+    expect(firstChild.nodeName).toBe('SPAN');
+    expect(secondChild.nodeName).toBe('I');
+  });
+
+  it('Should render a link with an icon on the left side', () => {
+    renderWithProviders(
+      <Link iconPosition="left" role="link" data-testid="testId" icon={icon}>
+        {textElement}
+      </Link>
+    );
+
+    const linkComponent = screen.queryAllByTestId('testId');
+    const [parentNode, firstChild, secondChild] = linkComponent;
+    expect(parentNode.nodeName).toBe('A');
+    expect(firstChild.nodeName).toBe('I');
+    expect(secondChild.nodeName).toBe('SPAN');
   });
 });

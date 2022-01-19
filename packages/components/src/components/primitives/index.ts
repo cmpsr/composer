a7153b84a257c4d8bfb2768105a1827c5ba5bacf
Pau Teruel Soldevila <93.pau.teruel@gmail.com>
2022-01-19 16:05:59 +0100

    feat: create Card component

--- packages/components/src/components/primitives/index.ts
+++ packages/components/src/components/primitives/index.ts
@@ -2,6 +2,7 @@ export * from './Alert';
 export * from './Avatar';
 export * from './Badge';
 export * from './Button';
+export * from './Card';
 export * from './Divider';
 export * from './Image';
 export * from './Slider';

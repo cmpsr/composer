17ac393c1a603f27f638c8a4c9f88f47aced9409
Pau Teruel <93.pau.teruel@gmail.com>
2022-01-21 14:29:31 +0100

    Update storybook and deprecate postcss (#327)
    * feat: update sb and remove postcss
    
    * feat: create changeset

--- packages/components/.storybook/main.js
+++ packages/components/.storybook/main.js
@@ -1,6 +1,7 @@
 module.exports = {
   stories: ['../src/**/*.stories.mdx', '../src/**/*.stories.@(js|jsx|ts|tsx)'],
   addons: ['@storybook/addon-links', '@storybook/addon-essentials'],
+  features: { postcss: false },
   refs: {
     '@chakra-ui/react': {
       disable: true,

3ad4b165215ee0aab1ef2301cd34380baed27123
TheEnmanuel23 <enmisac@gmail.com>
2022-01-28 15:31:56 -0600

    remove unused variant

--- packages/components/src/components/primitives/Textarea/types.ts
+++ packages/components/src/components/primitives/Textarea/types.ts
@@ -4,6 +4,6 @@ export const textareaSizes = ['s', 'm'] as const;
 
 export type TextareaSize = typeof textareaSizes[number];
 
-export interface TextareaProps extends ChakraTextareaProps {
+export interface TextareaProps extends Omit<ChakraTextareaProps, 'variant' | 'size'> {
   size?: TextareaSize;
 }

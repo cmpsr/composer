bd261bb3ee3aa0fc98538d4db647e128b885acc5
TheEnmanuel23 <enmisac@gmail.com>
2022-01-28 15:12:02 -0600

    remove variant from textarea component

--- packages/components/src/components/primitives/Textarea/types.ts
+++ packages/components/src/components/primitives/Textarea/types.ts
@@ -1,12 +1,9 @@
 import { TextareaProps as ChakraTextareaProps } from "@chakra-ui/react";
 
 export const textareaSizes = ['s', 'm'] as const;
-export const textareaVariants = ["default"] as const;
 
 export type TextareaSize = typeof textareaSizes[number];
-export type TextareaVariant = typeof textareaVariants[number];
 
 export interface TextareaProps extends ChakraTextareaProps {
-  variant?: TextareaVariant;
   size?: TextareaSize;
 }

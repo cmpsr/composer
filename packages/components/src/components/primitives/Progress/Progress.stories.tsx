a32908efab6791e2025d56455d0b6944271f1637
Pau Teruel Soldevila <93.pau.teruel@gmail.com>
2022-01-28 12:26:57 +0100

    feat: update story to have key

--- packages/components/src/components/primitives/Progress/Progress.stories.tsx
+++ packages/components/src/components/primitives/Progress/Progress.stories.tsx
@@ -1,7 +1,7 @@
 import React from 'react';
 import { Meta } from '@storybook/react';
 import { Progress } from './Progress';
-import { Divider } from '@chakra-ui/react';
+import { Divider, Box } from '@chakra-ui/react';
 import { progressSizes } from './types';
 
 export default {
@@ -11,10 +11,10 @@ export default {
 
 const AllTemplate = () =>
   progressSizes.map((size, key) => (
-    <>
-      <Progress value={20} size={size} key={key} />
+    <Box key={key}>
+      <Progress value={20} size={size} />
       <Divider my={3} />
-    </>
+    </Box>
   ));
 
 export const All = AllTemplate.bind({});

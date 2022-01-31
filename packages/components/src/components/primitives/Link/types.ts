ce78d3df9722994ded52581ca7ba288e13d33e6d
Takeichi Kanzaki Cabrera <tkanzakic@hey.com>
2022-01-31 11:53:10 +0100

    [COM-393] Add support for leading and trailing icons to Link (#351)
    * feat: expose IconSize type
    
    * fix: icon component
    - adds support for leading and trailing icons
    - fix spacing between text and icons
    
    * chore: generate changeset
    
    * feat: rename icons to leading and trailing
    
    * feat: replace margins with column gap

--- packages/components/src/components/primitives/Link/types.ts
+++ packages/components/src/components/primitives/Link/types.ts
@@ -2,14 +2,11 @@ import { LinkProps as ChakraLinkProps } from '@chakra-ui/react';
 import { FC } from 'react';
 import { IconProps } from '../Icons';
 
-export const linkIconPositions = ['leading', 'trailing'] as const;
 export const linkSizes = ['s', 'm', 'l'] as const;
-
-type LinkIconPositions = typeof linkIconPositions[number];
 type LinkSizes = typeof linkSizes[number];
 
 export interface LinkProps extends ChakraLinkProps {
-  icon?: FC<IconProps>;
-  iconPosition?: LinkIconPositions;
   size?: LinkSizes;
+  leadingIcon?: FC<IconProps>;
+  trailingIcon?: FC<IconProps>;
 }

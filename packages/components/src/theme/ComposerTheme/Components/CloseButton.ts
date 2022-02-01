9e797bf827709ead182b3c3e3a177d138d3f391e
TheEnmanuel23 <enmisac@gmail.com>
2022-02-01 16:08:31 -0600

    apply sizes to close button component

--- packages/components/src/theme/ComposerTheme/Components/CloseButton.ts
+++ packages/components/src/theme/ComposerTheme/Components/CloseButton.ts
@@ -1,7 +1,12 @@
 import { ComponentStyleConfig } from '@chakra-ui/react';
+import { cssVar } from '@chakra-ui/theme-tools';
 
+const $size = cssVar('close-button-size');
+console.log('>>', $size.reference)
 export const CloseButton: ComponentStyleConfig = {
   baseStyle: {
+    w: [$size.reference],
+    h: [$size.reference],
     color: 'text-primary',
     _hover: {
       bg: 'background-action-hover',
@@ -14,17 +19,23 @@ export const CloseButton: ComponentStyleConfig = {
     },
   },
   sizes: {
-    // s: {
-    //   fontSize: "10px",
-    // },
-    // m: {
-    //   fontSize: "12px",
-    // },
-    // l: {
-    //   fontSize: "16px",
-    // },
+    s: {
+      [$size.variable]: "1.5rem",
+      fontSize: "0.625rem", //10px
+      // fontSize: "1rem", //16px
+    },
+    m: {
+      [$size.variable]: "2rem",
+      fontSize: "1rem", // 16px
+      // fontSize: "1.5rem",  // 24px
+    },
+    l: {
+      [$size.variable]: '2.5rem',
+      fontSize: '1rem', // 16px
+      // fontSize: '1.5rem', // 24px
+    },
   },
   defaultProps: {
-    // size: 'l',
+    size: 'l',
   },
 };

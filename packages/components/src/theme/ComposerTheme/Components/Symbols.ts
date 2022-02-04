6394c3777512ce44ff8607b26b09a5264a58917e
Angel Gonzalez <angele.gm91@gmail.com>
2022-02-04 11:34:49 -0300

    fix: spreading textStyle and fix Figma tokens

--- packages/components/src/theme/ComposerTheme/Components/Symbols.ts
+++ packages/components/src/theme/ComposerTheme/Components/Symbols.ts
@@ -1,17 +1,17 @@
 import { ComponentStyleConfig } from '@chakra-ui/react';
 
 export const Symbols: ComponentStyleConfig = {
-  baseStyle: {
-    textStyle: 'text-body-meta-regular',
+  baseStyle: ({ theme }) => ({
+    ...theme.textStyles['text-body-floating-label-bold'],
     color: 'text-primary',
     background: 'background-card',
     borderStyle: 'solid',
     borderColor: 'ui-element-outline-default',
     borderWidth: '0.0625rem',
-    borderRadius: '0.375rem',
+    borderRadius: 'radii-2',
     paddingX: '0.25rem',
     paddingY: '0.125rem',
     // Override default chakra borderBottomWidth
     borderBottomWidth: '0.0625rem',
-  },
+  }),
 };

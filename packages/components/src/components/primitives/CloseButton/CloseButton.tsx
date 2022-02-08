719fe424b8a8452b37cc615b1e70b9a93290d4e8
TheEnmanuel23 <enmisac@gmail.com>
2022-02-07 22:07:00 -0600

    send rest properties to close button container

--- packages/components/src/components/primitives/CloseButton/CloseButton.tsx
+++ packages/components/src/components/primitives/CloseButton/CloseButton.tsx
@@ -3,9 +3,15 @@ import { IconButton, useStyleConfig } from '@chakra-ui/react';
 import { CloseButtonProps, CloseButtonStyle } from './types';
 import { IconX } from '../Icons';
 
-export const CloseButton: FC<CloseButtonProps> = (props) => {
-  const { container, icon } = useStyleConfig('CloseButton', props) as CloseButtonStyle;
+export const CloseButton: FC<CloseButtonProps> = ({ size, ...rest }) => {
+  const { container, icon } = useStyleConfig('CloseButton', { size }) as CloseButtonStyle;
   return (
-    <IconButton aria-label="close button" data-testid="cmpsr.closeButton" icon={<IconX {...icon} />} {...container} />
+    <IconButton
+      aria-label="close button"
+      data-testid="cmpsr.closeButton"
+      icon={<IconX {...icon} />}
+      {...container}
+      {...rest}
+    />
   );
 };

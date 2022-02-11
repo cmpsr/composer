ee32739f1645dab2e6a0c98cf50b9aa1ba50afba
Takeichi Kanzaki Cabrera <tkanzakic@hey.com>
2022-02-11 15:58:20 +0100

    fix: remove unnecessary moduleMapper

--- packages/analytics/jest.config.js
+++ packages/analytics/jest.config.js
@@ -82,9 +82,9 @@ module.exports = {
   // ],
 
   // A map from regular expressions to module names that allow to stub out resources with a single module
-  moduleNameMapper: {
-    '^.+\\.module\\.css$': 'identity-obj-proxy',
-  },
+  // moduleNameMapper: {
+  //   '^.+\\.module\\.css$': 'identity-obj-proxy',
+  // },
 
   // An array of regexp pattern strings, matched against all module paths before considered 'visible' to the module loader
   // modulePathIgnorePatterns: [],

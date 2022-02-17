eeab27e4ad371afc4df8a3384204355169bbc709
Takeichi Kanzaki Cabrera <tkanzakic@hey.com>
2022-02-17 10:17:01 +0100

    [COM-369] Document components creation process (#391)
    * feat: simplify Box component implementation
    - now it just re-export the chakra's counterpart
    
    * feat: simplify Flex component implementation
    - now it just re-export the chakra's counterpart
    
    * feat: simplify Image component implementation
    - now it just re-export the chakra's counterpart
    
    * feat: simplify Divider component implementation
    - now it just re-export the chakra's counterpart
    
    * feat: document components creation process
    
    * fix: improve wording
    
    Co-authored-by: Pau Teruel <93.pau.teruel@gmail.com>
    
    * fix: remove duplicate word
    
    Co-authored-by: Pau Teruel <93.pau.teruel@gmail.com>
    
    * feat: add link to slack channel
    
    * feat: show required node version
    
    * doc: document recommended imports
    
    * doc: link all figma files
    
    * doc: use rem instead of pixel
    
    * doc: generate changeset
    
    * doc: starting storybook
    
    Co-authored-by: Pau Teruel <93.pau.teruel@gmail.com>

--- packages/components/src/components/layouts/Flex/Flex.tsx
+++
@@ -1,5 +0,0 @@
-import React, { FC } from 'react';
-import { Flex as ChakraFlex } from '@chakra-ui/react';
-import { FlexProps } from './types';
-
-export const Flex: FC<FlexProps> = (props) => <ChakraFlex {...props} />;
--- packages/components/src/components/primitives/Divider/types.ts
+++
@@ -1,8 +0,0 @@
-import { DividerProps as ChakraDividerProps } from '@chakra-ui/react';
-
-export const DividerOrientations = ['horizontal', 'vertical'] as const;
-export type DividerOrientation = typeof DividerOrientations[number];
-
-export interface DividerProps extends ChakraDividerProps {
-  orientation?: DividerOrientation;
-}

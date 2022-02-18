99111ddaeb34dd01df0780a4dbdc98f5cf7daf00
Pau Teruel Soldevila <93.pau.teruel@gmail.com>
2022-02-18 13:27:40 +0100

    feat: remove unneeded files

--- packages/components/src/utils/getAlertStatus/getAlertStatus.ts
+++
@@ -1,9 +0,0 @@
-type CmpsrAlertStatus = 'success' | 'error' | 'warning' | 'inform';
-type ChakraAlertStatus = 'success' | 'error' | 'warning' | 'info';
-
-export const getAlertStatus = (status: ChakraAlertStatus): CmpsrAlertStatus =>
-  status === 'info' ? 'inform' : status;
-
-export const getAlertChakraStatus = (
-  status: CmpsrAlertStatus
-): ChakraAlertStatus => (status === 'inform' ? 'info' : status);

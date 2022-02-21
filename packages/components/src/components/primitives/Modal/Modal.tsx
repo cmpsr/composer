df1fe10e2769522933cb43d39ee33a48f9447572
Takeichi Kanzaki Cabrera <tkanzakic@hey.com>
2022-02-21 12:41:35 +0100

    feat: fix modal close button

--- packages/components/src/components/primitives/Modal/Modal.tsx
+++ packages/components/src/components/primitives/Modal/Modal.tsx
@@ -1,4 +1,4 @@
-import React, { FC } from 'react';
+import React, { FC, MouseEvent } from 'react';
 import {
   Modal as ChakraModal,
   ModalOverlay,
@@ -7,14 +7,31 @@ import {
   ModalFooter,
   ModalBody,
   forwardRef,
+  useModalContext,
 } from '@chakra-ui/react';
 import { ModalProps, ModalStaticMembers } from './types';
 import { CloseButton, CloseButtonProps } from '@components';
+import { callAllHandlers } from '@chakra-ui/utils';
 
 export const Modal: FC<ModalProps> & ModalStaticMembers = (props) => <ChakraModal {...props} />;
 
 const ModalCloseButton = forwardRef<CloseButtonProps, typeof CloseButton>((props, ref) => {
-  return <CloseButton position="absolute" top="1.25rem" right="1.25rem" size="m" ref={ref} {...props} />;
+  const { onClick, ...rest } = props;
+  const { onClose } = useModalContext();
+  return (
+    <CloseButton
+      position="absolute"
+      top="1.25rem"
+      right="1.25rem"
+      size="m"
+      ref={ref}
+      onClick={callAllHandlers(onClick, (event: MouseEvent) => {
+        event.stopPropagation();
+        onClose();
+      })}
+      {...rest}
+    />
+  );
 });
 
 Modal.Overlay = ModalOverlay;

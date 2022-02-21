5e94b224d17f62dff208d65c5aa6c1a290144e13
Takeichi Kanzaki Cabrera <tkanzakic@hey.com>
2022-02-21 11:53:06 +0100

    feat: export CloseButton as a forwardRef

--- packages/components/src/components/primitives/CloseButton/CloseButton.tsx
+++ packages/components/src/components/primitives/CloseButton/CloseButton.tsx
@@ -1,12 +1,10 @@
-import React, { FC } from 'react';
-import { CloseButton as ChakraCloseButton } from '@chakra-ui/react';
+import React from 'react';
+import { CloseButton as ChakraCloseButton, forwardRef } from '@chakra-ui/react';
 import { IconX } from '@components';
 import { CloseButtonProps } from './types';
 
-export const CloseButton: FC<CloseButtonProps> = ({ size, ...rest }) => {
-  return (
-    <ChakraCloseButton data-testid="cmpsr.close-button" size={size} {...rest}>
-      <IconX size={size === 's' ? size : 'l'} />
-    </ChakraCloseButton>
-  );
-};
+export const CloseButton = forwardRef<CloseButtonProps, typeof ChakraCloseButton>(({ size, ...rest }, ref) => (
+  <ChakraCloseButton ref={ref} data-testid="cmpsr.close-button" size={size} {...rest}>
+    <IconX size={size === 's' ? size : 'l'} />
+  </ChakraCloseButton>
+));

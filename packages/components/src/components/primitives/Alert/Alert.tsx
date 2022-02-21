32d3fe60856992c62b9e25a378a504e3f2fb85a4
Pau Teruel Soldevila <93.pau.teruel@gmail.com>
2022-02-21 15:55:20 +0100

    feat: refactor alert component to use composition

--- packages/components/src/components/primitives/Alert/Alert.tsx
+++ packages/components/src/components/primitives/Alert/Alert.tsx
@@ -1,89 +1,37 @@
-import React from 'react';
-import { FC } from 'react';
-import { AlertProps, AlertStyles } from './types';
+import React, { FC } from 'react';
+import { AlertProps, AlertStaticMembers, AlertStyles } from './types';
+import { Alert as ChakraAlert, AlertDescription, AlertTitle, useMultiStyleConfig } from '@chakra-ui/react';
 import {
-  Alert as ChakraAlert,
-  AlertDescription,
-  AlertIconProps,
-  AlertTitle,
-  chakra,
-  useMultiStyleConfig,
-  useStyles,
-} from '@chakra-ui/react';
-import { Flex, CloseButton, IconAlertCircle, IconAlertTriangle, IconCircleCheck, IconInfoCircle } from '@components';
+  CloseButton,
+  IconAlertCircle,
+  IconAlertTriangle,
+  IconCircleCheck,
+  IconInfoCircle,
+  CloseButtonProps,
+} from '@components';
 
-export const Alert: FC<AlertProps> = ({
-  state,
-  titleAlignment,
-  variant,
-  title,
-  description,
-  showClose = true,
-  onClose,
-  ...props
-}) => {
-  const calculateTitleAlignment = () => {
-    if (titleAlignment === 'top') {
-      return 'column';
-    }
-
-    return 'row';
+export const Alert: FC<AlertProps> & AlertStaticMembers = ({ status = 'info', children, variant, title, ...props }) => {
+  const { icon } = useMultiStyleConfig('Alert', { variant, status }) as AlertStyles;
+  const ICONS = {
+    info: IconInfoCircle,
+    warning: IconAlertTriangle,
+    success: IconCircleCheck,
+    error: IconAlertCircle,
   };
-
-  const calculateTrailingIconAlignment = () => {
-    if (titleAlignment === 'top') {
-      return {
-        justifyContent: 'flex-start',
-        top: '0.75rem',
-      };
-    }
-
-    return {
-      alignItems: 'center',
-      justifyContent: 'center',
-      top: '0px',
-    };
-  };
-
-  const {
-    icon: { status },
-    closeButton,
-  } = useMultiStyleConfig('Alert', { variant, state }) as AlertStyles;
+  const BaseIcon = ICONS[status];
 
   return (
-    <ChakraAlert status={state} variant={variant} {...props}>
-      <AlertIcon data-testid="cmpsr.alert-icon" status={state || 'info'} color={status[state]?.color || 'info'} />
-      <Flex data-testid="cmpsr.alert-content-wrapper" flexDir={calculateTitleAlignment()}>
-        {title && titleAlignment !== 'none' && <AlertTitle data-testid="cmpsr.alert-title">{title}</AlertTitle>}
-        {description && <AlertDescription>{description}</AlertDescription>}
-      </Flex>
-      <Flex
-        position="absolute"
-        right={variant === 'left-accent' ? '0.5rem' : '0.25rem'}
-        height="100%"
-        flexDirection="column"
-        {...calculateTrailingIconAlignment()}
-      >
-        {showClose && <CloseButton size="s" {...closeButton} />}
-      </Flex>
+    <ChakraAlert status={status} variant={variant} {...props}>
+      <BaseIcon data-testid="cmpsr.alert.icon" size="l" {...icon} color={icon.status[status]?.color} />
+      {children}
     </ChakraAlert>
   );
 };
 
-const STATUSES = {
-  info: { icon: IconInfoCircle, colorScheme: 'blue' },
-  warning: { icon: IconAlertTriangle, colorScheme: 'orange' },
-  success: { icon: IconCircleCheck, colorScheme: 'green' },
-  error: { icon: IconAlertCircle, colorScheme: 'red' },
-};
+const AlertCloseButton: FC<CloseButtonProps> = (props) => (
+  <CloseButton size="s" position="absolute" top="0.75rem" right="0.5rem" {...props} />
+);
 
-const AlertIcon: React.FC<AlertIconProps & { status: string }> = ({ status, ...props }) => {
-  const { icon: BaseIcon } = STATUSES[status];
-  const styles = useStyles();
-
-  return (
-    <chakra.span display="inherit" {...props} className="chakra-alert__icon" __css={styles.icon}>
-      <BaseIcon w="100%" h="100%" />
-    </chakra.span>
-  );
-};
+Alert.Title = AlertTitle;
+Alert.Description = AlertDescription;
+Alert.CloseButton = AlertCloseButton;

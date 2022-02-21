cfb65db34aadc54f83db315f23b5b519b58d5f39
Takeichi Kanzaki Cabrera <tkanzakic@hey.com>
2022-02-21 12:17:28 +0100

    test: remove unnecessary tests

--- packages/components/src/components/primitives/Modal/Modal.test.tsx
+++ packages/components/src/components/primitives/Modal/Modal.test.tsx
@@ -1,59 +1,23 @@
 import React from 'react';
-import { screen, renderWithProviders, fireEvent } from '@tests/renderWithProviders';
+import { screen, renderWithProviders } from '@tests/renderWithProviders';
 import { Modal } from '.';
-import { Button, Text } from '@components';
+import '@testing-library/jest-dom';
 
 describe('Modal', () => {
-  const label = 'Label';
-  const subLabel = 'SubLabel';
-  const mockOnPrimaryClick = jest.fn();
-  const mockOnSecondaryClick = jest.fn();
-  const mockOnClose = jest.fn();
-
-  const givenComponentRendered = ({ isOpen = false }) =>
+  const givenComponentRendered = (isOpen = false) =>
     renderWithProviders(
-      <Modal isOpen={isOpen} size="auto" onClose={mockOnClose}>
-        <Modal.Overlay />
-        <Modal.Content>
-          <Modal.Header>
-            <Text>{label} </Text>
-            <Text>{subLabel} </Text>
-          </Modal.Header>
-          <Modal.CloseButton />
-          <Modal.Body>modal content</Modal.Body>
-          <Modal.Footer>
-            <Button onClick={mockOnPrimaryClick}>Primary CTA</Button>
-            <Button onClick={mockOnSecondaryClick}>Secondary CTA</Button>
-          </Modal.Footer>
-        </Modal.Content>
+      <Modal isOpen={isOpen} size="auto" onClose={jest.fn()}>
+        Children
       </Modal>
     );
 
-  test('should render header', () => {
-    givenComponentRendered({ isOpen: true });
-    screen.getByText(label);
+  test('should render child if is open', () => {
+    givenComponentRendered(true);
+    screen.getByText(/Children/i);
   });
 
-  test('should render body', () => {
-    givenComponentRendered({ isOpen: true });
-    screen.getByText(/modal content/i);
-  });
-
-  test('should render footer actions', () => {
-    givenComponentRendered({ isOpen: true });
-    screen.getByText(/Primary CTA/i);
-    screen.getByText(/Secondary CTA/i);
-  });
-
-  test('should call action when click on primary footer action', () => {
-    givenComponentRendered({ isOpen: true });
-    fireEvent.click(screen.getByText(/Primary CTA/i));
-    expect(mockOnPrimaryClick).toBeCalledTimes(1);
-  });
-
-  test('should call action when click on secondary footer action', () => {
-    givenComponentRendered({ isOpen: true });
-    fireEvent.click(screen.getByText(/Secondary CTA/i));
-    expect(mockOnSecondaryClick).toBeCalledTimes(1);
+  test('should not render child if no open', () => {
+    givenComponentRendered(false);
+    expect(screen.queryByText(/Children/i)).not.toBeInTheDocument();
   });
 });

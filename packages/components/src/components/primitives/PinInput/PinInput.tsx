ca449695f18328ebf4adb235888ae0d83df5457b
TheEnmanuel23 <enmisac@gmail.com>
2022-02-21 13:56:35 -0600

    replace HStack with Flex component

--- packages/components/src/components/primitives/PinInput/PinInput.tsx
+++ packages/components/src/components/primitives/PinInput/PinInput.tsx
@@ -1,17 +1,17 @@
 import React, { FC } from 'react';
-import { HStack, PinInput as ChakraPinInput, PinInputField as ChakraPinInputField } from '@chakra-ui/react';
+import { Flex, PinInput as ChakraPinInput, PinInputField as ChakraPinInputField } from '@chakra-ui/react';
 import { PinInputProps } from './types';
 
 export const PinInput: FC<PinInputProps> = ({ numOfDigits = 4, placeholder = '0', pinInputFieldProps, ...rest }) => {
   const digits = Array.from(Array(numOfDigits).keys());
 
   return (
-    <HStack>
+    <Flex columnGap="0.5rem">
       <ChakraPinInput data-testid="cmpsr.pin-input" placeholder={placeholder} {...rest}>
         {digits.map((digit) => (
           <ChakraPinInputField key={digit} {...pinInputFieldProps} />
         ))}
       </ChakraPinInput>
-    </HStack>
+    </Flex>
   );
 };

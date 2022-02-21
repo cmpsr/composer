393af6c9cb71371454279d1641d4c947dbbbb23a
TheEnmanuel23 <enmisac@gmail.com>
2022-02-21 15:42:34 -0600

    added function to render fields by num of digits

--- packages/components/src/components/primitives/PinInput/PinInput.tsx
+++ packages/components/src/components/primitives/PinInput/PinInput.tsx
@@ -7,6 +7,17 @@ import {
 } from '@chakra-ui/react';
 import { PinInputProps, PinInputStaticMembers } from './types';
 
+const renderFieldsByNumOfDigits: (numOfDigits: number, pinInputFieldProps: PinInputFieldProps) => React.ReactNode[] = (
+  numOfDigits,
+  pinInputFieldProps
+) => {
+  const fields: React.ReactNode[] = [];
+  for (let digit = 0; digit < numOfDigits; digit++) {
+    fields.push(<ChakraPinInputField key={digit} {...pinInputFieldProps} />);
+  }
+  return fields;
+};
+
 export const PinInput: FC<PinInputProps> & PinInputStaticMembers = ({
   children,
   numOfDigits = 4,
@@ -14,12 +25,10 @@ export const PinInput: FC<PinInputProps> & PinInputStaticMembers = ({
   pinInputFieldProps,
   ...rest
 }) => {
-  const digits = Array.from(Array(numOfDigits).keys());
-
   return (
     <Flex justifyContent="center" columnGap="0.5rem">
       <ChakraPinInput data-testid="cmpsr.pin-input" placeholder={placeholder} {...rest}>
-        {children || digits.map((digit) => <ChakraPinInputField key={digit} {...pinInputFieldProps} />)}
+        {children || renderFieldsByNumOfDigits(numOfDigits, pinInputFieldProps)}
       </ChakraPinInput>
     </Flex>
   );

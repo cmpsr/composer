faf4da13affccdf112c4fc25c33d0164605703d2
Pau Teruel Soldevila <93.pau.teruel@gmail.com>
2022-02-22 11:36:16 +0100

    feat: reuse alert variants

--- packages/components/src/components/notifications/Toast/types.ts
+++ packages/components/src/components/notifications/Toast/types.ts
@@ -1,12 +1,11 @@
 import { UseToastOptions as ChakraUseToastOptions } from '@chakra-ui/react';
+import { AlertVariant } from '@components';
 
-export const toastVariants = ['solid', 'subtle', 'left-accent', 'top-accent'] as const;
 export const toastTitleAlignments = ['left', 'top'] as const;
 
-export type ToastVariant = typeof toastVariants[number];
 export type ToastTitleAlignment = typeof toastTitleAlignments[number];
 
 export interface UseToastOptions extends ChakraUseToastOptions {
-  variant?: ToastVariant;
+  variant?: AlertVariant;
   titleAlignment?: ToastTitleAlignment;
 }

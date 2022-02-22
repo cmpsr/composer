1b5960fe50293b77ba99ba00b0ce22a5aa6e9272
Pau Teruel Soldevila <93.pau.teruel@gmail.com>
2022-02-22 14:50:06 +0100

    test: update wrapper with generics

--- packages/components/src/tests/renderHookWithProviders.tsx
+++ packages/components/src/tests/renderHookWithProviders.tsx
@@ -2,5 +2,6 @@ import React from 'react';
 import { renderHook } from '@testing-library/react-hooks';
 import { ComposerProvider } from '..';
 
-export const renderHookWithProviders = (hook) =>
-  renderHook(() => hook(), { wrapper: ({ children }) => <ComposerProvider>{children}</ComposerProvider> });
+export function renderHookWithProviders<TProps, TResult>(callback: (props: TProps) => TResult) {
+  return renderHook(callback, { wrapper: ({ children }) => <ComposerProvider>{children}</ComposerProvider> });
+}

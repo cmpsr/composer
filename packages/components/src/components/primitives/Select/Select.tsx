a56babc3cf09ab7d33aa45280a357795452d7ce5
TheEnmanuel23 <enmisac@gmail.com>
2022-03-02 00:31:11 -0600

    refactor to set text color via theme

--- packages/components/src/components/primitives/Select/Select.tsx
+++ packages/components/src/components/primitives/Select/Select.tsx
@@ -1,33 +1,6 @@
 import React, { FC } from 'react';
-import { Select as ChakraSelect, useMultiStyleConfig } from '@chakra-ui/react';
+import { Select as ChakraSelect } from '@chakra-ui/react';
 import { SelectProps } from './types';
-import { IconChevronDown } from '..';
+import { IconChevronDown } from '@components';
 
-export const Select: FC<SelectProps> = ({ value, placeholder, ...props }) => {
-  const {
-    field: {
-      color,
-      placeholderColor,
-      _disabled: { disabledPlaceHolderColor },
-    },
-  } = useMultiStyleConfig('Select', { value, iconSize: props.iconSize }) as {
-    field: { color: string; placeholderColor: string; _disabled: { disabledPlaceHolderColor: string } };
-  };
-
-  let textColor = props?.isDisabled ? disabledPlaceHolderColor : color;
-
-  if (placeholder && (!value || value === '')) {
-    textColor = props?.isDisabled ? disabledPlaceHolderColor : placeholderColor;
-  }
-
-  return (
-    <ChakraSelect
-      data-testid="cmpsr.select"
-      textColor={textColor}
-      placeholder={placeholder}
-      icon={<IconChevronDown />}
-      value={value}
-      {...props}
-    />
-  );
-};
+export const Select: FC<SelectProps> = (props) => <ChakraSelect icon={<IconChevronDown />} {...props} />;
--- packages/components/src/theme/ComposerTheme/Components/Select.ts
+++ packages/components/src/theme/ComposerTheme/Components/Select.ts
@@ -2,6 +2,11 @@ import { ComponentStyleConfig } from '@chakra-ui/theme';
 import { PartsStyleFunction, PartsStyleInterpolation } from '@chakra-ui/theme-tools';
 import { selectAnatomy as parts } from '@chakra-ui/anatomy';
 
+const placeholderWasSelected = (placeholder, value) => placeholder && (!value || value === '');
+
+const getDefaultTextColor = (placeholder, value) =>
+  placeholderWasSelected(placeholder, value) ? 'text-secondary' : 'text-primary';
+
 const sizes: Record<string, PartsStyleInterpolation> = {
   l: ({ theme }) => ({
     field: {
@@ -35,99 +40,104 @@ const sizes: Record<string, PartsStyleInterpolation> = {
   }),
 };
 
-const baseStyle: PartsStyleFunction<typeof parts> = ({ theme, iconSize }) => {
-  return {
-    icon: {
-      boxSize: '1.25rem',
-      right: '0.75rem',
-      '> svg': {
-        // Icons are styled with inline style, which has highest priority, so !important is required to be able to override them.
-        boxSize: `${theme.components.Icon.sizes[iconSize].boxSize} !important`,
-      },
-      color: 'text-primary',
-      _disabled: {
-        color: 'text-disabled',
-        opacity: 1,
-      },
+const iconPosition = {
+  l: { right: '1rem', },
+  m: { right: '0.75rem', },
+  s: { right: '0.75rem', },
+}
+
+const baseStyle: PartsStyleFunction<typeof parts> = ({ size, value, placeholder }) => ({
+  icon: {
+    boxSize: '1.25rem',
+    ...iconPosition[size],
+    '> svg': {
+      // Icons are styled with inline style, which has highest priority, so !important is required to be able to override them.
+      boxSize: '1.25rem !important',
     },
-    field: {
-      backgroundColor: 'background-action-default',
-      // Using borderColor will not override the border-color set in the theme, as the border property has higher priority
-      border: '0.0625rem solid var(--chakra-colors-ui-element-outline-default)',
-      boxShadow: 'none',
-      color: 'text-primary',
-      placeholderColor: 'text-secondary',
-      borderRadius: '0.375rem',
-      _hover: {
-        backgroundColor: 'background-action-hover',
-        borderWidth: '0.0625rem',
-        borderColor: 'ui-element-outline-active',
-      },
-      _focus: {
-        backgroundColor: 'background-action-hover',
-        borderColor: 'ui-element-outline-default',
-        boxShadow: '0 0 0 0.1875rem var(--chakra-colors-primary-focus)',
-      },
-      _disabled: {
-        backgroundColor: 'background-action-disabled',
-        borderWidth: '0.0625rem',
-        borderColor: 'ui-element-outline-disabled',
-        disabledPlaceHolderColor: 'text-disabled',
-        opacity: 1,
-      },
-      _invalid: {
-        borderColor: 'transparent',
-        boxShadow: '0 0 0 0.125rem var(--chakra-colors-alert-error-default)',
-      },
+    color: 'text-primary',
+    _disabled: {
+      color: 'text-disabled',
+      opacity: 1,
     },
-  };
-};
+  },
+  field: {
+    backgroundColor: 'background-action-default',
+    // Using borderColor will not override the border-color set in the theme, as the border property has higher priority
+    border: '0.0625rem solid var(--chakra-colors-ui-element-outline-default)',
+    boxShadow: 'none',
+    color: getDefaultTextColor(placeholder, value),
+    borderRadius: '0.375rem',
+    _hover: {
+      backgroundColor: 'background-action-hover',
+      borderWidth: '0.0625rem',
+      borderColor: 'ui-element-outline-active',
+    },
+    _focus: {
+      backgroundColor: 'background-action-hover',
+      borderColor: 'ui-element-outline-default',
+      boxShadow: '0 0 0 0.1875rem var(--chakra-colors-primary-focus)',
+    },
+    _disabled: {
+      backgroundColor: 'background-action-disabled',
+      borderWidth: '0.0625rem',
+      borderColor: 'ui-element-outline-disabled',
+      opacity: 1,
+      color: 'text-disabled',
+    },
+    _invalid: {
+      borderColor: 'transparent',
+      boxShadow: '0 0 0 0.125rem var(--chakra-colors-alert-error-default)',
+    },
+  },
+})
 
 const flushedSpacing = {
   l: { paddingInlineStart: '1rem', pr: '2.75rem', pt: '0.625rem' },
   m: { paddingInlineStart: '0.75rem', pr: '2.5rem', py: '0.5rem' },
-  s: { paddingInlineStart: '0.75rem', pr: '2.5rem', },
-}
+  s: { paddingInlineStart: '0.75rem', pr: '2.5rem' },
+};
+
+const flushedStyle: PartsStyleFunction<typeof parts> = ({ size, value, placeholder }) => ({
+  icon: iconPosition[size],
+  field: {
+    bg: 'background-action-default',
+    border: 'none',
+    borderBottom: '0.063rem solid var(--chakra-colors-ui-element-outline-default)',
+    color: getDefaultTextColor(placeholder, value),
+    ...flushedSpacing[size],
+    _hover: {
+      bg: 'background-action-hover',
+      borderBottom: '0.063rem solid var(--chakra-colors-ui-element-outline-active)',
+    },
+    _focus: {
+      border: 'none',
+      borderBottom: '0.063rem solid var(--chakra-colors-ui-element-outline-default)',
+      bg: 'background-action-default',
+      boxShadow: '0 0.1875rem 0 0 var(--chakra-colors-primary-focus)',
+    },
+    _disabled: {
+      backgroundColor: 'background-action-disabled',
+      borderWidth: '0.063rem',
+      borderColor: 'ui-element-outline-disabled',
+      opacity: 1,
+      color: placeholderWasSelected(placeholder, value) ? 'text-disabled' : 'text-secondary',
+    },
+    _invalid: {
+      border: 'none',
+      boxShadow: '0 0.125rem 0 0 var(--chakra-colors-alert-error-default)',
+    },
+  },
+});
 
 export const Select: ComponentStyleConfig = {
   baseStyle,
-  defaultProps: {
-    size: 'm',
-    iconSize: 'm',
-    variant: 'outline',
-    error: false,
-  },
   sizes,
   variants: {
     outline: baseStyle,
-    flushed: ({ size }) => ({
-      field: {
-        bg: 'background-action-default',
-        border: 'none',
-        borderBottom: '0.063rem solid var(--chakra-colors-ui-element-outline-default)',
-        ...flushedSpacing[size],
-        _hover: {
-          bg: 'background-action-hover',
-          borderBottom: '0.063rem solid var(--chakra-colors-ui-element-outline-active)',
-        },
-        _focus: {
-          border: 'none',
-          borderBottom: '0.063rem solid var(--chakra-colors-ui-element-outline-default)',
-          bg: 'background-action-default',
-          boxShadow: '0 0.188rem 0 0 var(--chakra-colors-primary-focus)',
-        },
-        _disabled: {
-          backgroundColor: 'background-action-disabled',
-          borderWidth: '0.063rem',
-          borderColor: 'ui-element-outline-disabled',
-          disabledPlaceHolderColor: 'text-disabled',
-          opacity: 1,
-        },
-        _invalid: {
-          border: 'none',
-          boxShadow: '0 0.125rem 0 0 var(--chakra-colors-alert-error-default)',
-        },
-      }
-    })
+    flushed: flushedStyle,
+  },
+  defaultProps: {
+    size: 'm',
+    variant: 'outline',
   },
 };

8d1b1319aafa34a4c1709ff3c8f22b77e1827060
Enmanuel Jarquín <enmisac@gmail.com>
2022-03-07 07:51:26 -0600

    COM-433: Refactor slider component to use composition (#402)
    * refactor slider component to use composition
    - Styles fixed to match the design
    - Stories updated
    
    * re export types
    
    * rollback in the focus boxshadow to use values in base 16
    
    * match stories to design
    
    * refactor textarea stories to remove conditionals, and set react key to the correct item
    
    * rename variable from sliderOrientations to orientations
    
    * reuse orientations variables in story addons
    
    * feat: improve Slider storybook
    - removed rendering the template in other stories (improve the code listed in the docs)
    - added with mark story
    - added custom thumb story
    
    * feat: simplify Slider implementation
    
    * chore: generate changeset
    
    Co-authored-by: Takeichi Kanzaki Cabrera <tkanzakic@hey.com>

--- packages/components/src/components/primitives/Slider/Slider.tsx
+++ packages/components/src/components/primitives/Slider/Slider.tsx
@@ -1,37 +1,9 @@
-import React, { FC } from 'react';
-import {
-  CSSWithMultiValues,
-  RecursiveCSSObject,
-  Slider as ChakraSlider,
-  SliderFilledTrack,
-  SliderThumb,
-  SliderTrack,
-  useStyleConfig,
-} from '@chakra-ui/react';
+import { Slider, SliderFilledTrack, SliderThumb, SliderTrack, SliderMark } from '@chakra-ui/react';
 
-import { SliderProps } from './types';
-
-export const Slider: FC<SliderProps> = ({
-  filledTrackProps,
-  thumbProps,
-  trackProps,
-  ...props
-}) => {
-  const { sliderBg, _focus, boxSize, bg } = useStyleConfig('Slider') as Record<
-    string,
-    RecursiveCSSObject<CSSWithMultiValues>
-  >;
-
-  return (
-    <ChakraSlider {...props}>
-      <SliderTrack bg={sliderBg as string} {...trackProps}>
-        <SliderFilledTrack bg={bg as string} {...filledTrackProps} />
-      </SliderTrack>
-      <SliderThumb
-        _focus={_focus}
-        boxSize={boxSize as number}
-        {...thumbProps}
-      />
-    </ChakraSlider>
-  );
-};
+const SliderNamespace = Object.assign(Slider, {
+  Track: SliderTrack,
+  FilledTrack: SliderFilledTrack,
+  Thumb: SliderThumb,
+  Mark: SliderMark,
+});
+export { SliderNamespace as Slider };
--- packages/components/src/components/primitives/Slider/types.ts
+++ packages/components/src/components/primitives/Slider/types.ts
@@ -1,13 +1,1 @@
-import {
-  SliderProps as ChakraSliderProps,
-  SliderTrackProps as ChakraSliderTrackProps,
-  SliderThumbProps as ChakraSliderTumbProps,
-  BoxProps,
-} from '@chakra-ui/react';
-
-// SliderFilledTrack props matches with Box props
-export interface SliderProps extends ChakraSliderProps {
-  filledTrackProps?: BoxProps;
-  trackProps?: ChakraSliderTrackProps;
-  thumbProps?: ChakraSliderTumbProps;
-}
+export { SliderProps } from '@chakra-ui/react';

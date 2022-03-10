87842aa76880c721cd1bbe5dc7820992cf20e4fd
Angel Gonzalez <angele.gm91@gmail.com>
2022-03-10 11:13:07 -0300

    fix: format types.ts

--- packages/components/src/components/primitives/Dropdown/types.ts
+++ packages/components/src/components/primitives/Dropdown/types.ts
@@ -1,19 +1,28 @@
-import { FC } from "react";
-import { MenuButtonProps, MenuDividerProps, MenuGroupProps, MenuItemOptionProps, MenuItemProps, MenuListProps, MenuOptionGroupProps } from "@chakra-ui/react";
+import { FC } from 'react';
+import {
+  MenuButtonProps,
+  MenuDividerProps,
+  MenuGroupProps,
+  MenuItemOptionProps,
+  MenuItemProps,
+  MenuListProps,
+  MenuOptionGroupProps,
+} from '@chakra-ui/react';
 
 export {
-    MenuProps as DropdownProps,
-    MenuButtonProps as DropdownButtonProps,
-    MenuItemProps as DropdownItemProps,
-    MenuListProps as DropdownListProps,
-    MenuGroupProps as DropdownGroupProps,
+  MenuProps as DropdownProps,
+  MenuButtonProps as DropdownButtonProps,
+  MenuItemProps as DropdownItemProps,
+  MenuListProps as DropdownListProps,
+  MenuGroupProps as DropdownGroupProps,
 } from '@chakra-ui/react';
+
 export interface DropdownStaticMembers {
-    Group: FC<MenuGroupProps>;
-    Divider: FC<MenuDividerProps>;
-    OptionGroup: FC<MenuOptionGroupProps>;
-    ItemOption: FC<MenuItemOptionProps>;
-    Button: FC<MenuButtonProps>;
-    List: FC<MenuListProps>;
-    Item: FC<MenuItemProps>;
+  Group: FC<MenuGroupProps>;
+  Divider: FC<MenuDividerProps>;
+  OptionGroup: FC<MenuOptionGroupProps>;
+  ItemOption: FC<MenuItemOptionProps>;
+  Button: FC<MenuButtonProps>;
+  List: FC<MenuListProps>;
+  Item: FC<MenuItemProps>;
 }

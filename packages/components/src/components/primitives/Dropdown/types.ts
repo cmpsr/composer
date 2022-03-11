7b101ec6113be6347b68e934171b7769a55c19e4
Angel Gonzalez <41210262+Angele91@users.noreply.github.com>
2022-03-11 13:10:41 -0300

    [COM-464] Implementation of Dropdown component. (#409)
    * COM-464: Implementation of Dropdown component.
    
    * COM-464: Add missing changeset
    
    * fix: PR comments
    
    * fix: dropdown tests
    
    * fix: add missing group export to dropdown
    
    * fix: remove unnecessary code
    
    * feat: adding group, divider, optiongroup and itemoption
    
    * fix: format types.ts
    
    * fix: add missing prop exports
    
    * feat: add new storybook with groups and options

---
+++ packages/components/src/components/primitives/Dropdown/types.ts
@@ -0,0 +1,31 @@
+import { FC } from 'react';
+import {
+  MenuButtonProps,
+  MenuDividerProps,
+  MenuGroupProps,
+  MenuItemOptionProps,
+  MenuItemProps,
+  MenuListProps,
+  MenuOptionGroupProps,
+} from '@chakra-ui/react';
+
+export {
+  MenuProps as DropdownProps,
+  MenuButtonProps as DropdownButtonProps,
+  MenuItemProps as DropdownItemProps,
+  MenuListProps as DropdownListProps,
+  MenuGroupProps as DropdownGroupProps,
+  MenuOptionGroupProps as DropdownOptionGroupProps,
+  MenuItemOptionProps as DropdownItemOptionProps,
+  MenuDividerProps as DropdownDividerProps,
+} from '@chakra-ui/react';
+
+export interface DropdownStaticMembers {
+  Group: FC<MenuGroupProps>;
+  Divider: FC<MenuDividerProps>;
+  OptionGroup: FC<MenuOptionGroupProps>;
+  ItemOption: FC<MenuItemOptionProps>;
+  Button: FC<MenuButtonProps>;
+  List: FC<MenuListProps>;
+  Item: FC<MenuItemProps>;
+}

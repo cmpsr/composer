c07829d7648e35dad5ce1e5d32a1d1399696567a
TheEnmanuel23 <enmisac@gmail.com>
2022-03-12 20:37:36 -0600

    added numberInput playground

--- packages/components/src/components/primitives/NumberInput/NumberInput.stories.tsx
+++ packages/components/src/components/primitives/NumberInput/NumberInput.stories.tsx
@@ -2,7 +2,7 @@ import React, { Fragment } from 'react';
 import { Meta } from '@storybook/react';
 import { Table, Tbody, Td, Th, Thead, Tr } from '@chakra-ui/react';
 import { NumberInput } from '.';
-import { inputSizes, inputVariants } from '../Input/types';
+import { InputSize, inputSizes, InputVariant, inputVariants } from '../Input/types';
 
 export default {
   component: NumberInput,
@@ -18,6 +18,37 @@ export default {
     },
   },
 } as Meta;
+
+const Template = ({
+  variant,
+  size,
+  state,
+  showStepper = true,
+  ...rest
+}: {
+  variant: InputVariant;
+  size: InputSize;
+  state: string;
+  showStepper?: boolean;
+}) => (
+  <NumberInput
+    maxWidth="12.5rem"
+    variant={variant}
+    size={size}
+    isDisabled={['disabled', 'disabled-and-filled'].includes(state)}
+    isInvalid={state === 'error'}
+    defaultValue={['filled', 'disabled-and-filled'].includes(state) && 10}
+    {...rest}
+  >
+    <NumberInput.Field placeholder="Placeholder" />
+    {showStepper && (
+      <NumberInput.Stepper>
+        <NumberInput.IncrementStepper />
+        <NumberInput.DecrementStepper />
+      </NumberInput.Stepper>
+    )}
+  </NumberInput>
+);
 
 const states = ['default', 'filled', 'error', 'disabled', 'disabled-and-filled'];
 
@@ -44,19 +75,7 @@ export const All = () => {
                 <Td>{state}</Td>
                 {inputSizes.map((size, i) => (
                   <Td key={`${variant}-${size}-${i}-${state}`}>
-                    <NumberInput
-                      variant={variant}
-                      size={size}
-                      isDisabled={['disabled', 'disabled-and-filled'].includes(state)}
-                      isInvalid={state === 'error'}
-                      defaultValue={['filled', 'disabled-and-filled'].includes(state) && 10}
-                    >
-                      <NumberInput.Field placeholder="Placeholder" />
-                      <NumberInput.Stepper>
-                        <NumberInput.IncrementStepper />
-                        <NumberInput.DecrementStepper />
-                      </NumberInput.Stepper>
-                    </NumberInput>
+                    <Template variant={variant} size={size} state={state} />
                   </Td>
                 ))}
               </Tr>
@@ -68,26 +87,14 @@ export const All = () => {
   );
 };
 
-// export const All = AllTemplate.bind({});
+export const Playground = Template.bind({});
 
-// const Template = (args) => (
-//   <NumberInput
-//     {...{
-//       ...(args.showLeadingIcon && {
-//         leadingIcon: <IconCloud />,
-//       }),
-//     }}
-//     {...args}
-//   />
-// );
-// export const Playground = Template.bind({});
-// Playground.args = {
-//   variant: 'outline',
-//   size: 'l',
-//   placeholder: 'Placeholder',
-//   leftLabel: '',
-//   rightLabel: '',
-//   isInvalid: false,
-//   isDisabled: false,
-//   showLeadingIcon: true,
-// };
+Playground.args = {
+  variant: 'outline',
+  size: 'l',
+  placeholder: 'Placeholder',
+  isInvalid: false,
+  isDisabled: false,
+  step: 1,
+  showStepper: true,
+};

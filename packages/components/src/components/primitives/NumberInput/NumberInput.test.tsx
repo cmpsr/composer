2767faae3599c7ab1c92a747fba844cb1c19a23e
TheEnmanuel23 <enmisac@gmail.com>
2022-03-12 21:40:23 -0600

    add number input tests

---
+++ packages/components/src/components/primitives/NumberInput/NumberInput.test.tsx
@@ -0,0 +1,24 @@
+import React from 'react';
+import '@testing-library/jest-dom';
+import { screen, renderWithProviders } from '@tests/renderWithProviders';
+import { NumberInput } from '.';
+
+describe('NumberInput', () => {
+  test('it should render', () => {
+    renderWithProviders(
+      <NumberInput data-testid="cmpsr.number-input">
+        <NumberInput.Field data-testid="cmpsr.number-input.field" />
+        <NumberInput.Stepper data-testid="cmpsr.number-input.stepper-group">
+          <NumberInput.IncrementStepper data-testid="cmpsr.number-input.increment-stepper" />
+          <NumberInput.DecrementStepper data-testid="cmpsr.number-input.decrement-stepper" />
+        </NumberInput.Stepper>
+      </NumberInput>
+    );
+
+    expect(screen.queryByTestId('cmpsr.number-input')).toBeInTheDocument();
+    expect(screen.queryByTestId('cmpsr.number-input.field')).toBeInTheDocument();
+    expect(screen.queryByTestId('cmpsr.number-input.stepper-group')).toBeInTheDocument();
+    expect(screen.queryByTestId('cmpsr.number-input.increment-stepper')).toBeInTheDocument();
+    expect(screen.queryByTestId('cmpsr.number-input.decrement-stepper')).toBeInTheDocument();
+  });
+});
--- packages/components/src/components/primitives/NumberInput/types.ts
+++ packages/components/src/components/primitives/NumberInput/types.ts
@@ -2,6 +2,6 @@ import { NumberInputProps as ChackraNumberInputProps } from '@chakra-ui/react';
 import { InputSize, InputVariant } from '../Input/types';
 
 export interface NumberInputProps extends ChackraNumberInputProps {
-  variant: InputVariant;
-  size: InputSize;
+  variant?: InputVariant;
+  size?: InputSize;
 }

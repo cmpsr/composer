0562913770b551257d3c65df4e22d274573b8b04
Enmanuel Jarquín <enmisac@gmail.com>
2022-05-05 01:03:43 -0600

    COM-372: implement inline alert component (#412)
    * add inline alert component and its storybook file
    
    * added unit tests to inline alert component
    
    * changeset file generated
    
    * reuse alert icons object from the alert component
    
    * remove duplicaded style property
    
    * feat: add chakra-ui/react-utils dependency
    
    * feat: refactor inline alert to use composition
    
    Co-authored-by: Takeichi Kanzaki Cabrera <tkanzakic@hey.com>

---
+++ .changeset/curvy-cheetahs-scream.md
@@ -0,0 +1,5 @@
+---
+"@cmpsr/components": minor
+---
+
+Inline Alert component implemented, it includes stories and unit tests
---
+++ packages/components/src/theme/ComposerTheme/Components/InlineAlert.ts
@@ -0,0 +1,35 @@
+import { ComponentMultiStyleConfig } from '@chakra-ui/theme';
+
+const messageColor = {
+  success: {
+    color: 'text-success',
+  },
+  error: {
+    color: 'text-error',
+  },
+  warning: {
+    color: 'text-warning',
+  },
+  info: {
+    color: 'text-secondary',
+  },
+};
+
+export const InlineAlert: ComponentMultiStyleConfig = {
+  parts: ['container', 'description', 'icon'],
+  baseStyle: ({ status, ...rest }) => {
+    return {
+      container: {
+        alignItems: 'center',
+        gap: '0.5rem',
+      },
+      message: {
+        textStyle: 'body-meta-regular',
+        ...messageColor[status],
+      },
+      icon: {
+        ...rest.theme.components.Alert.baseStyle(rest).icon.status[status],
+      },
+    };
+  },
+};

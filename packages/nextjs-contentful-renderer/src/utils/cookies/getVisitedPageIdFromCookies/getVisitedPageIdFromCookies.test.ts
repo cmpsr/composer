61593847dc4151e30eeb1eac9b993e69483408bb
Takeichi Kanzaki Cabrera <tkanzakic@hey.com>
2022-05-11 11:29:36 +0200

    feat: port getVisitedPageIdFromCookies function

---
+++ packages/nextjs-contentful-renderer/src/utils/cookies/getVisitedPageIdFromCookies/getVisitedPageIdFromCookies.test.ts
@@ -0,0 +1,21 @@
+import { getVisitedPageIdFromCookies } from '.';
+
+describe('getVisitedPageIdFromCookies', () => {
+  const fakeContext: any = {
+    req: {
+      cookies: {
+        'x-cmpsr-pages-/home': 'page_id',
+      },
+    },
+  };
+
+  test('should return page id for existing slug', async () => {
+    const pageId = getVisitedPageIdFromCookies(fakeContext, '/home');
+    expect(pageId).toBe('page_id');
+  });
+
+  test('should return undefined for non existing slug', () => {
+    const pageId = getVisitedPageIdFromCookies(fakeContext, '/other/page');
+    expect(pageId).not.toBeDefined();
+  });
+});

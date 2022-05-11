919a04089fc0f3f75dfb5b21dc6df5a56a3b30b9
Takeichi Kanzaki Cabrera <tkanzakic@hey.com>
2022-05-11 11:48:48 +0200

    feat: port getPageId function

---
+++ packages/nextjs-contentful-renderer/src/utils/getPageId/getPageId.test.ts
@@ -0,0 +1,41 @@
+import { getPageId } from '.';
+
+describe('getPageId', () => {
+  const route = {
+    id: 'route_id',
+    variants: [
+      {
+        traffic: 0.7,
+        page: 'page_a_id',
+      },
+      {
+        traffic: 0.3,
+        page: 'page_b_id',
+      },
+      {
+        traffic: 0,
+        page: 'page_campaign_id',
+        utmCampaign: 'campaign',
+      },
+    ],
+  };
+  test('should get variant matching the utmCampaign', () => {
+    const pageId = getPageId(route, 'campaign');
+    expect(pageId).toBe(route.variants[2].page);
+  });
+  test('should get variant matching an item of the utmCampaign', () => {
+    const pageId = getPageId(route, ['campaign']);
+    expect(pageId).toBe(route.variants[2].page);
+  });
+  test.each`
+    randomValue | expectedPage
+    ${0.6}      | ${'page_a_id'}
+    ${0.8}      | ${'page_b_id'}
+    ${1.1}      | ${'page_a_id'}
+  `('should return $expectedPage page id for $randomValue', ({ randomValue, expectedPage }) => {
+    const randomSpy = jest.spyOn(Math, 'random');
+    randomSpy.mockReturnValue(randomValue);
+    const pageId = getPageId(route);
+    expect(pageId).toBe(expectedPage);
+  });
+});

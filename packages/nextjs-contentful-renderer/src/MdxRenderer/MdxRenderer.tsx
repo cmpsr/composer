8e9d3bfdfe20ce549c9ddc61d45ae54596efdd60
Takeichi Kanzaki Cabrera <tkanzakic@hey.com>
2022-05-18 14:21:37 +0200

    fix: explicitly pass children

--- packages/nextjs-contentful-renderer/src/MdxRenderer/MdxRenderer.tsx
+++ packages/nextjs-contentful-renderer/src/MdxRenderer/MdxRenderer.tsx
@@ -4,6 +4,22 @@ import { getMDXComponent } from 'mdx-bundler/client';
 import { Paragraph, Text } from './components';
 import { MdxRendererProps } from './types';
 
+const renderHeader = ({ children, ...props }: Composer.TextProps, as: 'h1' | 'h2' | 'h3' | 'h4' | 'h5' | 'h6') => {
+  const variants = {
+    h1: 'text-header-4XL' as const,
+    h2: 'text-header-3XL' as const,
+    h3: 'text-header-2XL' as const,
+    h4: 'text-header-XL' as const,
+    h5: 'text-header-L' as const,
+    h6: 'text-header-M' as const,
+  };
+  return (
+    <Text as={as} variant={variants[as]} {...props}>
+      {children}
+    </Text>
+  );
+};
+
 const components: any = Object.keys(Composer).reduce(
   (acc, key) => {
     const descriptor = Reflect.getOwnPropertyDescriptor(Composer, key);
@@ -18,12 +34,12 @@ const components: any = Object.keys(Composer).reduce(
   },
   {
     p: Paragraph,
-    h1: (props: Composer.TextProps) => <Text as="h1" variant="text-header-4XL" {...props} />,
-    h2: (props: Composer.TextProps) => <Text as="h2" variant="text-header-3XL" {...props} />,
-    h3: (props: Composer.TextProps) => <Text as="h3" variant="text-header-2XL" {...props} />,
-    h4: (props: Composer.TextProps) => <Text as="h4" variant="text-header-XL" {...props} />,
-    h5: (props: Composer.TextProps) => <Text as="h5" variant="text-header-L" {...props} />,
-    h6: (props: Composer.TextProps) => <Text as="h6" variant="text-header-M" {...props} />,
+    h1: (props: Composer.TextProps) => renderHeader(props, 'h1'),
+    h2: (props: Composer.TextProps) => renderHeader(props, 'h2'),
+    h3: (props: Composer.TextProps) => renderHeader(props, 'h3'),
+    h4: (props: Composer.TextProps) => renderHeader(props, 'h4'),
+    h5: (props: Composer.TextProps) => renderHeader(props, 'h5'),
+    h6: (props: Composer.TextProps) => renderHeader(props, 'h6'),
     a: (props: Composer.LinkProps) => <Composer.Link display="inline-flex" {...props} />,
     img: Composer.Image,
   }

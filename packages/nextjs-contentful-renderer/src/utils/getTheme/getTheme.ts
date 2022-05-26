9478a797f21553c657cf70a1898ecfb5dc819883
Takeichi Kanzaki Cabrera <tkanzakic@hey.com>
2022-05-26 09:37:28 +0200

    feat: remove current getTheme impl

--- packages/nextjs-contentful-renderer/src/utils/getTheme/getTheme.ts
+++
@@ -1,7 +0,0 @@
-import { getThemeByPageId } from 'src/utils/contentful/getThemeByPageId';
-import { getApolloClient } from 'src/utils/getApolloClient';
-
-export const getTheme = (pageId: string, preview: boolean) => {
-  const apolloClient = getApolloClient({ preview });
-  return getThemeByPageId(apolloClient, pageId, preview);
-};

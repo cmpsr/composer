b8f6d92007ef02be771c29a5b2e27c043036c32d
TheEnmanuel23 <enmisac@gmail.com>
2022-05-29 23:51:00 -0600

    fix lint errors

--- packages/nextjs-contentful-renderer/src/configNavbar/configNavbar.test.ts
+++ packages/nextjs-contentful-renderer/src/configNavbar/configNavbar.test.ts
@@ -17,18 +17,27 @@ const dummyPageContent = {
 };
 
 describe('setDefaultNavbar', () => {
+  const mockApolloClient: any = {
+    query: jest.fn(),
+  };
+  const mockQueryConfig = {
+    apolloClient: mockApolloClient,
+    domain: '',
+    preview: true,
+  };
+
   test('should return null if there is no default navbar', async () => {
-    const newContent = await configNavbar({ content: null }, { apolloClient: {} });
+    const newContent = await configNavbar({ content: null }, mockQueryConfig);
     expect(newContent).toBeNull();
   });
   test('should return original content if there is no default navbar', async () => {
-    const newContent = await configNavbar(dummyPageContent, { apolloClient: {} });
+    const newContent = await configNavbar(dummyPageContent, mockQueryConfig);
     expect(newContent).toStrictEqual(dummyContent);
   });
 
   test('should return an array with navbar if it has a navbar but content is null', async () => {
     const navbar = { model: { base: '- opt 1' } };
-    const newContent = await configNavbar({ content: null, navbar }, { apolloClient: {} });
+    const newContent = await configNavbar({ content: null, navbar }, mockQueryConfig);
     expect(newContent).toStrictEqual([
       {
         models: [{ base: '- opt 1' }],
@@ -39,7 +48,7 @@ describe('setDefaultNavbar', () => {
 
   test('should concat navbar to content', async () => {
     const navbar = { model: { base: '- opt 1' } };
-    const newContent = await configNavbar({ content: dummyContent, navbar }, { apolloClient: {} });
+    const newContent = await configNavbar({ content: dummyContent, navbar }, mockQueryConfig);
     expect(newContent).toStrictEqual([
       {
         models: [{ base: '- opt 1' }],
@@ -52,7 +61,7 @@ describe('setDefaultNavbar', () => {
   test('should concat defaultNavbar to content', async () => {
     const defaultNavbar = { model: { base: '- opt 1' } };
     mockGetDefaultNavbar.mockResolvedValueOnce(defaultNavbar);
-    const newContent = await configNavbar({ content: dummyContent }, { apolloClient: {} });
+    const newContent = await configNavbar({ content: dummyContent }, mockQueryConfig);
     expect(newContent).toStrictEqual([
       {
         models: [{ base: '- opt 1' }],
@@ -65,7 +74,7 @@ describe('setDefaultNavbar', () => {
   test('should return defaultNavbar if there is neither content nor navbar', async () => {
     const defaultNavbar = { model: { base: '- opt 1' } };
     mockGetDefaultNavbar.mockResolvedValueOnce(defaultNavbar);
-    const newContent = await configNavbar({ content: null }, { apolloClient: {} });
+    const newContent = await configNavbar({ content: null }, mockQueryConfig);
     expect(newContent).toStrictEqual([
       {
         models: [{ base: '- opt 1' }],

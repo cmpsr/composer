0fcb3c90a988b2cbd0f7db57269a0cbedd50e31f
Pau Teruel Soldevila <93.pau.teruel@gmail.com>
2022-05-30 10:27:59 +0200

    feat: remove unneeded testing tools

--- packages/blocks/src/config/jest/assetTransformer.test.ts
+++
@@ -1,9 +0,0 @@
-import { assetTransformer } from './assetTransformer';
-
-describe('assetTransformer', () => {
-  it('should transform assets', () => {
-    const transformed = assetTransformer.process('https://www.example.com/image.png');
-
-    expect(transformed).toEqual('module.exports = "image.png";');
-  });
-});
--- packages/blocks/src/tests/renderHookWithProviders.tsx
+++
@@ -1,7 +0,0 @@
-import React from 'react';
-import { renderHook } from '@testing-library/react-hooks';
-import { ComposerProvider } from '@cmpsr/components';
-
-export function renderHookWithProviders<TProps, TResult>(callback: (props: TProps) => TResult) {
-  return renderHook(callback, { wrapper: ({ children }) => <ComposerProvider>{children}</ComposerProvider> });
-}

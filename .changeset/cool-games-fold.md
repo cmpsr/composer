465da90ff6211a5001f7711c71c688dfe9286d4a
Pau Teruel <93.pau.teruel@gmail.com>
2022-05-31 13:15:51 +0200

    feat: create usScrollPosition hook (#465)
    * feat: create usScrollPosition hook
    
    * feat: create changeset
    
    * feat: reexport hook from parent export
    
    * fix: use object define property to override pageyOffset

---
+++ .changeset/cool-games-fold.md
@@ -0,0 +1,5 @@
+---
+"@cmpsr/components": minor
+---
+
+feat: add new hook useScrollPosition
--- packages/components/src/hooks/index.ts
+++ packages/components/src/hooks/index.ts
@@ -1,3 +1,4 @@
 export { useBreakpointValue, useDisclosure } from '@chakra-ui/react';
 export { createBreakpoints } from '@chakra-ui/theme-tools';
 export * from './useResponsiveValue';
+export * from './useScrollPosition';

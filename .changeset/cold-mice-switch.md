ae4b7781e3453868885c114a9518cf9267c8ad7b
Pau Teruel <93.pau.teruel@gmail.com>
2022-06-03 11:09:42 +0200

    Add textAlign property to TextPairing (#472)
    * feat: allow text align for text pairing
    
    * chore: create changeset
    
    * feat: add text alignments options to const/type and use select control on storybook

---
+++ .changeset/cold-mice-switch.md
@@ -0,0 +1,5 @@
+---
+"@cmpsr/components": minor
+---
+
+feat: add text align property to TextPairing
--- packages/components/src/components/primitives/TextPairing/TextPairing.stories.tsx
+++ packages/components/src/components/primitives/TextPairing/TextPairing.stories.tsx
@@ -1,6 +1,6 @@
 import React from 'react';
 import { Meta } from '@storybook/react';
-import { TextPairing, TextPairingProps, textPairingVariants } from '.';
+import { TextPairing, TextPairingProps, textPairingVariants, textPairingTextAlignments } from '.';
 import { Text } from '@components';
 import { Box, Flex, Spacer, Stack, StackDivider } from '@chakra-ui/layout';
 import { defaultColors } from '@theme/ComposerTheme/Colors';
@@ -21,10 +21,14 @@ export default {
       options: defaultColors,
       control: { type: 'select' },
     },
+    textAlign: {
+      options: textPairingTextAlignments,
+      control: { type: 'select' },
+    },
   },
 } as Meta;
 
-const AllTemplate = ({ label, subLabel }: TextPairingProps) => (
+const AllTemplate = ({ label, subLabel, textAlign }: TextPairingProps) => (
   <Stack spacing="2rem" py="2rem" divider={<StackDivider borderColor="#888" />}>
     {textPairingVariants.map((variant) => (
       <Flex key={variant} px="1rem">
@@ -33,7 +37,7 @@ const AllTemplate = ({ label, subLabel }: TextPairingProps) => (
         </Box>
         <Spacer />
         <Box>
-          <TextPairing variant={variant} label={label} subLabel={subLabel} />
+          <TextPairing variant={variant} label={label} subLabel={subLabel} textAlign={textAlign} />
         </Box>
       </Flex>
     ))}

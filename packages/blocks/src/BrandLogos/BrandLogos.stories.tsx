49fe2c2bfa27b8da6dcfc43b60be5fd8f7fe2b87
Pau Teruel <93.pau.teruel@gmail.com>
2022-06-15 14:19:58 +0200

    [COM-586] Footer Block (#488)
    * feat: create footer block
    
    * chore: create changeset
    
    * docs: update stories to show scode in canvas
    
    * Update packages/blocks/src/Hero/Hero.stories.tsx
    
    Co-authored-by: Takeichi Kanzaki Cabrera <tkanzakic@hey.com>

--- packages/blocks/src/BrandLogos/BrandLogos.stories.tsx
+++ packages/blocks/src/BrandLogos/BrandLogos.stories.tsx
@@ -8,33 +8,37 @@ export default {
   title: 'Blocks/BrandLogos',
 } as Meta;
 
-const Template = (args) => <BrandLogos {...args} />;
+export const WithText = () => (
+  <BrandLogos
+    backgroundColor={'background-page'}
+    logos={Array(8).fill('https://avatars0.githubusercontent.com/u/67131017?s=200')}
+    logosMaxHeight="12.5rem"
+    textProps={{ children: 'Title', variant: 'text-header-2XL' }}
+  />
+);
 
-const defaultArgs = {
-  logos: Array(8).fill('https://avatars0.githubusercontent.com/u/67131017?s=200'),
-  backgroundColor: 'background-page',
-  logosMaxHeight: '12.5rem',
-};
+export const WithTextPairing = () => (
+  <BrandLogos
+    backgroundColor={'background-page'}
+    logos={Array(8).fill('https://avatars0.githubusercontent.com/u/67131017?s=200')}
+    logosMaxHeight="12.5rem"
+    textProps={{
+      labelProps: { children: 'Title' },
+      subLabelProps: { children: 'Description' },
+      variant: 'textpairing-header-2XL',
+    }}
+  />
+);
 
-export const WithText = Template.bind({});
-WithText.args = {
-  ...defaultArgs,
-  textProps: { children: 'Title', variant: 'text-header-2XL' },
-};
-
-export const WithTextPairing = Template.bind({});
-WithTextPairing.args = {
-  ...defaultArgs,
-  textProps: {
-    labelProps: { children: 'Title' },
-    subLabelProps: { children: 'Description' },
-    variant: 'textpairing-header-2XL',
-  },
-};
+const Template = ({ logos, backgroundColor, logosMaxHeight, textProps }) => (
+  <BrandLogos logos={logos} backgroundColor={backgroundColor} logosMaxHeight={logosMaxHeight} textProps={textProps} />
+);
 
 export const Playground = Template.bind({});
 Playground.args = {
-  ...defaultArgs,
+  logos: Array(8).fill('https://avatars0.githubusercontent.com/u/67131017?s=200'),
+  backgroundColor: 'background-page',
+  logosMaxHeight: '12.5rem',
   textProps: {
     labelProps: { children: 'Title' },
     subLabelProps: { children: 'Description' },

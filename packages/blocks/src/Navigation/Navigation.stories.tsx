b32c581e6794b5099f3ebf70755e86bb1525e8b6
Takeichi Kanzaki Cabrera <tkanzakic@hey.com>
2022-07-28 13:41:44 +0200

    [COM-717] Change Navigation block to use dot notation (#549)
    * feat: change Navigation component to use dot notation
    
    * chore: generate changeset

--- packages/blocks/src/Navigation/Navigation.stories.tsx
+++ packages/blocks/src/Navigation/Navigation.stories.tsx
@@ -1,6 +1,6 @@
 import React from 'react';
 import { Meta } from '@storybook/react';
-import { Flex, Button, useBreakpointValue, ButtonSize, Text } from '@cmpsr/components';
+import { Flex, Text } from '@cmpsr/components';
 
 import { Navigation } from './Navigation';
 
@@ -10,83 +10,77 @@ export default {
   parameters: { layout: 'fullscreen' },
 } as Meta;
 
-export const Sticky = () => {
-  const buttonSize = useBreakpointValue({ base: 'l', lg: 'm' }) as ButtonSize;
-  return (
-    <>
-      <Navigation
-        anchors={Array(5).fill({ label: 'Default Link', href: '#' })}
-        logoUrl={'https://avatars0.githubusercontent.com/u/67131017?s=200'}
-        logoHref="#"
-        sticky
-        actions={
-          <Flex
-            flexDirection={{ base: 'column', md: 'row' }}
-            gap={{ base: '1.5rem', lg: '0.75rem' }}
-            pt={{ base: '7rem', md: '5.75rem', lg: 0 }}
-          >
-            <Button variant="primary" size={buttonSize}>
-              Default
-            </Button>
-            <Button variant="primary-alt" size={buttonSize}>
-              Default
-            </Button>
-          </Flex>
-        }
-      />
-      <Flex maxWidth="48rem" flexDirection="column" margin="0 auto" gap="1rem" p="spacer-12">
-        {Array(20).fill(
-          <Text variant="text-body-regular">
+export const Sticky = () => (
+  <>
+    <Navigation sticky>
+      <Navigation.Logo src="https://avatars0.githubusercontent.com/u/67131017?s=200" href="#" />
+      <Navigation.Links>
+        <Navigation.Link href="#" showDivider>
+          Default Link
+        </Navigation.Link>
+        <Navigation.Link href="#" showDivider>
+          Default Link
+        </Navigation.Link>
+        <Navigation.Link href="#" showDivider>
+          Default Link
+        </Navigation.Link>
+        <Navigation.Link href="#" showDivider>
+          Default Link
+        </Navigation.Link>
+        <Navigation.Link href="#">Default Link</Navigation.Link>
+      </Navigation.Links>
+      <Navigation.Actions>
+        <Navigation.Action variant="primary">Default</Navigation.Action>
+        <Navigation.Action variant="primary-alt">Default</Navigation.Action>
+      </Navigation.Actions>
+    </Navigation>
+    <Flex maxWidth="48rem" flexDirection="column" margin="0 auto" gap="1rem" p="spacer-12">
+      {Array(20)
+        .fill(0)
+        .map((_, index) => (
+          <Text key={index} variant="text-body-regular">
             Lorem ipsum dolor sit amet, consectetur adipiscing elit. Aenean volutpat velit ut mollis malesuada. Integer
             molestie in dolor cursus tristique. Nullam est tortor, dapibus sed sem eget, pellentesque vehicula felis.
             Vestibulum sit amet dui lorem. Nam enim magna, rutrum et turpis a, sollicitudin vehicula orci. Pellentesque
             dapibus pulvinar sapien, eu mattis lorem eleifend sed. Nullam volutpat ligula vitae bibendum pharetra.
             Phasellus pharetra vel eros vel lacinia.
           </Text>
-        )}
-      </Flex>
-    </>
-  );
-};
+        ))}
+    </Flex>
+  </>
+);
 
-const Template = ({ anchors, logoUrl, logoHref, sticky }) => {
-  const buttonSize = useBreakpointValue({ base: 'l', lg: 'm' }) as ButtonSize;
-  return (
-    <>
-      <Navigation
-        anchors={anchors}
-        logoUrl={logoUrl}
-        logoHref={logoHref}
-        sticky={sticky}
-        actions={
-          <Flex
-            flexDirection={{ base: 'column', md: 'row' }}
-            gap={{ base: '1.5rem', lg: '0.75rem' }}
-            pt={{ base: '7rem', md: '5.75rem', lg: 0 }}
-          >
-            <Button variant="primary" size={buttonSize}>
-              Default
-            </Button>
-            <Button variant="primary-alt" size={buttonSize}>
-              Default
-            </Button>
-          </Flex>
-        }
-      />
-      <Flex maxWidth="48rem" flexDirection="column" margin="0 auto" gap="1rem" p="spacer-12">
-        {Array(20).fill(
-          <Text variant="text-body-regular">
+const Template = ({ anchors, logoUrl, logoHref, sticky }) => (
+  <>
+    <Navigation sticky={sticky}>
+      <Navigation.Logo src={logoUrl} href={logoHref} />
+      <Navigation.Links>
+        {anchors.map(({ label, href }, index: number) => (
+          <Navigation.Link key={index} href={href}>
+            {label}
+          </Navigation.Link>
+        ))}
+      </Navigation.Links>
+      <Navigation.Actions>
+        <Navigation.Action variant="primary">Default</Navigation.Action>
+        <Navigation.Action variant="primary-alt">Default</Navigation.Action>
+      </Navigation.Actions>
+    </Navigation>
+    <Flex maxWidth="48rem" flexDirection="column" margin="0 auto" gap="1rem" p="spacer-12">
+      {Array(20)
+        .fill(0)
+        .map((_, index) => (
+          <Text key={index} variant="text-body-regular">
             Lorem ipsum dolor sit amet, consectetur adipiscing elit. Aenean volutpat velit ut mollis malesuada. Integer
             molestie in dolor cursus tristique. Nullam est tortor, dapibus sed sem eget, pellentesque vehicula felis.
             Vestibulum sit amet dui lorem. Nam enim magna, rutrum et turpis a, sollicitudin vehicula orci. Pellentesque
             dapibus pulvinar sapien, eu mattis lorem eleifend sed. Nullam volutpat ligula vitae bibendum pharetra.
             Phasellus pharetra vel eros vel lacinia.
           </Text>
-        )}
-      </Flex>
-    </>
-  );
-};
+        ))}
+    </Flex>
+  </>
+);
 
 export const Playground = Template.bind({});
 Playground.args = {

2266f1c9b3cc80192457f63e006d35a8262be7e4
Takeichi Kanzaki Cabrera <tkanzakic@hey.com>
2022-07-29 10:52:36 +0200

    [COM-719] Change Footer to use dot notation (#550)
    * feat: extract Footer.Logo
    
    * feat: extract Footer.Bottom
    
    * feat: extract Footer.Content
    - Footer.Content.Links
    - Footer.Content.LinkGroup
    - Footer.Content.LinkGroup.Title
    - Footer.Content.LinkGroup.Link
    
    * feat: extract Footer.CopyGroup
    
    * chore: generate changeset
    
    * fix: linter issues

--- packages/blocks/src/Footer/Footer.tsx
+++ packages/blocks/src/Footer/Footer.tsx
@@ -1,38 +1,55 @@
 import React, { FC } from 'react';
-import { Divider, Flex, Image, Link, Text } from '@cmpsr/components';
+import {
+  Divider,
+  DividerProps,
+  Flex,
+  FlexProps,
+  Image,
+  ImageProps,
+  Link,
+  LinkProps,
+  Text,
+  TextProps,
+} from '@cmpsr/components';
 
-import { FooterProps } from './types';
+import { FooterContentStaticMembers, FooterLinkGroupStaticMember, FooterProps, FooterStaticMembers } from './types';
 
-export const Footer: FC<FooterProps> = ({
-  imageProps,
-  linkGroups,
-  copyGroup,
-  bottomContent,
-  dividerProps,
-  ...props
-}) => (
-  <Flex maxWidth="66.75rem" flexDirection="column" width="100%" {...props}>
-    <Image alt="Footer image" width="100%" mb={{ base: '2.75rem', md: '3rem' }} {...imageProps} />
-    <Flex flexDirection={{ base: 'column', lg: 'row' }} gap={{ base: '2.75rem', md: '3rem' }}>
-      <Flex gap={{ base: '2.75rem', lg: '1.5rem' }} flexDirection={{ base: 'column', md: 'row' }} flex={{ lg: 1 }}>
-        {linkGroups?.map(({ title, items }, i) => (
-          <Flex flexDirection="column" gap="1.25rem" flex={1} key={i}>
-            <Text variant="text-body-bold">{title}</Text>
-            <Flex flexDirection="column" gap="0.75rem" alignItems="start">
-              {items?.map((item, i) => (
-                <Link key={i} size="m" color="text-link-secondary-default" {...item} />
-              ))}
-            </Flex>
-          </Flex>
-        ))}
-      </Flex>
-      {copyGroup}
-    </Flex>
-    {bottomContent && (
-      <>
-        <Divider width="100%" my={{ base: '2.75rem', md: '4rem' }} {...dividerProps} />
-        {bottomContent}
-      </>
-    )}
-  </Flex>
+export const Footer: FC<FooterProps> & FooterStaticMembers = (props) => (
+  <Flex maxWidth="66.75rem" flexDirection="column" width="100%" {...props} />
 );
+
+const FooterLogo: FC<ImageProps> = (props) => (
+  <Image alt="Footer image" width="100%" mb={{ base: '2.75rem', md: '3rem' }} {...props} />
+);
+Footer.Logo = FooterLogo;
+
+const FooterLinkGroupTitle: FC<TextProps> = (props) => <Text variant="text-body-bold" mb="0.5rem" {...props} />;
+const FooterLinkGroupLink: FC<LinkProps> = (props) => <Link size="m" color="text-link-secondary-default" {...props} />;
+const FooterContentLinkGroup: FC<FlexProps> & FooterLinkGroupStaticMember = (props) => (
+  <Flex flexDirection="column" gap="0.75rem" flex={1} {...props} />
+);
+const FooterContentLinks: FC<FlexProps> = (props) => (
+  <Flex
+    gap={{ base: '2.75rem', lg: '1.5rem' }}
+    flexDirection={{ base: 'column', md: 'row' }}
+    flex={{ lg: 1 }}
+    {...props}
+  />
+);
+FooterContentLinkGroup.Title = FooterLinkGroupTitle;
+FooterContentLinkGroup.Link = FooterLinkGroupLink;
+const FooterContent: FC<FlexProps> & FooterContentStaticMembers = (props) => (
+  <Flex flexDirection={{ base: 'column', lg: 'row' }} gap={{ base: '2.75rem', md: '3rem' }} {...props} />
+);
+FooterContent.Links = FooterContentLinks;
+FooterContent.LinkGroup = FooterContentLinkGroup;
+FooterContent.CopyGroup = Flex;
+Footer.Content = FooterContent;
+
+const FooterBottom: FC<DividerProps> = ({ children, ...props }) => (
+  <>
+    <Divider width="100%" my={{ base: '2.75rem', md: '4rem' }} {...props} />
+    {children}
+  </>
+);
+Footer.Bottom = FooterBottom;

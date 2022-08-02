50131253155d58298926e09b0818283404a73f34
Takeichi Kanzaki Cabrera <me@tkanzakic.com>
2022-08-02 12:19:35 +0200

    feat: use a Context instead of cloning for Alert Icon - useAlertStyles instead of the deprecated useStyles

--- packages/components/src/components/notifications/Alert/Alert.tsx
+++ packages/components/src/components/notifications/Alert/Alert.tsx
@@ -1,6 +1,7 @@
 import React, { FC } from 'react';
-import { AlertProps, AlertStaticMembers, AlertIconStyles, AlertIconProps } from './types';
-import { Alert as ChakraAlert, AlertDescription, AlertTitle, useStyles } from '@chakra-ui/react';
+import { AlertProps, AlertStaticMembers, AlertIconStyles, AlertStatus } from './types';
+import { Alert as ChakraAlert, AlertDescription, AlertTitle, useAlertStyles } from '@chakra-ui/react';
+import { createContext } from '@chakra-ui/react-utils';
 import {
   CloseButton,
   IconAlertCircle,
@@ -17,21 +18,29 @@ export const ALERT_ICONS = {
   error: IconAlertCircle,
 };
 
-export const Alert: FC<AlertProps> & AlertStaticMembers = ({ status = 'info', children, variant, ...props }) => {
+interface AlertContext {
+  status: AlertStatus;
+}
+
+const [AlertProvider, useAlertContext] = createContext<AlertContext>({
+  name: 'AlertContext',
+  errorMessage: 'useAlertContext: `context` is undefined. Seems you forgot to wrap alert components in `<Alert />`',
+});
+
+export const Alert: FC<AlertProps> & AlertStaticMembers = ({ status = 'info', ...props }) => {
   return (
-    <ChakraAlert status={status} variant={variant} {...props}>
-      {React.Children.map(children, (child) => {
-        if (React.isValidElement(child) && child.type === Alert.Icon) {
-          return React.cloneElement(child, { status });
-        } else return child;
-      })}
-    </ChakraAlert>
+    <AlertProvider value={{ status }}>
+      <ChakraAlert status={status} {...props} />
+    </AlertProvider>
   );
 };
 
-const Icon = ({ status }: AlertIconProps) => {
-  const { icon } = useStyles() as AlertIconStyles;
+const Icon = () => {
+  const { icon } = useAlertStyles() as AlertIconStyles;
+  const { status } = useAlertContext();
   const BaseIcon = ALERT_ICONS[status];
+
+  console.log(useAlertStyles());
 
   return <BaseIcon data-testid="cmpsr.alert.icon" size="l" {...icon} color={icon.status[status]?.color} />;
 };

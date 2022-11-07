029d5a728b07af81eaac4290dcd8a09e495ff9f1
Pau Teruel <93.pau.teruel@gmail.com>
2022-11-07 18:21:21 +0100

    [COM-872] Replace ENV_VAR field type with process.env (#586)
    * feat: replace fieldType ENV_VAR with process environment
    
    * chore: create changeset
    
    * chore: remove console log

--- packages/nextjs-contentful-renderer/src/generateMdx/generateMdx.ts
+++ packages/nextjs-contentful-renderer/src/generateMdx/generateMdx.ts
@@ -113,6 +113,10 @@ const replacePropValues = (mdx: string, values: Record<string, string> = {}): st
       case 'list':
         searchValue = listPattern ? escapeCharactersInListPattern(match) : match;
         break;
+      case 'ENV_VAR':
+        searchValue = match;
+        newValue = process.env[propName];
+        break;
       default:
         searchValue = match;
     }

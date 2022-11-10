4eaf23aa4b272b3751e0f122bfee63aaa6d62bf6
Takeichi Kanzaki Cabrera <me@tkanzakic.com>
2022-11-10 15:12:00 +0100

    Add default value to flexWrap (#592)
    * feat: add default flex-wrap to ColumnLayout
    
    * chore: generate changeset
    
    * feat: remove unnecessary flexWrap and gap

--- packages/blocks/src/ColumnLayout/ColumnLayout.tsx
+++ packages/blocks/src/ColumnLayout/ColumnLayout.tsx
@@ -1,7 +1,7 @@
 import React, { FC } from 'react';
-import { Flex, Link, Text, TextPairing, TextProps } from '@cmpsr/components';
+import { Flex, FlexProps, Link, Text, TextPairing, TextProps } from '@cmpsr/components';
 import { ColumnLayoutType } from './types';
-import { MediaBlock } from '../';
+import { MediaBlock } from '../MediaBlock';
 
 export const ColumnLayout: ColumnLayoutType = (props) => <Flex flexDirection="column" {...props} />;
 
@@ -9,7 +9,7 @@ const ColumnLayoutOverline: FC<TextProps> = (props) => (
   <Text color="text-secondary" variant="text-body-display-S" textTransform="uppercase" {...props} />
 );
 
-ColumnLayout.ColumnsContainer = Flex;
+ColumnLayout.ColumnsContainer = (props: FlexProps) => <Flex flexWrap="wrap" gap="1rem" {...props} />;
 ColumnLayout.Column = MediaBlock;
 ColumnLayout.Title = TextPairing;
 ColumnLayout.Action = Link;

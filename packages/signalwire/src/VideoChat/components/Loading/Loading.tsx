b8412e1f00140b9f524091a30078826ed30129b6
Takeichi Kanzaki Cabrera <me@tkanzakic.com>
2022-11-21 14:29:28 +0100

    feat: import Loading component

---
+++ packages/signalwire/src/VideoChat/components/Loading/Loading.tsx
@@ -0,0 +1,7 @@
+import React, { Flex, Spinner } from '@cmpsr/components';
+
+export const Loading = () => (
+  <Flex justifyContent="center" alignItems="center" backgroundColor="background-dark" width="100%" height="100vh">
+    <Spinner size="s" />
+  </Flex>
+);

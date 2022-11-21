20232a63fc04ce33f6a418f26d28375b5323fa02
Takeichi Kanzaki Cabrera <me@tkanzakic.com>
2022-11-21 15:18:52 +0100

    fix: linter issues

--- packages/signalwire/src/VideoChat/components/MembersList/types.ts
+++ packages/signalwire/src/VideoChat/components/MembersList/types.ts
@@ -1,7 +1,7 @@
-import { VideoMemberEntity } from "@signalwire/js"
+import { VideoMemberEntity } from '@signalwire/js';
 
 export type MembersListProps = {
-    members: VideoMemberEntity[]
-    ownUserName?: string;
-    ownMemberId?: string;
-}
+  members: VideoMemberEntity[];
+  ownUserName?: string;
+  ownMemberId?: string;
+};

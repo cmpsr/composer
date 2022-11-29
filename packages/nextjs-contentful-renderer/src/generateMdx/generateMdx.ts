78867ed20d4fc2e3a39cc9c35e00786c587ce5d9
Takeichi Kanzaki Cabrera <me@tkanzakic.com>
2022-11-29 15:51:36 +0100

    fix: properly handle multiple stylings in the same content

--- packages/nextjs-contentful-renderer/src/generateMdx/generateMdx.ts
+++ packages/nextjs-contentful-renderer/src/generateMdx/generateMdx.ts
@@ -62,7 +62,8 @@ const replaceValuesByBreakpoint = (mdxModel: Model, values: PropsValue): Model =
 
 const bundler = async (code: string): Promise<string> => (await bundleMDX({ source: code.trim() })).code;
 
-export const getRgxInstance = () => new RegExp('{{(\\w+):(\\w+)(?:\\((.+)?\\))?(\\[(.+)?\\])?(?::([^}}]*))?}}', 'g');
+export const getRgxInstance = () =>
+  new RegExp('{{(\\w+):(\\w+)(?:\\((.+)?\\))?(\\[([^\\]]*)?\\])?(?::([^}}]*))?}}', 'g');
 
 const replacePropValues = (mdx: string, values: Record<string, string> = {}): string => {
   if (!mdx) return '';

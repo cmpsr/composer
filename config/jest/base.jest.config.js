ea132353a3080e9fb6c82d2ebfff1342f3fa6278
Wesley Costa <wesleylira@gmail.com>
2023-01-04 16:42:42 -0300

    chore(COM-574) updates jest configs to leverage a reusable base config

--- config/jest/base.jest.config.js
+++ config/jest/base.jest.config.js
@@ -133,7 +133,9 @@ module.exports = {
   // setupFiles: [],
 
   // A list of paths to modules that run some code to configure or set up the testing framework before each test
-  setupFilesAfterEnv: [path.resolve(__dirname, 'config/jest/setupTests.ts')],
+  setupFilesAfterEnv: [
+    path.resolve(__dirname, '../../config/jest/setupTests.ts'),
+  ],
 
   // A list of paths to snapshot serializer modules Jest should use for snapshot testing
   // snapshotSerializers: [],

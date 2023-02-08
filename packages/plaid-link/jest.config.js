4c550ec823f6543aa138c81b28040382bc8cd44b
Wesley Costa <wesleylira@gmail.com>
2023-02-08 13:57:42 -0300

    chore(COM-574) updates coverage threshold

--- packages/plaid-link/jest.config.js
+++ packages/plaid-link/jest.config.js
@@ -3,4 +3,11 @@ const config = require('../../config/jest/base.jest.config');
 module.exports = {
   ...config,
   displayName: require('./package.json').name,
+  coverageThreshold: {
+    global: {
+      ...config.coverageThreshold.global,
+      lines: 75,
+      statements: 80,
+    },
+  },
 };

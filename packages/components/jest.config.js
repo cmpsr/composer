4281caf35a51bc0e26e5e35cf7dd4d4a3f0a799c
Wesley Costa <wesleylira@gmail.com>
2023-02-08 17:12:24 -0300

    chore(COM-574) updates components package coverage threshold

--- packages/components/jest.config.js
+++ packages/components/jest.config.js
@@ -3,4 +3,13 @@ const config = require('../../config/jest/base.jest.config');
 module.exports = {
   ...config,
   displayName: require('./package.json').name,
+  coverageThreshold: {
+    global: {
+      ...config.coverageThreshold.global,
+      branches: 20,
+      functions: 27,
+      lines: 81,
+      statements: 77,
+    },
+  },
 };

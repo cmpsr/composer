3aab0097fbafb8e9285c34361803524db5c55aed
Wesley Costa <wesleylira@gmail.com>
2023-02-08 13:50:05 -0300

    chore(COM-574) sets coverage threshold for each package

--- packages/nextjs-contentful-renderer/jest.config.js
+++ packages/nextjs-contentful-renderer/jest.config.js
@@ -3,4 +3,14 @@ const config = require('../../config/jest/base.jest.config');
 module.exports = {
   ...config,
   displayName: require('./package.json').name,
+  testEnvironment: undefined,
+  coverageThreshold: {
+    global: {
+      ...config.coverageThreshold.global,
+      branches: 53,
+      functions: 47,
+      lines: 56,
+      statements: 62,
+    },
+  },
 };

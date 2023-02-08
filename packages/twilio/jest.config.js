361a7db870731b7e4f381f5dfd3ab4145177cdf3
Wesley Costa <wesleylira@gmail.com>
2023-02-08 14:34:18 -0300

    chore(COM-574) updates coverage threshold

--- packages/twilio/jest.config.js
+++ packages/twilio/jest.config.js
@@ -3,4 +3,13 @@ const config = require('../../config/jest/base.jest.config');
 module.exports = {
   ...config,
   displayName: require('./package.json').name,
+  coverageThreshold: {
+    global: {
+      ...config.coverageThreshold.global,
+      branches: 80,
+      functions: 94,
+      lines: 91,
+      statements: 92,
+    },
+  },
 };

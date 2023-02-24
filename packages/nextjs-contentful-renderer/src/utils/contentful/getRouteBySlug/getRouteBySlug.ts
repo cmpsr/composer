3453687c77a4a0920ba8cabf2c8f8c7faa09793e
Ryan Hefner <hi@ryanhefner.com>
2023-02-24 17:06:51 -0500

    update getRouteBySlug to query for both routes and pageModels and return the matching entry

--- packages/nextjs-contentful-renderer/src/utils/contentful/getRouteBySlug/getRouteBySlug.ts
+++ packages/nextjs-contentful-renderer/src/utils/contentful/getRouteBySlug/getRouteBySlug.ts
@@ -1,17 +1,33 @@
 import { ApolloClient, gql, NormalizedCacheObject } from '@apollo/client';
-import { Route } from './types';
+import { PageModel, Route } from './types';
 
 export const getRouteBySlug = async (
   apolloClient: ApolloClient<NormalizedCacheObject>,
   slug: string,
   preview: boolean,
   domain = process.env.SITE_DOMAIN
-): Promise<Route> => {
+): Promise<PageModel | Route> => {
   const normalizedSlug = slug.startsWith('/') ? slug : `/${slug}`;
   const { data } = await apolloClient.query({
     query: gql`
       query routeBySlug($slug: String, $domain: String, $preview: Boolean) {
-        route: routeCollection(where: { slug: $slug, domain: $domain }, preview: $preview) {
+        pageModel: pageModelCollection(where: { domain: $domain, slug: $slug }, preview: $preview) {
+          items {
+            sys {
+              id
+            }
+            id
+            domain
+            modelData
+            pageTemplate {
+              sys {
+                id
+              }
+            }
+            slug
+          }
+        }
+        route: routeCollection(where: { domain: $domain, slug: $slug }, preview: $preview) {
           items {
             id
             variants: variantsCollection(limit: 10) {
@@ -36,17 +52,24 @@ export const getRouteBySlug = async (
     },
   });
 
-  const result = data.route.items.map((route) => {
-    return {
-      id: route.id,
+  // Resolve with PageModel, if available
+  if (data.pageModels.items.length) {
+    return data.pageModel.items.map((pageModel) => ({
+      id: pageModel.id,
+      modelData: pageModel.modelData,
+      page: pageModel?.pageTemplate?.sys?.id,
       slug,
-      variants: route.variants.items.map((variant: any) => ({
-        traffic: variant.traffic,
-        page: variant.page.sys.id,
-        utmCampaign: variant.utmCampaign,
-      })),
-    };
-  })[0];
+    }))[0];
+  }
 
-  return result;
+  // Resolve with Route
+  return data.route.items.map((route) => ({
+    id: route.id,
+    slug,
+    variants: route.variants.items.map((variant: any) => ({
+      traffic: variant.traffic,
+      page: variant.page.sys.id,
+      utmCampaign: variant.utmCampaign,
+    })),
+  }))[0];
 };

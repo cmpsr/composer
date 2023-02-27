062328dadb3e700a0151965c533283233b7cb0ea
Ryan Hefner <hi@ryanhefner.com>
2023-02-27 14:36:38 -0500

    update getStaticPageContent to support PageModel response

--- packages/nextjs-contentful-renderer/src/getStaticPageContent/getStaticPageContent.ts
+++ packages/nextjs-contentful-renderer/src/getStaticPageContent/getStaticPageContent.ts
@@ -1,12 +1,20 @@
 import { getApolloClient } from '../utils/getApolloClient';
 import { getRouteBySlug } from '../utils/contentful';
 import { getPageById } from '../utils/getPageById';
+import type { PageModel, Route } from '../utils/contentful/getRouteBySlug/types';
 
 export const getStaticPageContent = async (slug: string, preview = false, domain: string | undefined = undefined) => {
   const apolloClient = getApolloClient({ preview });
 
-  const route = await getRouteBySlug(apolloClient, slug, preview, domain);
+  const pageModelRoute = await getRouteBySlug(apolloClient, slug, preview, domain);
+
+  if (Object.keys(pageModelRoute).includes('modelData')) {
+    const pageModel = pageModelRoute as PageModel;
+    return await getPageById(apolloClient, pageModel.page, preview, pageModel.modelData);
+  }
+
   // We always take the first variant of the route for static pages
+  const route = pageModelRoute as Route;
   const pageId = route?.variants?.[0]?.page;
   if (!pageId) return undefined;
 

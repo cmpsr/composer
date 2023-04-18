4aa8ad623124bdb45fb32a7b050f023b536ce7e3
Ryan Hefner <hi@ryanhefner.com>
2023-04-18 12:31:17 -0400

    apply type to modelData arg

--- packages/nextjs-contentful-renderer/src/utils/getPageById/getPageById.ts
+++ packages/nextjs-contentful-renderer/src/utils/getPageById/getPageById.ts
@@ -3,12 +3,13 @@ import merge from 'lodash/merge';
 import { getPageById as getPageByIdFromContentful } from '../contentful';
 import { configFooter } from './configFooter';
 import { configNavbar } from './configNavbar';
+import { PropsValue } from '@cmpsr/cml';
 
 export const getPageById = async (
   apolloClient: ApolloClient<NormalizedCacheObject>,
   pageId: string,
   preview: boolean,
-  modelData?: any
+  modelData?: PropsValue[][]
 ) => {
   const page = await getPageByIdFromContentful(apolloClient, pageId, preview);
   if (page) {

42bd29af6da39606b53399a1322afe73c2d0842a
Ryan Hefner <hi@ryanhefner.com>
2023-04-19 11:19:49 -0400

    remove cookie check/set from getPageContent calls

--- packages/nextjs-contentful-renderer/src/getPageContent/getPageContent.ts
+++ packages/nextjs-contentful-renderer/src/getPageContent/getPageContent.ts
@@ -2,7 +2,6 @@ import { GetServerSidePropsContext } from 'next';
 import { getPageById } from '../utils/getPageById';
 import { getRouteBySlug } from '../utils/contentful';
 import { Page } from '../utils/contentful/getPageById/types';
-import { getVisitedPageIdFromCookies, setCookie } from '../utils/cookies';
 import { getApolloClient } from '../utils/getApolloClient';
 import { getPageId } from '../utils/getPageId';
 import { getSlug } from '../utils/getSlug';
@@ -19,23 +18,14 @@ export const getPageContent = async (
 
   const slug = getSlug(context.query.slug || '/');
 
-  const existingPageId = getVisitedPageIdFromCookies(context, slug);
-  if (existingPageId) {
-    const { modelData, pageId } = existingPageId;
-    const page = await getPageById(apolloClient, pageId, preview, modelData);
-    if (page) {
-      return page;
-    }
-  }
-
   const replicaRoute = await getRouteBySlug(apolloClient, slug, preview, domain);
+
   if (!replicaRoute) return undefined;
 
   const modelData = isReplica(replicaRoute) ? (replicaRoute as Replica).modelData : null;
 
   const pageId = getPageId(replicaRoute, context.query.utm_campaign);
   const page = await getPageById(apolloClient, pageId, preview, modelData);
-  setCookie(context, slug, { modelData, pageId });
 
   return page;
 };

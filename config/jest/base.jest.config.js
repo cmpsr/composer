c4a9a9373291a7da3929fd8964f5eea13a2a8ee0
Pau Teruel Soldevila <93.pau.teruel@gmail.com>
2023-04-25 16:07:49 +0200

    feat: support css modules in jest

--- config/jest/base.jest.config.js
+++ config/jest/base.jest.config.js
@@ -93,9 +93,9 @@ module.exports = {
   // ],
 
   // A map from regular expressions to module names that allow to stub out resources with a single module
-  // moduleNameMapper: {
-  //   "^.+\\.module\\.css$": "identity-obj-proxy",
-  // },
+  moduleNameMapper: {
+    '\\.(css|scss)$': 'identity-obj-proxy',
+  },
 
   // An array of regexp pattern strings, matched against all module paths before considered 'visible' to the module loader
   // modulePathIgnorePatterns: [],

c44ae51cb88033350e03b1b671c83c2399df0476
Pau Teruel Soldevila <93.pau.teruel@gmail.com>
2023-04-25 15:58:46 +0200

    feat: update carousel test

--- packages/components/src/components/other/Carousel/Carousel.test.tsx
+++ packages/components/src/components/other/Carousel/Carousel.test.tsx
@@ -3,10 +3,26 @@ import { screen, renderWithProviders } from '@tests/renderWithProviders';
 import { Carousel } from './Carousel';
 
 describe('Carousel', () => {
-  const givenComponentRendered = () => renderWithProviders(<Carousel>Children</Carousel>);
+  const givenComponentRendered = () =>
+    renderWithProviders(
+      <Carousel naturalSlideWidth={300} naturalSlideHeight={120} totalSlides={6}>
+        <Carousel.Slider>
+          <Carousel.Slide index={0}>Slide 1</Carousel.Slide>
+          <Carousel.Slide index={1}>Slide 2</Carousel.Slide>
+        </Carousel.Slider>
+        <Carousel.DotGroup
+          renderDots={() => (
+            <>
+              <Carousel.Dot slide={0} />
+              <Carousel.Dot slide={1} />
+            </>
+          )}
+        />
+      </Carousel>
+    );
 
-  test('should render a children', () => {
+  test('should render Carousel with slides', () => {
     givenComponentRendered();
-    screen.getByText(/Children/i);
+    screen.getByText(/Slide 1/i);
   });
 });

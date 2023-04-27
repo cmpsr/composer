3f737152989e2c51ff4b748ea64de116bcf22cb7
Pau Teruel Soldevila <93.pau.teruel@gmail.com>
2023-04-27 17:56:11 +0200

    test: add carousel sample test

--- config/jest/base.jest.config.js
+++ config/jest/base.jest.config.js
@@ -93,9 +93,9 @@ module.exports = {
   // ],
 
   // A map from regular expressions to module names that allow to stub out resources with a single module
-  // moduleNameMapper: {
-  // "^.+\\.module\\.css$": "identity-obj-proxy",
-  // },
+  moduleNameMapper: {
+    '^.+\\.(css)$': '<rootDir>/../../config/jest/cssTransform.js',
+  },
 
   // An array of regexp pattern strings, matched against all module paths before considered 'visible' to the module loader
   // modulePathIgnorePatterns: [],
--- packages/blocks/src/Carousel/Carousel.test.tsx
+++ packages/blocks/src/Carousel/Carousel.test.tsx
@@ -1,56 +1,29 @@
-// import React from 'react';
-// import { screen, renderWithProviders } from '../tests/renderWithProviders';
+import React from 'react';
+import { screen, renderWithProviders } from '../tests/renderWithProviders';
 
-// import { Hero } from './Carousel';
+import { Carousel } from './Carousel';
 
-// describe('Hero', () => {
-//   test('should render childrens', () => {
-//     renderWithProviders(
-//       <Hero>
-//         <Hero.Content>
-//           <Hero.ContentContainer>
-//             <Hero.Tag>Tag</Hero.Tag>
-//             <Hero.Overline>Legend</Hero.Overline>
-//             <Hero.Title variant="textpairing-header-4XL">
-//               <Hero.Title.Label>There is life elsewhere in the universe</Hero.Title.Label>
-//               <Hero.Title.SubLabel color="text-secondary">
-//                 We are right now on the verge of finding out whether there is life elsewhere in the universe, and there
-//                 are three ways we could find it.
-//               </Hero.Title.SubLabel>
-//             </Hero.Title>
-//           </Hero.ContentContainer>
-//           <Hero.ContentContainer gap="1.5rem">
-//             <Hero.MediaBlock>
-//               <Hero.MediaBlock.Image src="https://avatars0.githubusercontent.com/u/67131017?s=200" maxWidth="2rem" />
-//               <Hero.MediaBlock.Title>
-//                 <Hero.MediaBlock.Title.Label>MediaBlock</Hero.MediaBlock.Title.Label>
-//                 <Hero.MediaBlock.Title.SubLabel>SubLabel</Hero.MediaBlock.Title.SubLabel>
-//               </Hero.MediaBlock.Title>
-//             </Hero.MediaBlock>
-//           </Hero.ContentContainer>
-//           <Hero.ContentContainer>
-//             <Hero.Actions alignItems={{ base: 'center', md: 'start' }}>
-//               <Hero.Action children="Link 1" variant="primary" />
-//               <Hero.Action children="Link 2" variant="primary-alt" />
-//             </Hero.Actions>
-//             <Hero.Disclaimer>Disclaimer</Hero.Disclaimer>
-//             <Hero.Action size="s" children="I need help >" />
-//           </Hero.ContentContainer>
-//         </Hero.Content>
-//         <Hero.Image
-//           src="https://avatars0.githubusercontent.com/u/67131017?s=200"
-//           maxWidth="20rem"
-//           data-testid="cmpsr.block.hero-image"
-//         />
-//       </Hero>
-//     );
-//     screen.getByText('Tag');
-//     screen.getByText('Legend');
-//     screen.getByText('MediaBlock');
-//     screen.getByText('Link 1');
-//     screen.getByText('Link 2');
-//     screen.getByText('Disclaimer');
-//     screen.getByTestId('cmpsr.block.hero-image');
-//     screen.getByText('There is life elsewhere in the universe');
-//   });
-// });
+describe('Carousel', () => {
+  test('should render childrens', () => {
+    renderWithProviders(
+      <Carousel visibleSlides={1} naturalSlideWidth={1} naturalSlideHeight={1} totalSlides={1}>
+        <Carousel.Slider>
+          <Carousel.Slide index={0}>
+            <Carousel.MediaBlock>
+              <Carousel.MediaBlock.Image src="https://avatars0.githubusercontent.com/u/67131017?s=200" />
+              <Carousel.MediaBlock.Title>
+                <Carousel.MediaBlock.Title.Label>Label</Carousel.MediaBlock.Title.Label>
+                <Carousel.MediaBlock.Title.SubLabel>SubLabel</Carousel.MediaBlock.Title.SubLabel>
+              </Carousel.MediaBlock.Title>
+            </Carousel.MediaBlock>
+          </Carousel.Slide>
+        </Carousel.Slider>
+        <Carousel.NavigationContainer>
+          <Carousel.Dot slide={0} />
+        </Carousel.NavigationContainer>
+      </Carousel>
+    );
+    screen.getByText('Label');
+    screen.getByText('SubLabel');
+  });
+});

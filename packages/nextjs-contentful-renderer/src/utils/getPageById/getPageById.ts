bb24364a7541e88c4139bcbf248809400dad28a6
Ryan Hefner <hi@ryanhefner.com>
2023-05-09 13:49:11 -0400

    update getPageById to accept replica instead of discreet props from Replica

--- packages/nextjs-contentful-renderer/src/utils/getPageById/getPageById.ts
+++ packages/nextjs-contentful-renderer/src/utils/getPageById/getPageById.ts
@@ -1,24 +1,34 @@
 import { ApolloClient, NormalizedCacheObject } from '@apollo/client';
 import merge from 'lodash/merge';
 import { getPageById as getPageByIdFromContentful } from '../contentful';
+import type { Replica } from '../contentful/getRouteBySlug/types';
 import { configFooter } from './configFooter';
 import { configNavbar } from './configNavbar';
-import { PropsValue } from '@cmpsr/cml';
 
 export const getPageById = async (
   apolloClient: ApolloClient<NormalizedCacheObject>,
   pageId: string,
   preview: boolean,
-  modelData?: PropsValue[][]
+  replica?: Replica
 ) => {
   const page = await getPageByIdFromContentful(apolloClient, pageId, preview);
   if (page) {
-    if (modelData) {
+    // Merge metaConfiguration from replica
+    if (replica?.metaConfiguration) {
+      page.metaConfiguration = merge({}, page.metaConfiguration, replica.metaConfiguration);
+    }
+
+    // Merge modelData from replica
+    if (replica?.modelData) {
       page.content = page.content.map((block, blockIndex) => ({
         ...block,
-        propsValues: block.models.map((model, modelIndex) =>
-          merge({}, block.propsValues[modelIndex] ?? {}, modelData?.[blockIndex]?.[modelIndex] ?? {})
-        ),
+        propsValues: block.models
+          .map((model, modelIndex) =>
+            block.propsValues[modelIndex] || replica.modelData?.[blockIndex]?.[modelIndex]
+              ? merge({}, block.propsValues[modelIndex] ?? {}, replica.modelData?.[blockIndex]?.[modelIndex] ?? {})
+              : null
+          )
+          .filter((model) => model !== null),
       }));
     }
     page.content = configNavbar(page);

f42a86d3ff7921804aaa4267c543d4917dbb4a3a
Ryan Hefner <hi@ryanhefner.com>
2023-05-26 09:32:19 -0400

    update getStaticRoutes to gracefully handle missing Replica model

--- packages/nextjs-contentful-renderer/src/utils/contentful/getStaticRoutes/getStaticRoutes.ts
+++ packages/nextjs-contentful-renderer/src/utils/contentful/getStaticRoutes/getStaticRoutes.ts
@@ -6,18 +6,9 @@ export const getStaticRoutes = async (
   preview: boolean,
   domain: string
 ): Promise<Route[]> => {
-  const { data } = await apolloClient.query({
+  const { data: routesData } = await apolloClient.query({
     query: gql`
       query StaticRoutes($domain: String!, $preview: Boolean!) {
-        replicas: replicaCollection(where: { domain: $domain }, preview: $preview) {
-          items {
-            sys {
-              id
-            }
-            id
-            slug
-          }
-        }
         routes: routeCollection(where: { isStatic: true, domain: $domain }, preview: $preview) {
           items {
             sys {
@@ -35,7 +26,36 @@ export const getStaticRoutes = async (
     },
   });
 
-  return [...(data?.routes?.items ?? []), ...(data?.replicas?.items ?? [])].map(({ id, slug }) => ({
+  let replicasData = null;
+
+  try {
+    const { data: replicaDataResponse } = await apolloClient.query({
+      query: gql`
+        query StaticReplicas($domain: String!, $preview: Boolean!) {
+          replicas: replicaCollection(where: { domain: $domain }, preview: $preview) {
+            items {
+              sys {
+                id
+              }
+              id
+              slug
+            }
+          }
+        }
+      `,
+      variables: {
+        domain,
+        preview,
+      },
+    });
+
+    replicasData = replicaDataResponse;
+  } catch (e) {
+    // @todo Potentially track this, so we can reach out to installations that might need to be updated - Ryan
+    // Or, warn in the console so they know an update is available
+  }
+
+  return [...(routesData?.routes?.items ?? []), ...(replicasData?.replicas?.items ?? [])].map(({ id, slug }) => ({
     id,
     slug,
     variants: [],

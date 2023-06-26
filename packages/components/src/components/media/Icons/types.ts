fe2e3efce3d481e00e528ab8634ada4975d4a0c2
Pau Teruel <93.pau.teruel@gmail.com>
2023-06-26 17:03:14 +0200

    feat: support onClick on icons

--- packages/components/src/components/media/Icons/types.ts
+++ packages/components/src/components/media/Icons/types.ts
@@ -1,9 +1,10 @@
 import { StyleProps } from '@chakra-ui/react';
 
 export const iconSizes = ['xs', 's', 'm', 'l'] as const;
-export type IconSize = typeof iconSizes[number];
+export type IconSize = (typeof iconSizes)[number];
 
 export interface IconProps extends StyleProps {
   boxSize?: string | number;
   size?: IconSize;
+  onClick?: React.MouseEventHandler<SVGElement>;
 }

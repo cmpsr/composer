7f5953a9fab22d9a511d4a406261552a87331c2e
Ryan Hefner <hi@ryanhefner.com>
2023-06-28 12:25:26 -0400

    remove hardcoded route

--- packages/nextjs-contentful-renderer/src/Sitemap/Sitemap.tsx
+++ packages/nextjs-contentful-renderer/src/Sitemap/Sitemap.tsx
@@ -7,7 +7,7 @@ export const getServerSideProps: GetServerSideProps = async (ctx) => {
   const staticRoutes = await getStaticRoutes(process.env.SITE_DOMAIN, process.env.CONTENTFUL_PREVIEW === 'true');
   const dynamicRoutes = await getDynamicRoutes(process.env.SITE_DOMAIN, process.env.CONTENTFUL_PREVIEW === 'true');
 
-  const fields = [{ slug: '/' }, ...staticRoutes, ...dynamicRoutes].map((route) => ({
+  const fields = [...staticRoutes, ...dynamicRoutes].map((route) => ({
     loc: `${process.env.SITE_URL}${route.slug}`,
     lastmod: route?.lastmod ?? new Date().toISOString(),
   }));

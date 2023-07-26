1b61379b3bc13e64c5e8abd7ec0dcdd3e807c253
Wesley Costa <wesleylira@gmail.com>
2023-07-25 21:41:19 -0300

    feat(HAM-392) updates IconButton to use chakra's forwardRef - https://chakra-ui.com/docs/components/menu/usage#customizing-the-button

--- packages/components/src/components/overlay/Dropdown/Dropdown.stories.tsx
+++ packages/components/src/components/overlay/Dropdown/Dropdown.stories.tsx
@@ -2,6 +2,7 @@ import React from 'react';
 import { Meta } from '@storybook/react';
 import { Dropdown } from '.';
 import { IconCreditCard, IconPlus, IconShare } from '@components/media';
+import { Button } from '@components/form';
 
 export default {
   component: Dropdown,
@@ -79,6 +80,19 @@ const WithIconsTemplate = () => (
 );
 
 export const WithIcons = WithIconsTemplate.bind({});
+
+const WithCustomButtonTemplate = () => (
+  <Dropdown>
+    <Dropdown.Button as={Button}>Custom button</Dropdown.Button>
+    <Dropdown.List>
+      <Dropdown.Item icon={<IconPlus />}>Add</Dropdown.Item>
+      <Dropdown.Item icon={<IconCreditCard />}>Pay</Dropdown.Item>
+      <Dropdown.Item icon={<IconShare />}>Share</Dropdown.Item>
+    </Dropdown.List>
+  </Dropdown>
+);
+
+export const WithCustomButton = WithCustomButtonTemplate.bind({});
 
 const renderDropdownItems = (args) => {
   if (args.showGroupedItems) {

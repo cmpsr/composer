6ee90cdf15e62e05a8a60266feefc713b330beee
Wesley Costa <wesleylira@gmail.com>
2023-07-28 11:10:30 -0400

    fixes minor composer Dropdown bugs (#761)
    * feat(HAM-392) updates IconButton to use chakra's forwardRef - https://chakra-ui.com/docs/components/menu/usage#customizing-the-button
    
    * feat(HAM-392) updates IconButton to use chakra's forwardRef - https://chakra-ui.com/docs/components/menu/usage#customizing-the-button
    
    * feat(HAM-392) updates IconButton to use chakra's forwardRef - https://chakra-ui.com/docs/components/menu/usage#customizing-the-button
    
    * feat(HAM-392) updates IconButton to use chakra's forwardRef - https://chakra-ui.com/docs/components/menu/usage#customizing-the-button
    
    * feat(HAM-392) WithCustomButtonTemplate to test Dropdown.Item as links
    
    * feat(HAM-392) fixes IconButton ref
    
    * feat(HAM-392) fixes Dropdown DropdownStaticMembers types
    
    * chore: testing build --no-cache
    
    * rolling back after build --no-cache has worked
    
    * changesets

--- packages/components/src/components/form/IconButton/types.ts
+++ packages/components/src/components/form/IconButton/types.ts
@@ -1,13 +1,14 @@
+import { RefAttributes } from 'react';
 import { IconButtonProps as ChakraIconButtonProps } from '@chakra-ui/react';
 import { ButtonProps, SpinnerProps } from '@components';
 
 export const iconButtonSizes = ['xs', 's', 'm', 'l'] as const;
 export const iconButtonVariants = ['accent', 'primary', 'primary-alt', 'secondary', 'secondary-alt', 'ghost'] as const;
 
-export type IconButtonSize = typeof iconButtonSizes[number];
-export type IconButtonVariant = typeof iconButtonVariants[number];
+export type IconButtonSize = (typeof iconButtonSizes)[number];
+export type IconButtonVariant = (typeof iconButtonVariants)[number];
 
-export interface IconButtonProps extends ChakraIconButtonProps {
+export interface IconButtonProps extends ChakraIconButtonProps, RefAttributes<HTMLButtonElement> {
   size?: IconButtonSize;
   variant?: IconButtonVariant;
 }

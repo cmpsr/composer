b4bd4d9a2efe9e4a8f8a4e6d3b8fe4fb6fd9d743
Wesley Costa <wesleylira@gmail.com>
2023-08-03 06:46:19 -0700

    feat: updates navigation to allow keeping action links always visible on the navbar

--- packages/blocks/src/Navigation/components/NavigationActions.tsx
+++ packages/blocks/src/Navigation/components/NavigationActions.tsx
@@ -1,16 +1,29 @@
 import React, { FC } from 'react';
-import { Flex, FlexProps, Link, LinkProps, useBreakpointValue } from '@cmpsr/components';
+import { Flex, FlexProps, Link, LinkProps, LinkSize, useBreakpointValue } from '@cmpsr/components';
+import { useNavigationContext } from './NavigationContext';
 
-export const NavigationActions: FC<FlexProps> = (props) => (
-  <Flex
-    flexDirection={{ base: 'column', md: 'row' }}
-    gap={{ base: '1.5rem', lg: '0.75rem' }}
-    pt={{ base: '7rem', md: '5.75rem', lg: 0 }}
-    {...props}
-  />
-);
+export const NavigationActions: FC<FlexProps> = (props) => {
+  const { keepActionsAlwaysVisible } = useNavigationContext();
+
+  const keepActionsAlwaysVisibleOverrideProps: FlexProps = keepActionsAlwaysVisible
+    ? { flexDirection: { base: 'row' }, pt: 0 }
+    : {};
+
+  return (
+    <Flex
+      flexDirection={{ base: 'column', md: 'row' }}
+      gap={{ base: '1.5rem', lg: '0.75rem' }}
+      pt={{ base: '7rem', md: '5.75rem', lg: 0 }}
+      {...keepActionsAlwaysVisibleOverrideProps}
+      {...props}
+    />
+  );
+};
 
 export const NavigationAction: FC<LinkProps> = ({ size: givenSize, ...props }) => {
-  const size = givenSize || useBreakpointValue({ base: 'l', lg: 'm' }, 'base');
+  const { keepActionsAlwaysVisible } = useNavigationContext();
+
+  const size: LinkSize =
+    givenSize || keepActionsAlwaysVisible ? 'm' : useBreakpointValue({ base: 'l', lg: 'm' }, 'base');
   return <Link size={size} {...props} />;
 };

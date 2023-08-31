38e7235734ddb8903c7621c9ec381a084e3ac164
Takeichi Kanzaki Cabrera <me@tkanzakic.com>
2023-08-31 11:11:12 +0200

    fix: remove align options
    - content alignment is not supported in markdown

--- packages/markdown-editor/src/MarkdownEditor/plugins/ToolbarPlugin/ToolbarPlugin.tsx
+++ packages/markdown-editor/src/MarkdownEditor/plugins/ToolbarPlugin/ToolbarPlugin.tsx
@@ -490,44 +490,7 @@ export const ToolbarPlugin = () => {
             isDisabled={undefined}
             title={`Insert link (${IS_APPLE ? '⌘' : 'Ctrl+'}K)`}
           />
-          {/* {isLink && createPortal(<FloatingLinkEditor editor={activeEditor} />, document.body)} */}
           <Divider orientation="vertical" />
-          <button
-            onClick={() => {
-              editor.dispatchCommand(FORMAT_ELEMENT_COMMAND, 'left');
-            }}
-            className="toolbar-item spaced"
-            aria-label="Left Align"
-          >
-            <i className="format left-align" />
-          </button>
-          <button
-            onClick={() => {
-              editor.dispatchCommand(FORMAT_ELEMENT_COMMAND, 'center');
-            }}
-            className="toolbar-item spaced"
-            aria-label="Center Align"
-          >
-            <i className="format center-align" />
-          </button>
-          <button
-            onClick={() => {
-              editor.dispatchCommand(FORMAT_ELEMENT_COMMAND, 'right');
-            }}
-            className="toolbar-item spaced"
-            aria-label="Right Align"
-          >
-            <i className="format right-align" />
-          </button>
-          <button
-            onClick={() => {
-              editor.dispatchCommand(FORMAT_ELEMENT_COMMAND, 'justify');
-            }}
-            className="toolbar-item"
-            aria-label="Justify Align"
-          >
-            <i className="format justify-align" />
-          </button>{' '}
         </>
       )}
     </Flex>

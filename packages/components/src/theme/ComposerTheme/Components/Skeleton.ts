cf71073b1e4c20743d49af9bbacef90f2dff788a
Pau Teruel <93.pau.teruel@gmail.com>
2023-09-08 13:56:31 +0200

    Update packages/components/src/theme/ComposerTheme/Components/Skeleton.ts
    Co-authored-by: Takeichi Kanzaki Cabrera <me@tkanzakic.com>

--- packages/components/src/theme/ComposerTheme/Components/Skeleton.ts
+++ packages/components/src/theme/ComposerTheme/Components/Skeleton.ts
@@ -1,9 +1,10 @@
 import { ComponentStyleConfig, cssVar } from '@chakra-ui/react';
 
+const $startColor = cssVar('skeleton-start-color');
+const $endColor = cssVar('skeleton-end-color');
+    
 export const Skeleton: ComponentStyleConfig = {
   baseStyle: ({ theme }) => {
-    const $startColor = cssVar('skeleton-start-color');
-    const $endColor = cssVar('skeleton-end-color');
     return {
       [$startColor.variable]: theme.colors['background-skeleton-start'],
       [$endColor.variable]: theme.colors['background-skeleton-end'],

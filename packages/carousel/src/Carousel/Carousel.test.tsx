fd325cf49483a3320bb20bb9c0b275aa62ddbd53
Pau Teruel <93.pau.teruel@gmail.com>
2023-10-02 15:04:14 +0200

    [COM-1324] Adapt Carousel to use composition (#814)
    * feat: update carousel component to use composition
    
    * chore: create changeset
    
    * docs: update stories to improve code docs
    
    * docs: update index to match snapshot

--- packages/carousel/src/Carousel/Carousel.test.tsx
+++ packages/carousel/src/Carousel/Carousel.test.tsx
@@ -28,12 +28,15 @@ describe('Carousel', () => {
 
   test('should render Carousel with dots', () => {
     renderWithProviders(
-      <Carousel visibleSlides={1} naturalSlideWidth={1} naturalSlideHeight={1} showDots>
+      <Carousel visibleSlides={1} naturalSlideWidth={1} naturalSlideHeight={1}>
         <Carousel.Slider>
           <Carousel.Slide index={0}>
             <Text>Label</Text>
           </Carousel.Slide>
         </Carousel.Slider>
+        <Carousel.NavigationContainer>
+          <Carousel.Dot key={0} slide={0} />
+        </Carousel.NavigationContainer>
       </Carousel>
     );
 
@@ -42,7 +45,7 @@ describe('Carousel', () => {
 
   test('should render Carousel with arrows', () => {
     renderWithProviders(
-      <Carousel visibleSlides={1} naturalSlideWidth={1} naturalSlideHeight={1} showArrows>
+      <Carousel visibleSlides={1} naturalSlideWidth={1} naturalSlideHeight={1}>
         <Carousel.Slider>
           <Carousel.Slide index={0}>
             <Text>Label</Text>
@@ -51,45 +54,18 @@ describe('Carousel', () => {
             <Text>Label 1</Text>
           </Carousel.Slide>
         </Carousel.Slider>
+        <Carousel.NavigationContainer>
+          <Carousel.ButtonBack>
+            <Text>back</Text>
+          </Carousel.ButtonBack>
+          <Carousel.ButtonNext>
+            <Text>next</Text>
+          </Carousel.ButtonNext>
+        </Carousel.NavigationContainer>
       </Carousel>
     );
 
     expect(screen.getByLabelText('back')).toBeInTheDocument();
     expect(screen.getByLabelText('next')).toBeInTheDocument();
-  });
-
-  test('should render Carousel with arrows and dots', () => {
-    renderWithProviders(
-      <Carousel visibleSlides={1} naturalSlideWidth={1} naturalSlideHeight={1} showArrows showDots>
-        <Carousel.Slider>
-          <Carousel.Slide index={0}>
-            <Text>Label</Text>
-          </Carousel.Slide>
-          <Carousel.Slide index={1}>
-            <Text>Label</Text>
-          </Carousel.Slide>
-        </Carousel.Slider>
-      </Carousel>
-    );
-
-    expect(screen.queryAllByRole('button', { name: 'slide dot' }).length).toBe(2);
-    expect(screen.getByLabelText('back')).toBeInTheDocument();
-    expect(screen.getByLabelText('next')).toBeInTheDocument();
-  });
-
-  test('should group Carousel dots', async () => {
-    renderWithProviders(
-      <Carousel visibleSlides={4} naturalSlideWidth={1} naturalSlideHeight={1} showDots>
-        <Carousel.Slider data-testid="carousel-slider">
-          {new Array(13).fill(null).map((_, index) => (
-            <Carousel.Slide key={index} index={index}>
-              <Text>Slide {index + 1}</Text>
-            </Carousel.Slide>
-          ))}
-        </Carousel.Slider>
-      </Carousel>
-    );
-
-    expect(screen.queryAllByRole('button', { name: 'slide dot' }).length).toBe(4);
   });
 });

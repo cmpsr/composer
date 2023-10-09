6957e77074c5509dfe8c62e5790feb2f1797492c
Wesley Costa <wesleylira@gmail.com>
2023-10-09 16:05:09 -0300

    feat(COM-1376) adds markdown view toggle button to markdown editor (#818)

--- packages/markdown-editor/src/MarkdownEditor/MarkdownEditor.tsx
+++ packages/markdown-editor/src/MarkdownEditor/MarkdownEditor.tsx
@@ -1,5 +1,5 @@
-import React, { FC } from 'react';
-import { MarkdownEditorProps } from './types';
+import React, { FC, useCallback, useRef, useState } from 'react';
+import { MarkdownEditorProps, EditorMode } from './types';
 import { Box } from '@cmpsr/components';
 
 import { defaultTheme } from './themes/defaultTheme';
@@ -7,6 +7,8 @@ import { Placeholder } from './components/Placeholder';
 
 import { LexicalComposer } from '@lexical/react/LexicalComposer';
 import LexicalErrorBoundary from '@lexical/react/LexicalErrorBoundary';
+import { EditorRefPlugin } from '@lexical/react/LexicalEditorRefPlugin';
+import { PlainTextPlugin } from '@lexical/react/LexicalPlainTextPlugin';
 import { RichTextPlugin } from '@lexical/react/LexicalRichTextPlugin';
 import { ContentEditable } from '@lexical/react/LexicalContentEditable';
 import { HistoryPlugin } from '@lexical/react/LexicalHistoryPlugin';
@@ -17,6 +19,8 @@ import { CodeHighlightNode, CodeNode } from '@lexical/code';
 import { AutoLinkNode, LinkNode } from '@lexical/link';
 import { ListPlugin } from '@lexical/react/LexicalListPlugin';
 import { HorizontalRuleNode } from '@lexical/react/LexicalHorizontalRuleNode';
+import { $createParagraphNode, $createTextNode, $getRoot, LexicalEditor } from 'lexical';
+import { $convertFromMarkdownString, $convertToMarkdownString } from '@lexical/markdown';
 
 import { OnChangeMarkdown } from './plugins/OnChangeMarkdown';
 import { CodeHighlightPlugin } from './plugins/CodeHighlightPlugin';
@@ -31,6 +35,7 @@ import { PLAYGROUND_TRANSFORMERS } from './plugins/MarkdownTransformers';
 import { SetInitialValuePlugin } from './plugins/SetInitialValuePlugin';
 
 import './styles';
+import { DISCARD_HISTORY_CANDIDATE } from './constants';
 
 const editorConfig = {
   theme: defaultTheme,
@@ -76,7 +81,36 @@ export const MarkdownEditor: FC<MarkdownEditorProps> = ({
   contentProps,
   contentEditableStyles,
   externalToolbarActions,
+  editorMode: initialEditorMode = EditorMode.RichText,
 }) => {
+  const editorRef = useRef<LexicalEditor>(null);
+  const [editorMode, setEditorMode] = useState<EditorMode>(initialEditorMode);
+  const TextPlugin = editorMode === EditorMode.RichText ? RichTextPlugin : PlainTextPlugin;
+
+  const toggleEditorMode = useCallback(() => {
+    setEditorMode((currentEditorMode) => {
+      const isRichEditorModeOn = currentEditorMode === EditorMode.RichText;
+      const updateOptions = {
+        tag: DISCARD_HISTORY_CANDIDATE,
+      };
+
+      editorRef.current?.update(() => {
+        const root = $getRoot();
+
+        if (isRichEditorModeOn) {
+          const markdown = $convertToMarkdownString(PLAYGROUND_TRANSFORMERS, root);
+          root.clear().append($createParagraphNode().append($createTextNode(markdown)));
+        } else {
+          $convertFromMarkdownString(root.getTextContent(), PLAYGROUND_TRANSFORMERS, root);
+        }
+
+        root.selectStart();
+      }, updateOptions);
+
+      return isRichEditorModeOn ? EditorMode.PlainText : EditorMode.RichText;
+    });
+  }, [setEditorMode]);
+
   return (
     <LexicalComposer initialConfig={editorConfig}>
       <Box
@@ -96,9 +130,12 @@ export const MarkdownEditor: FC<MarkdownEditorProps> = ({
           isDisabled={isReadonly}
           externalActions={externalToolbarActions}
           toolbarPluginProps={toolbarPluginProps}
+          editorMode={editorMode}
+          toggleEditorMode={toggleEditorMode}
         />
         <Box backgroundColor={backgroundColor} position="relative" width="100%" {...contentProps}>
-          <RichTextPlugin
+          <EditorRefPlugin editorRef={editorRef} />
+          <TextPlugin
             contentEditable={
               <ContentEditable
                 style={{
@@ -137,7 +174,7 @@ export const MarkdownEditor: FC<MarkdownEditorProps> = ({
             transformers={PLAYGROUND_TRANSFORMERS}
           />
           <FloatingLinkEditorPlugin />
-          <SetInitialValuePlugin value={initialValue} version={initialValueVersion} />
+          <SetInitialValuePlugin value={initialValue} version={initialValueVersion} editorMode={editorMode} />
         </Box>
       </Box>
     </LexicalComposer>
---
+++ packages/markdown-editor/src/MarkdownEditor/constants.ts
@@ -0,0 +1,5 @@
+/**
+ * Used as tag to discard history candidate when updating the editor's state.
+ * More info: https://github.com/facebook/lexical/blob/main/packages/lexical-history/src/index.ts#L247
+ */
+export const DISCARD_HISTORY_CANDIDATE = 'historic';

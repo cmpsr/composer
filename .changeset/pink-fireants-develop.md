20240c88f9839d4ea7faa0f4f1ddece8699d55ef
Pau Teruel <93.pau.teruel@gmail.com>
2023-11-14 14:48:58 +0100

    [HAM-639] FloatingLink shouldn't be shown when editor is not focused (#831)
    * feat: create useEditorFocus to detect when the editor is focused
    
    * chore: create changeset
    
    * Remove unused import and code related to editor
    focus.
    
    * Fix floating link editor behavior when editor is
    focused

---
+++ .changeset/pink-fireants-develop.md
@@ -0,0 +1,5 @@
+---
+"@cmpsr/markdown-editor": patch
+---
+
+feat: avoid showing FloatingLink when editor is not focused
---
+++ packages/markdown-editor/src/MarkdownEditor/utils/useEditorFocus.tsx
@@ -0,0 +1,38 @@
+import { FOCUS_COMMAND, BLUR_COMMAND, COMMAND_PRIORITY_LOW } from 'lexical';
+import { useLayoutEffect, useState } from 'react';
+import { mergeRegister } from '@lexical/utils';
+import { useLexicalComposerContext } from '@lexical/react/LexicalComposerContext';
+
+export const useEditorFocus = () => {
+  const [editor] = useLexicalComposerContext();
+  // Initial state based on whether the editor is currently focused
+  const [hasFocus, setHasFocus] = useState(() => {
+    return editor.getRootElement() === document.activeElement;
+  });
+
+  useLayoutEffect(() => {
+    // Set initial focus state when the component mounts
+    setHasFocus(editor.getRootElement() === document.activeElement);
+
+    return mergeRegister(
+      editor.registerCommand(
+        FOCUS_COMMAND,
+        () => {
+          setHasFocus(true);
+          return true;
+        },
+        COMMAND_PRIORITY_LOW
+      ),
+      editor.registerCommand(
+        BLUR_COMMAND,
+        () => {
+          setHasFocus(false);
+          return true;
+        },
+        COMMAND_PRIORITY_LOW
+      )
+    );
+  }, [editor]);
+
+  return hasFocus;
+};

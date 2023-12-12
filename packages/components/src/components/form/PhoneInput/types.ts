3aac0d406226fed53b27d38d77cbc71ec0e9e1f7
Pau Teruel <93.pau.teruel@gmail.com>
2023-12-12 10:55:36 +0100

    lint: import react

--- packages/components/src/components/form/PhoneInput/types.ts
+++ packages/components/src/components/form/PhoneInput/types.ts
@@ -1,10 +1,11 @@
+import { ChangeEvent } from 'react';
 import { CountryCode } from 'libphonenumber-js';
 import { InputProps } from '../Input';
 import { MaskitoMask } from '@maskito/core';
 
 export interface PhoneInputProps extends Omit<InputProps, 'onChange'> {
   maskOptions?: PhoneMaskOptions;
-  onChange?: (event: React.ChangeEvent<HTMLInputElement>, isValid: boolean) => void;
+  onChange?: (event: ChangeEvent<HTMLInputElement>, isValid: boolean) => void;
 }
 
 export type PhoneMaskOptions = {

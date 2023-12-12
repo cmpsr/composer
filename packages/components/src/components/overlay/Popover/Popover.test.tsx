5cf10ac70c787e2dab8abef9e9b30a16f6b6bf48
Pau Teruel <93.pau.teruel@gmail.com>
2023-12-12 18:44:00 +0100

    [COM-1478] Popover (#872)
    * feat: create Popover
    
    * chore: create changeset

---
+++ packages/components/src/components/overlay/Popover/Popover.test.tsx
@@ -0,0 +1,27 @@
+import React from 'react';
+import { Popover } from './Popover';
+import { renderWithProviders, screen } from '@tests/renderWithProviders';
+
+describe('Popover', () => {
+  it('should render all Popover elements', () => {
+    renderWithProviders(
+      <Popover>
+        <Popover.Trigger>
+          <button>Trigger</button>
+        </Popover.Trigger>
+        <Popover.Content>
+          <Popover.Arrow />
+          <Popover.CloseButton />
+          <Popover.Header>Header</Popover.Header>
+          <Popover.Body>Body</Popover.Body>
+          <Popover.Footer>Footer</Popover.Footer>
+        </Popover.Content>
+      </Popover>
+    );
+
+    screen.getByText('Trigger');
+    screen.getByText('Header');
+    screen.getByText('Body');
+    screen.getByText('Footer');
+  });
+});

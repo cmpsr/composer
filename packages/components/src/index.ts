787898016d94f24179f713564a21f3f707c53f2c
Pau Teruel <93.pau.teruel@gmail.com>
2023-12-12 12:10:52 +0100

    feat: re-export Global

--- packages/components/src/index.ts
+++ packages/components/src/index.ts
@@ -2,3 +2,4 @@ export * from './components';
 export * from './hooks';
 export * from './theme';
 export { keyframes, Fade, ScaleFade, Slide, SlideFade, Collapse, useMergeRefs } from '@chakra-ui/react';
+export { Global } from '@emotion/react';

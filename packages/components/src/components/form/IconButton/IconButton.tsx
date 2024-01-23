3546be544f758b56df460dc5d681104e87affb1e
Pau Teruel <93.pau.teruel@gmail.com>
2024-01-23 17:26:58 +0100

    feat: update IconButton to use responsive size

--- packages/components/src/components/form/IconButton/IconButton.tsx
+++ packages/components/src/components/form/IconButton/IconButton.tsx
@@ -1,24 +1,27 @@
 import React, { FC } from 'react';
 import { forwardRef, IconButton as ChakraIconButton, useMultiStyleConfig } from '@chakra-ui/react';
-import { IconButtonProps, IconButtonStyle } from './types';
+import { IconButtonProps, IconButtonSize, IconButtonStyle } from './types';
 import { Spinner } from '@components';
+import { useResponsiveValue } from '@hooks';
 
 export const IconButton: FC<IconButtonProps> = forwardRef<IconButtonProps, 'button'>(
-  ({ isRound, icon, ...props }, ref) => {
+  ({ isRound, icon, size, ...props }, ref) => {
+    const responsiveSize = useResponsiveValue(size) as IconButtonSize;
     const { loading: iconButtonLoading, ...iconButtonStyles } = useMultiStyleConfig('IconButton', {
       isRound: isRound,
-      size: props.size,
+      size: responsiveSize,
       variant: props.variant,
       isLoading: props.isLoading,
     }) as IconButtonStyle;
 
-    const Icon = React.cloneElement(icon, { size: props.size });
+    const Icon = React.cloneElement(icon, { size: responsiveSize });
 
     return (
       <ChakraIconButton
         ref={ref}
         icon={Icon}
         spinner={<Spinner {...iconButtonLoading} />}
+        size={responsiveSize}
         {...iconButtonStyles}
         {...props}
       />

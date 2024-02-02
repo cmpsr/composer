97e8c18ccd6f15ead314a93e1011b0627240471b
Pau Teruel <93.pau.teruel@gmail.com>
2024-02-02 13:41:51 +0100

    Revert "feat: update IconButton to use resolvedButtonVariant"
    This reverts commit 35dc4126c0cc121734cccc7d4d1d85bd2630a0cc.

--- packages/components/src/theme/ComposerTheme/Components/IconButton.ts
+++ packages/components/src/theme/ComposerTheme/Components/IconButton.ts
@@ -12,7 +12,7 @@ export const IconButton: ComponentStyleConfig = {
       borderRadius,
       loading: buttonLoadingStyles,
       _before: {
-        ...resolvedButtonVariant._before,
+        ...buttonVariant._before,
         borderRadius,
       },
     };

47a2d55f40f64f9302825e13784d575a587a5881
Alejandra Arri <aleja.arri@gmail.com>
2024-02-06 16:55:40 +0100

    Fix types

--- packages/components/src/components/form/IconButton/types.ts
+++ packages/components/src/components/form/IconButton/types.ts
@@ -1,8 +1,19 @@
-import { ButtonIconProps, ButtonIconSize, ButtonIconStyle, ButtonIconVariant } from '@components';
+import { ButtonIconSize, ButtonIconVariant, buttonIconSizes, buttonIconVariants } from '@components';
+
+import { RefAttributes } from 'react';
+import { IconButtonProps as ChakraButtonIconProps, ResponsiveValue } from '@chakra-ui/react';
+import { ButtonProps, SpinnerProps } from '@components';
+
+export const iconButtonSizes = buttonIconSizes;
+export const iconButtonVariants = buttonIconVariants;
 
 export type IconButtonSize = ButtonIconSize;
 export type IconButtonVariant = ButtonIconVariant;
+export interface IconButtonProps extends ChakraButtonIconProps, RefAttributes<HTMLButtonElement> {
+  size?: ResponsiveValue<IconButtonSize>;
+  variant?: ResponsiveValue<IconButtonVariant>;
+}
 
-export interface IconButtonProps extends ButtonIconProps {}
-
-export interface IconButtonStyle extends ButtonIconStyle {}
+export interface IconButtonStyle extends ButtonProps {
+  loading?: SpinnerProps;
+}

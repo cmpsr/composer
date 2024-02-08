23c136bb6db806d43de07817c9bd1828b7f3d4ce
Alejandra Arri <aleja.arri@gmail.com>
2024-02-08 15:50:28 +0100

    last fixes

--- packages/components/src/theme/ComposerTheme/Components/LinkIcon.ts
+++ packages/components/src/theme/ComposerTheme/Components/LinkIcon.ts
@@ -1,32 +1,5 @@
 import { ComponentStyleConfig } from '@chakra-ui/theme';
 import { StyleFunctionProps } from '@chakra-ui/theme-tools';
-import { linkSizes } from '@components';
-import { generateLink, linkPrimary } from './Link';
-
-export const linkIconBaseStyle = {
-  width: 'inherit',
-  _hover: {
-    textDecoration: 'none',
-  },
-};
-
-const getSizes = () => {
-  const sizes = {};
-  linkSizes.forEach((size) => {
-    sizes[size] = (props) => {
-      return {
-        ...props.theme.textStyles[
-          {
-            s: 'text-body-meta-medium',
-            m: 'text-body-medium',
-            l: 'text-body-large-medium',
-          }[size]
-        ],
-      };
-    };
-  });
-  return sizes;
-};
 
 export const LinkIcon: ComponentStyleConfig = {
   baseStyle: (props: StyleFunctionProps & { isInline: boolean }) => {
@@ -35,32 +8,15 @@ export const LinkIcon: ComponentStyleConfig = {
       : { display: 'inline-flex' };
 
     return {
-      ...linkIconBaseStyle,
+      ...props.theme.components.Link.baseStyle,
       ...linkDisplayStyles,
     };
   },
-  sizes: getSizes(),
   variants: {
-    link: linkPrimary,
-    'link-primary': linkPrimary,
-    'link-accent': generateLink({
-      default: 'text-link-accent-default',
-      hover: 'text-link-accent-hover',
-      pressed: 'text-link-accent-pressed',
-      focus: 'accent-focus',
-    }),
-    'link-secondary': generateLink({
-      default: 'text-link-secondary-default',
-      hover: 'text-link-secondary-hover',
-      pressed: 'text-link-secondary-pressed',
-      focus: 'secondary-focus',
-    }),
-    'link-destroy': generateLink({
-      default: 'alert-error-default',
-      hover: 'alert-error-hover',
-      pressed: 'alert-error-pressed',
-      focus: 'alert-error-focus',
-    }),
+    'link-primary': (props) => props.theme.components.Link.variants['link-primary'],
+    'link-accent': (props) => props.theme.components.Link.variants['link-accent'],
+    'link-secondary': (props) => props.theme.components.Link.variants['link-secondary'],
+    'link-destroy': (props) => props.theme.components.Link.variants['link-destroy'],
   },
   defaultProps: {
     size: 'l',

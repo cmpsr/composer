efbb3d9ea2d2710615b35ff65066dde6efcbd082
Alejandra Arri <aleja.arri@gmail.com>
2024-02-14 12:03:49 +0100

    fix: Fix the import in the NumberedList type

--- packages/components/src/components/dataDisplay/NumberedList/types.ts
+++ packages/components/src/components/dataDisplay/NumberedList/types.ts
@@ -1,5 +1,5 @@
 import { FC } from 'react';
-import { FlexProps, TextVariant } from '@components';
+import { FlexProps, TextVariant } from '@cmpsr/components';
 
 export interface NumberedListProps extends FlexProps {
   number?: number;

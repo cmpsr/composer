e87af11a26c3dad41d0aa2f9d0a9a843810bb48e
Gloria Talavera <gloria.tare@gmail.com>
2024-03-12 12:06:21 +0100

    [COM-1552] Badge component new variants

--- packages/components/src/components/dataDisplay/Badge/types.ts
+++ packages/components/src/components/dataDisplay/Badge/types.ts
@@ -1,8 +1,21 @@
 import { BadgeProps as ChakraBadgeProps } from '@chakra-ui/react';
 
 export const badgeVariants = ['solid', 'outline', 'subtle'] as const;
-export type BadgeVariant = typeof badgeVariants[number];
+export type BadgeVariant = (typeof badgeVariants)[number];
+
+export const badgeStatuses = [
+  'custom',
+  'accent',
+  'primary',
+  'secondary',
+  'info',
+  'success',
+  'warning',
+  'error',
+] as const;
+export type BadgeStatus = (typeof badgeStatuses)[number];
 
 export interface BadgeProps extends ChakraBadgeProps {
   variant?: BadgeVariant;
+  status?: BadgeStatus;
 }
--- packages/components/src/theme/ComposerTheme/Components/Badge.ts
+++ packages/components/src/theme/ComposerTheme/Components/Badge.ts
@@ -1,5 +1,16 @@
 import { ComponentStyleConfig } from '@chakra-ui/react';
 
+export const statusStyles = {
+  custom: { backgroundColor: 'primary-default', borderColor: 'primary-default', color: 'text-badge-outline' },
+  accent: { backgroundColor: 'accent-default', borderColor: 'accent-default', color: 'accent-default' },
+  primary: { backgroundColor: 'primary-default', borderColor: 'primary-default', color: 'primary-default' },
+  secondary: { backgroundColor: 'secondary-default', borderColor: 'secondary-default', color: 'secondary-default' },
+  info: { backgroundColor: 'alert-inform-default', borderColor: 'alert-inform-default', color: 'alert-inform-default' },
+  success: { backgroundColor: 'alert-success-default', borderColor: 'alert-success-default', color: 'text-success' },
+  warning: { backgroundColor: 'alert-warning-default', borderColor: 'alert-warning-default', color: 'text-warning' },
+  error: { backgroundColor: 'alert-error-default', borderColor: 'alert-error-default', color: 'text-error' },
+};
+
 export const Badge: ComponentStyleConfig = {
   baseStyle: ({ theme }) => ({
     letterSpacing: '0.05em',
@@ -7,22 +18,23 @@ export const Badge: ComponentStyleConfig = {
     ...theme.textStyles['text-body-floating-label-bold'],
   }),
   variants: {
-    solid: {
-      backgroundColor: 'primary-default',
+    solid: ({ status }) => ({
+      backgroundColor: statusStyles[status].backgroundColor,
       color: 'text-badge-solid',
-    },
-    outline: {
-      borderColor: 'primary-default',
+    }),
+    outline: ({ status }) => ({
+      borderColor: statusStyles[status].borderColor,
       borderWidth: '1px',
       boxShadow: 'none',
-      color: 'text-badge-outline',
-    },
-    subtle: {
+      color: statusStyles[status].color,
+    }),
+    subtle: ({ status }) => ({
       backgroundColor: 'background-action-default',
-      color: 'text-badge-subtle',
-    },
+      color: statusStyles[status].color,
+    }),
   },
   defaultProps: {
     variant: 'solid',
+    status: 'primary',
   },
 };

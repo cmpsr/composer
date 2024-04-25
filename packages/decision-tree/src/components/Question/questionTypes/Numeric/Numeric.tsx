a73eef4e659344a11f5a2a8dc54bb05b86e3ab02
Alexis Munoz <alexismunoz@impulsum.vc>
2024-04-25 15:42:27 +0200

    fix: mask input

--- packages/decision-tree/src/components/Question/questionTypes/Numeric/Numeric.tsx
+++ packages/decision-tree/src/components/Question/questionTypes/Numeric/Numeric.tsx
@@ -1,32 +1,28 @@
-import React, { FC, useState } from 'react';
-import { type QuestionProps } from '../../types';
+import React, { FC } from 'react';
+import { type QuestionProps } from '@components/Question/types';
 import { Box, MaskInput, Flex } from '@cmpsr/components';
 import { QuestionTitle } from '../../components/QuestionTitle';
 import { NumericQuestion } from './types';
+import { HandleAnswersActions } from '@hooks';
 
-export const Numeric: FC<QuestionProps> = ({ data, submitAnswer }) => {
-  const { question, inputs, tooltip, id: questionId } = data as NumericQuestion;
-  const [answers, setAnswers] = useState({});
+export const Numeric: FC<QuestionProps> = ({ data, answersDispatch }) => {
+  const { question, tooltip, id, placeholder, trailingMask } = data as NumericQuestion;
 
   return (
     <Box>
       <QuestionTitle question={question} tooltip={tooltip} />
       <Flex>
-        {inputs.map(({ id, placeholder, trailingMask }) => (
-          <MaskInput
-            key={id}
-            variant="outline"
-            size="l"
-            placeholder={placeholder}
-            mask={{}}
-            type="number"
-            postFix={` ${trailingMask}`}
-            onBlur={(nextValue) => {
-              setAnswers({ ...answers, [id]: nextValue });
-              submitAnswer({ questionId, value: JSON.stringify(answers) });
-            }}
-          />
-        ))}
+        <MaskInput
+          maskOptions={{
+            mask: /^\d{0,3}(\.\d{0,2})?$/,
+            postfix: ` ${trailingMask}`,
+          }}
+          key={id}
+          variant="outline"
+          size="l"
+          placeholder={placeholder}
+          onBlur={({ target }) => answersDispatch({ type: HandleAnswersActions.SaveAnswer, payload: target.value })}
+        />
       </Flex>
     </Box>
   );

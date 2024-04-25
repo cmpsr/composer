eb9993ba36d00c1472f910aaa525056b5660d310
Alexis Munoz <alexismunoz@impulsum.vc>
2024-04-25 15:39:45 +0200

    fix: choice questions on change event

--- packages/decision-tree/src/components/Question/questionTypes/MultipleChoice/MultipleChoice.tsx
+++ packages/decision-tree/src/components/Question/questionTypes/MultipleChoice/MultipleChoice.tsx
@@ -1,27 +1,28 @@
 import React, { FC, useState } from 'react';
-import { type QuestionProps } from '../../types';
+import { type QuestionProps } from '@components/Question/types';
 import { Box, Checkbox } from '@cmpsr/components';
 import { QuestionTitle } from '../../components/QuestionTitle';
 import { MultipleChoiceQuestion } from './types';
+import { HandleAnswersActions } from '@hooks';
 
-export const MultipleChoice: FC<QuestionProps> = ({ data, submitAnswer }) => {
-  const { question, choices, tooltip, id: questionId } = data as MultipleChoiceQuestion;
-  const [answers, setAnswers] = useState(new Set());
+export const MultipleChoice: FC<QuestionProps> = ({ data, answersDispatch }) => {
+  const { question, choices, tooltip } = data as MultipleChoiceQuestion;
+  const [answers, setAnswers] = useState(new Set<string>());
 
   return (
     <Box>
       <QuestionTitle question={question} tooltip={tooltip} />
       {choices.map(({ id, label, subLabel }) => (
-        <Box key={id}>
+        <Box key={`multipleChoice-${id}`}>
           <Box>
             <Box>{label}</Box>
             {subLabel ? <Box>{subLabel}</Box> : null}
           </Box>
           <Checkbox
             size="l"
-            onBlur={(nextValue) => {
-              setAnswers(answers.add(nextValue));
-              submitAnswer({ questionId, value: JSON.stringify(answers.values()) });
+            onChange={({ target }) => {
+              setAnswers(answers.add(target.value));
+              answersDispatch({ type: HandleAnswersActions.SaveAnswer, payload: answers.values() });
             }}
           />
         </Box>
--- packages/decision-tree/src/components/Question/questionTypes/SingleChoice/SingleChoice.tsx
+++ packages/decision-tree/src/components/Question/questionTypes/SingleChoice/SingleChoice.tsx
@@ -1,34 +1,38 @@
 import React, { FC } from 'react';
-import { type QuestionProps } from '../../types';
+import { type QuestionProps } from '@components/Question/types';
 import { Box, RadioGroup, Radio } from '@cmpsr/components';
 import { QuestionTitle } from '../../components/QuestionTitle';
 import { SingleChoiceQuestion } from './types';
+import { HandleAnswersActions } from '@hooks';
 
-export const SingleChoice: FC<QuestionProps> = ({ data, submitAnswer }) => {
-  const { question, choices, tooltip, id } = data as SingleChoiceQuestion;
+export const SingleChoice: FC<QuestionProps> = ({ data, answersDispatch }) => {
+  const { question, choices, tooltip } = data as SingleChoiceQuestion;
   return (
     <Box>
       <QuestionTitle question={question} tooltip={tooltip} />
-      <RadioGroup onChange={(nextValue) => submitAnswer({ questionId: id, value: nextValue })}>
+      <RadioGroup
+        onChange={(nextValue) => answersDispatch({ type: HandleAnswersActions.SaveAnswer, payload: nextValue })}
+      >
         {choices.map(({ id, label, subLabel }) => (
-          <Box key={id} 
-            border='1px solid #666' 
-            borderColor='ui-element-outline-default' 
-            background='background-action-default' 
-            borderRadius='radii-4' 
-            width='350px' 
-            mb='spacer-4'
-            mx='auto'
+          <Box
+            key={`SingleChoice-${id}`}
+            border="1px solid #666"
+            borderColor="ui-element-outline-default"
+            background="background-action-default"
+            borderRadius="radii-4"
+            width="350px"
+            mb="spacer-4"
+            mx="auto"
           >
-            <Radio 
-              display='flex'
-              flexDirection='row-reverse' 
-              variant="outline" 
+            <Radio
+              display="flex"
+              flexDirection="row-reverse"
+              variant="outline"
               size="l"
-              m='auto'
-              justifyContent='space-between' 
-              py='spacer-3' 
-              px='spacer-4' 
+              m="auto"
+              justifyContent="space-between"
+              py="spacer-3"
+              px="spacer-4"
               value={id}
             >
               <Box>

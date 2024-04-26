94a729974ded01b29ad55ff58d2ab81575f07192
Alexis Munoz <alexismunoz@impulsum.vc>
2024-04-26 07:45:09 +0200

    fix: coverage filechecks

--- packages/decision-tree/jest.config.js
+++ packages/decision-tree/jest.config.js
@@ -3,4 +3,8 @@ const config = require('../../config/jest/base.jest.config');
 module.exports = {
   ...config,
   displayName: require('./package.json').name,
+  coveragePathIgnorePatterns: [
+    ...config.coveragePathIgnorePatterns,
+    ".mock.ts",
+],
 };

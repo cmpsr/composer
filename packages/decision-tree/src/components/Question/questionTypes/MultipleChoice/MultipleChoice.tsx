4cf681145c3fd1c3eef27e0f0db673456819f556
Alexis Munoz <alexismunoz@impulsum.vc>
2024-04-26 11:01:44 +0200

    feat: add question option component to be shared by check and radio

--- packages/decision-tree/src/components/Question/questionTypes/MultipleChoice/MultipleChoice.tsx
+++ packages/decision-tree/src/components/Question/questionTypes/MultipleChoice/MultipleChoice.tsx
@@ -1,9 +1,10 @@
 import React, { FC, useState } from 'react';
 import { type QuestionProps } from '@components/Question/types';
-import { Box, Checkbox } from '@cmpsr/components';
+import { Box } from '@cmpsr/components';
 import { QuestionTitle } from '../../components/QuestionTitle';
 import { MultipleChoiceQuestion } from './types';
 import { HandleAnswersActions } from '@hooks';
+import { QuestionOption } from '../../components/QuestionOption';
 
 export const MultipleChoice: FC<QuestionProps> = ({ data, answersDispatch }) => {
   const { question, choices, tooltip } = data as MultipleChoiceQuestion;
@@ -13,19 +14,19 @@ export const MultipleChoice: FC<QuestionProps> = ({ data, answersDispatch }) => 
     <Box>
       <QuestionTitle question={question} tooltip={tooltip} />
       {choices.map(({ id, label, subLabel }) => (
-        <Box key={`multipleChoice-${id}`}>
-          <Box>
-            <Box>{label}</Box>
-            {subLabel ? <Box>{subLabel}</Box> : null}
-          </Box>
-          <Checkbox
-            size="l"
-            onChange={({ target }) => {
+        <QuestionOption
+          key={`multipleChoice-${id}`}
+          componentType="checkbox"
+          componentProps={{
+            value: id,
+            onChange: ({ target }) => {
               setAnswers(answers.add(target.value));
               answersDispatch({ type: HandleAnswersActions.SaveAnswer, payload: answers.values() });
-            }}
-          />
-        </Box>
+            },
+          }}
+          label={label}
+          subLabel={subLabel}
+        />
       ))}
     </Box>
   );
--- packages/decision-tree/src/components/Question/questionTypes/SingleChoice/SingleChoice.tsx
+++ packages/decision-tree/src/components/Question/questionTypes/SingleChoice/SingleChoice.tsx
@@ -1,9 +1,10 @@
 import React, { FC } from 'react';
 import { type QuestionProps } from '@components/Question/types';
-import { Box, RadioGroup, Radio } from '@cmpsr/components';
+import { Box, RadioGroup } from '@cmpsr/components';
 import { QuestionTitle } from '../../components/QuestionTitle';
 import { SingleChoiceQuestion } from './types';
 import { HandleAnswersActions } from '@hooks';
+import { QuestionOption } from '../../components/QuestionOption';
 
 export const SingleChoice: FC<QuestionProps> = ({ data, answersDispatch }) => {
   const { question, choices, tooltip } = data as SingleChoiceQuestion;
@@ -14,33 +15,15 @@ export const SingleChoice: FC<QuestionProps> = ({ data, answersDispatch }) => {
         onChange={(nextValue) => answersDispatch({ type: HandleAnswersActions.SaveAnswer, payload: nextValue })}
       >
         {choices.map(({ id, label, subLabel }) => (
-          <Box
+          <QuestionOption
             key={`SingleChoice-${id}`}
-            border="1px solid #666"
-            borderColor="ui-element-outline-default"
-            background="background-action-default"
-            borderRadius="radii-4"
-            width="350px"
-            mb="spacer-4"
-            mx="auto"
-          >
-            <Radio
-              display="flex"
-              flexDirection="row-reverse"
-              variant="outline"
-              size="l"
-              m="auto"
-              justifyContent="space-between"
-              py="spacer-3"
-              px="spacer-4"
-              value={id}
-            >
-              <Box>
-                <Box>{label}</Box>
-                {subLabel ? <Box>{subLabel}</Box> : null}
-              </Box>
-            </Radio>
-          </Box>
+            componentType="radio"
+            componentProps={{
+              value: id,
+            }}
+            label={label}
+            subLabel={subLabel}
+          />
         ))}
       </RadioGroup>
     </Box>

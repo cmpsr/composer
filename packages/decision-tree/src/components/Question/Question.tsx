8dedc8b468e5df197afc2793a22fceb64a51f157
Alexis Munoz <alexismunoz@impulsum.vc>
2024-04-29 20:00:48 +0200

    fix: IDK button position

--- packages/decision-tree/src/components/Question/Question.tsx
+++ packages/decision-tree/src/components/Question/Question.tsx
@@ -1,6 +1,6 @@
 import React, { FC } from 'react';
 import { type QuestionProps } from './types';
-import { Flex } from '@cmpsr/components';
+import { Flex, Box } from '@cmpsr/components';
 import { Height, SingleChoice, MultipleChoice, Numeric } from './questionTypes';
 import { IDontKnowButton } from './components/IDontKnowButton';
 
@@ -22,24 +22,28 @@ export const Question: FC<QuestionProps> = ({
   if (typeof questionTypesMap[data.type] != 'function') return null;
 
   return (
-    <Flex
-      justifyContent="center"
-      mt={{ base: 'spacer-8', lg: 'spacer-16' }}
-      mb={{ base: 'spacer-14', lg: 'spacer-16' }}
-      mx={{ base: 'spacer-5', md: 'spacer-20', lg: 'spacer-16', xl: 'spacer-24', xxl: 'spacer-32' }}
-      px={{ base: '0', lg: 'spacer-32', xl: 'spacer-52', xxl: 'spacer-64' }}
-    >
-      {questionTypesMap[data.type]({ data, answersDispatch, defaultValue })}
-      {data.skippable ? (
-        <IDontKnowButton
-          iDontKnowAnswer={iDontKnowAnswer}
-          currentPage={data.id}
-          paginationDispatch={paginationDispatch}
-        />
+    <Box mb={{ base: 'spacer-14', lg: 'spacer-16' }}>
+      <Flex
+        justifyContent="center"
+        mt={{ base: 'spacer-8', lg: 'spacer-16' }}
+        mb="spacer-8"
+        mx={{ base: 'spacer-5', md: 'spacer-20', lg: 'spacer-16', xl: 'spacer-24', xxl: 'spacer-32' }}
+        px={{ base: '0', lg: 'spacer-32', xl: 'spacer-52', xxl: 'spacer-64' }}
+      >
+        {questionTypesMap[data.type]({ data, answersDispatch, defaultValue })}
+      </Flex>
+      {data.skippable !== false ? (
+        <Flex justifyContent="center">
+          <IDontKnowButton
+            iDontKnowAnswer={iDontKnowAnswer}
+            currentPage={data.id}
+            paginationDispatch={paginationDispatch}
+          />
+        </Flex>
       ) : (
         ''
       )}
-    </Flex>
+    </Box>
   );
 };
 

dea952f2a897eb33ce2e9fe1cfe9ce80036252fd
Alexis Munoz <alexismunoz@impulsum.vc>
2024-04-29 14:24:31 +0200

    feat: stepbar breakpoints

--- packages/decision-tree/src/components/StepBar/StepBar.tsx
+++ packages/decision-tree/src/components/StepBar/StepBar.tsx
@@ -1,27 +1,29 @@
 import React, { FC } from 'react';
 import { type StepBarProps } from './types';
-import { Stepper, Flex } from '@cmpsr/components';
+import { Stepper, Flex, Box } from '@cmpsr/components';
 
 export const StepBar: FC<StepBarProps> = ({ steps, activeStep }) => {
   return (
-    <Stepper index={activeStep}>
-      {steps.map(({ id, name }) => (
-        <Stepper.Step key={id} flexDirection="column" _horizontal={{ alignItems: 'start' }}>
-          <Flex width="100%" alignItems="center">
-            <Stepper.Step.Indicator>
-              <Stepper.Step.Status
-                complete={<Stepper.Step.Number color="white" />}
-                incomplete={<Stepper.Step.Number color="background-static" />}
-                active={<Stepper.Step.Number color="background-page-02" />}
-              />
-            </Stepper.Step.Indicator>
-            <Stepper.Step.Separator />
-          </Flex>
-          <Flex flexDirection="column" flexShrink={0} mt="0.5rem" gap="spacer-1">
-            <Stepper.Step.Title variant="text-body-floating-label-bold">{name}</Stepper.Step.Title>
-          </Flex>
-        </Stepper.Step>
-      ))}
-    </Stepper>
+    <Box px={{ base: '0', lg: 'spacer-16', xl: 'spacer-24', xxl: 'spacer-32' }} py={{ base: '0', lg: 'spacer-4' }}>
+      <Stepper index={activeStep}>
+        {steps.map(({ id, name }) => (
+          <Stepper.Step key={id} flexDirection="column" _horizontal={{ alignItems: 'start' }}>
+            <Flex width="100%" alignItems="center">
+              <Stepper.Step.Indicator>
+                <Stepper.Step.Status
+                  complete={<Stepper.Step.Number color="white" />}
+                  incomplete={<Stepper.Step.Number color="background-static" />}
+                  active={<Stepper.Step.Number color="background-page-02" />}
+                />
+              </Stepper.Step.Indicator>
+              <Stepper.Step.Separator />
+            </Flex>
+            <Flex flexDirection="column" flexShrink={0} mt="0.5rem" gap="spacer-1">
+              <Stepper.Step.Title variant="text-body-floating-label-bold">{name}</Stepper.Step.Title>
+            </Flex>
+          </Stepper.Step>
+        ))}
+      </Stepper>
+    </Box>
   );
 };

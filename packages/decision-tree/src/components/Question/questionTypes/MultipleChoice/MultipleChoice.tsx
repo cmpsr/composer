6df03d6a615ef0aa85d7f148bc100dd2aa9cad5c
Alexis Munoz <alexismunoz@impulsum.vc>
2024-05-06 21:21:18 +0200

    fix: answers memory next disabled bug

--- packages/decision-tree/src/components/Question/questionTypes/MultipleChoice/MultipleChoice.tsx
+++ packages/decision-tree/src/components/Question/questionTypes/MultipleChoice/MultipleChoice.tsx
@@ -8,7 +8,7 @@ import { inputMargin } from '@components/Question/Question';
 
 export const MultipleChoice: FC<MultipleChoiceProps> = ({ data, answersDispatch, defaultValue }) => {
   const { question, choices, tooltip } = data as MultipleChoiceQuestion;
-  const [answers, setAnswers] = useState<MultipleChoiceAnswer>({ type: 'multipleChoice', values: [] });
+  const [answers, setAnswers] = useState<MultipleChoiceAnswer>(defaultValue ?? { type: 'multipleChoice', values: [] });
 
   return (
     <Box>
@@ -22,12 +22,13 @@ export const MultipleChoice: FC<MultipleChoiceProps> = ({ data, answersDispatch,
             value: id,
             defaultChecked: defaultValue && defaultValue.values.length > 0 && defaultValue.values.includes(id),
             onChange: ({ currentTarget }) => {
-              if (currentTarget.checked) setAnswers({ ...answers, values: [...answers.values, currentTarget.value] });
-              if (!currentTarget.checked)
-                setAnswers({ ...answers, values: answers.values.filter((answer) => answer !== currentTarget.value) });
+              const newAnswers = !currentTarget.checked
+                ? { ...answers, values: answers.values.filter((answer) => answer !== currentTarget.value) }
+                : { ...answers, values: [...answers.values, currentTarget.value] };
+              setAnswers(newAnswers);
               answersDispatch({
                 type: HandleAnswersActions.SaveAnswer,
-                payload: answers,
+                payload: newAnswers,
               });
             },
           }}

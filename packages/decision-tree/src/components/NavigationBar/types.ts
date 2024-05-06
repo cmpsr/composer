76eeb2ddb5a3c59e82c94d7a220f636cd3158d7e
Alexis Munoz <alexismunoz@impulsum.vc>
2024-05-06 17:16:31 +0200

    fix: any type

--- packages/decision-tree/src/components/NavigationBar/types.ts
+++ packages/decision-tree/src/components/NavigationBar/types.ts
@@ -1,9 +1,10 @@
 import { type Dispatch } from 'react';
 import { PaginationAction } from '@hooks';
+import { QuestionnaireType } from '@types';
 
 export type NavigationBarProps = {
   isBackDisabled: boolean;
   isNextDisabled: boolean;
   dispatch: Dispatch<PaginationAction>;
-  submitAnswer: () => Promise<any>;
+  submitAnswer: () => Promise<QuestionnaireType>;
 };

be276b38e54a0851b309639a495bd9578192ed34
Alexis Munoz <alexismunoz@impulsum.vc>
2024-05-08 14:42:36 +0200

    fix: format

--- packages/decision-tree/src/components/Question/components/QuestionTitle/QuestionTitle.tsx
+++ packages/decision-tree/src/components/Question/components/QuestionTitle/QuestionTitle.tsx
@@ -2,17 +2,15 @@ import React, { FC } from 'react';
 import { type QuestionTitleProps } from './types';
 import { Text, Flex, IconHelp, Tooltip, Box } from '@cmpsr/components';
 
-export const QuestionTitle: FC<QuestionTitleProps> = ({ question, tooltip }) => {
-  return (
-    <Flex justifyContent={"center"} mb='spacer-8'>
-      <Text variant="text-header-L" mb="spacer-8" my="auto">
-        {question}
-      </Text>
-      <Tooltip label={tooltip} placement="bottom">
-        <Box display='inline' my='auto' ml='spacer-2'>
-          <IconHelp color='text-link-secondary-default' />
-        </Box>
-      </Tooltip>
-    </Flex>
-  );
-};
+export const QuestionTitle: FC<QuestionTitleProps> = ({ question, tooltip }) => (
+  <Flex justifyContent={'center'} mb="spacer-8">
+    <Text variant="text-header-L" mb="spacer-8" my="auto">
+      {question}
+    </Text>
+    <Tooltip label={tooltip} placement="bottom">
+      <Box display="inline" my="auto" ml="spacer-2">
+        <IconHelp color="text-link-secondary-default" />
+      </Box>
+    </Tooltip>
+  </Flex>
+);

5bf670c1179bbdc2081bd04262bbeb0aeb71a319
Alexis Munoz <alexismunoz@impulsum.vc>
2024-05-10 18:39:54 +0200

    feat: expose section intro props

--- packages/decision-tree/src/components/Question/questionTypes/SectionIntro/SectionIntro.tsx
+++ packages/decision-tree/src/components/Question/questionTypes/SectionIntro/SectionIntro.tsx
@@ -1,17 +1,21 @@
 import React, { FC } from 'react';
-import { Box, Text } from '@cmpsr/components';
+import { Box, BoxProps, Text, TextProps } from '@cmpsr/components';
 import { SectionIntroQuestion } from './types';
 import { type QuestionProps } from '@components/Question/types';
 
-export const SectionIntro: FC<QuestionProps> = ({ data }) => {
+export const SectionIntro: FC<QuestionProps & { textProps: TextProps; boxProps: BoxProps }> = ({
+  data,
+  textProps,
+  boxProps,
+}) => {
   const { question, description, orderNumber } = data as SectionIntroQuestion;
 
   return (
-    <Box height="100%" textAlign="center">
+    <Box height="100%" textAlign="center" {...boxProps}>
       <Text variant="text-body-regular" mb="spacer-6">
         SECTION {orderNumber}
       </Text>
-      <Text variant="text-header-4XL" mb={{ base: '0', lg: 'spacer-3' }}>
+      <Text variant="text-header-4XL" mb={{ base: '0', lg: 'spacer-3' }} {...textProps}>
         {question}
       </Text>
       <Text variant="text-body-meta-regular">{description}</Text>

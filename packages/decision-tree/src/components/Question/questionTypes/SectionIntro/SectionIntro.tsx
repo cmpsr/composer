7e4dd39e087d0c7cd7cc1ba9ce11faee2fefcce6
Alexis Munoz <alexismunoz@impulsum.vc>
2024-05-10 18:51:14 +0200

    fix: section intro props type

--- packages/decision-tree/src/components/Question/questionTypes/SectionIntro/SectionIntro.tsx
+++ packages/decision-tree/src/components/Question/questionTypes/SectionIntro/SectionIntro.tsx
@@ -1,14 +1,9 @@
 import React, { FC } from 'react';
-import { Box, BoxProps, Text, TextProps } from '@cmpsr/components';
-import { SectionIntroQuestion } from './types';
-import { type QuestionProps } from '@components/Question/types';
+import { Box, Text } from '@cmpsr/components';
+import { SectionIntroProps } from './types';
 
-export const SectionIntro: FC<QuestionProps & { textProps: TextProps; boxProps: BoxProps }> = ({
-  data,
-  textProps,
-  boxProps,
-}) => {
-  const { question, description, orderNumber } = data as SectionIntroQuestion;
+export const SectionIntro: FC<SectionIntroProps> = ({ data, textProps, boxProps }) => {
+  const { question, description, orderNumber } = data;
 
   return (
     <Box height="100%" textAlign="center" {...boxProps}>

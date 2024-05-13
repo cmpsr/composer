ccb5c47197e8fd18db4a7f78b730f356ae5174f1
Alexis Munoz <alexismunoz@impulsum.vc>
2024-05-13 18:15:38 +0200

    fix: IDK button

--- packages/decision-tree/src/components/Question/components/IDontKnowButton/IDontKnowButton.tsx
+++ packages/decision-tree/src/components/Question/components/IDontKnowButton/IDontKnowButton.tsx
@@ -3,18 +3,18 @@ import { type IDontKnowButtonProps } from './types';
 import { IconArrowRight, Button } from '@cmpsr/components';
 import { PaginationActions, PaginationPayload } from '@hooks';
 
-export const IDontKnowButton: FC<IDontKnowButtonProps> = ({ submitIDKAnswer, currentPage, paginationDispatch }) => {
+export const IDontKnowButton: FC<IDontKnowButtonProps> = ({ submitIDKAnswer, paginationDispatch }) => {
   return (
     <Button
       onClick={async () => {
-        const response = await submitIDKAnswer(currentPage);
+        const response = await submitIDKAnswer();
         paginationDispatch({ type: PaginationActions.NextQuestion, payload: response as PaginationPayload });
       }}
       variant="link"
       size="m"
       trailingIcon={<IconArrowRight />}
-      children="I'm not sure right now"
-      role="link"
-    />
+    >
+      I&aposm not sure right now
+    </Button>
   );
 };

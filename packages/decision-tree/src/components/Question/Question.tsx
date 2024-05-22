0660f145b7d906172079395636853c7fa9177808
Alexis Munoz <alexismunoz@impulsum.vc>
2024-05-22 12:17:41 +0200

    fix: terniary in question

--- packages/decision-tree/src/components/Question/Question.tsx
+++ packages/decision-tree/src/components/Question/Question.tsx
@@ -14,7 +14,7 @@ const questionTypesMap = {
 
 export const Question: FC<QuestionProps> = ({ submitIDKAnswer, paginationDispatch, ...props }) => {
   if (props.data.type === 'sectionIntro') return questionTypesMap[props.data.type](props);
-
+  console.log(props.data.skippable, 'wtf');
   return (
     <Box mb={{ base: 'spacer-14', lg: 'spacer-16' }} flexGrow="1">
       <Flex
@@ -26,12 +26,10 @@ export const Question: FC<QuestionProps> = ({ submitIDKAnswer, paginationDispatc
       >
         {questionTypesMap[props.data.type](props)}
       </Flex>
-      {props.data.skippable !== false ? (
+      {props.data.skippable && (
         <Flex justifyContent="center">
           <IDontKnowButton submitIDKAnswer={submitIDKAnswer} paginationDispatch={paginationDispatch} />
         </Flex>
-      ) : (
-        ''
       )}
     </Box>
   );

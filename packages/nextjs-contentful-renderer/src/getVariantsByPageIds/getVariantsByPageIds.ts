8465c7165c1769f3261ab62d3d234320b176eab2
Pau Teruel <93.pau.teruel@gmail.com>
2024-07-30 13:26:54 +0200

    feat: unneeded domain

--- packages/nextjs-contentful-renderer/src/getVariantsByPageIds/getVariantsByPageIds.ts
+++ packages/nextjs-contentful-renderer/src/getVariantsByPageIds/getVariantsByPageIds.ts
@@ -1,5 +1,5 @@
 import { getApolloClient } from '../utils/getApolloClient';
 import { getVariantsByPageIds as getVariantsByPageIdsContentful } from '../utils/contentful';
 
-export const getVariantsByPageIds = async (pageIds: string[], preview = false, domain = process.env.SITE_DOMAIN) =>
-  await getVariantsByPageIdsContentful(getApolloClient({ preview }), pageIds, preview, domain);
+export const getVariantsByPageIds = async (pageIds: string[], preview = false) =>
+  await getVariantsByPageIdsContentful(getApolloClient({ preview }), pageIds, preview);

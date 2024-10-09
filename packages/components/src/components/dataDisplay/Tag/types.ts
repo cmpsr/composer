092a375131d6580d041da8b1dd4d829ec4f6d3f6
ClaraTJ <clara@claratoloba.com>
2024-10-09 18:06:38 +0200

    update types

--- packages/components/src/components/dataDisplay/Tag/types.ts
+++ packages/components/src/components/dataDisplay/Tag/types.ts
@@ -4,7 +4,7 @@ export const tagSizes = ['s', 'm', 'l'] as const;
 export const tagVariants = ['subtle', 'outline', 'solid', 'custom'] as const;
 
 export type TagSize = (typeof tagSizes)[number];
-export type TagVariant = (typeof tagVariants)[number] | ChakraTagProps['variant'];
+export type TagVariant = (typeof tagVariants)[number];
 
 export interface TagProps extends ChakraTagProps {
   size?: ResponsiveValue<TagSize>;

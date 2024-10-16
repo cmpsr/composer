0f928086e350621f82e58d9b972b93a81101dcce
ClaraTJ <clara@claratoloba.com>
2024-10-16 17:45:18 +0200

    Refactor badge styles and update color variants

--- packages/components/src/theme/ComposerTheme/Components/Badge.ts
+++ packages/components/src/theme/ComposerTheme/Components/Badge.ts
@@ -10,36 +10,43 @@ export const statusStyles = {
   },
   accent: {
     backgroundColor: 'accent-default',
+    borderColor: 'accent-default',
     colorOutlineSubtle: 'accent-default',
     colorSolid: 'text-badge-solid',
   },
   primary: {
     backgroundColor: 'primary-default',
+    borderColor: 'primary-default',
     colorOutlineSubtle: 'primary-default',
     colorSolid: 'text-badge-solid',
   },
   secondary: {
     backgroundColor: 'secondary-default',
+    borderColor: 'secondary-default',
     colorOutlineSubtle: 'secondary-default',
     colorSolid: 'text-badge-solid',
   },
   info: {
     backgroundColor: 'alert-inform-default',
+    borderColor: 'alert-inform-default',
     colorOutlineSubtle: 'text-inform',
     colorSolid: 'text-badge-solid',
   },
   success: {
     backgroundColor: 'alert-success-default',
+    borderColor: 'alert-success-default',
     colorOutlineSubtle: 'text-success',
     colorSolid: 'text-badge-solid',
   },
   warning: {
     backgroundColor: 'alert-warning-default',
+    borderColor: 'alert-warning-default',
     colorOutlineSubtle: 'text-warning',
     colorSolid: 'text-badge-solid',
   },
   error: {
     backgroundColor: 'alert-error-default',
+    borderColor: 'alert-error-default',
     colorOutlineSubtle: 'text-error',
     colorSolid: 'text-badge-solid',
   },
@@ -57,10 +64,10 @@ export const Badge: ComponentStyleConfig = {
       color: statusStyles[status].colorSolid,
     }),
     outline: ({ status }) => ({
-      borderColor: status === 'custom' ? statusStyles[status].borderColor : statusStyles[status].backgroundColor,
+      borderColor: statusStyles[status].borderColor,
       borderWidth: '1px',
       boxShadow: 'none',
-      color: status === 'custom' ? statusStyles[status].colorOutline : statusStyles[status].backgroundColor,
+      color: status === 'custom' ? statusStyles[status].colorOutline : statusStyles[status].colorOutlineSubtle,
     }),
     subtle: ({ status }) => ({
       backgroundColor: 'background-action-default',

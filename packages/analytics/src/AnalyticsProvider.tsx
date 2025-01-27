fcc4d4e15bd4e7ce238d5cb08ccfa9ceed4f7e1c
Alexis Munoz <alexismunoz@impulsum.vc>
2025-01-27 10:55:32 +0100

    feat: logs

--- packages/analytics/src/AnalyticsProvider.tsx
+++ packages/analytics/src/AnalyticsProvider.tsx
@@ -38,19 +38,21 @@ const ssr = !(typeof window !== 'undefined' && window.document && window.documen
 
 const COOKIE_NAME = 'composer_anonymous_id';
 
-const proxyToIntegrations = (integrations: IIntegration[], func: string, args: any[], disabledFunctions) => {
-  integrations.forEach((integration) => {
+const proxyToIntegrations = (
+  integrations: Record<string, typeof supportedIntegrations>,
+  func: string,
+  args: any[],
+  disabledFunctions
+) => {
+  Object.keys(integrations).forEach((integrationKey) => {
+    const integration = integrations[integrationKey];
     console.log(disabledFunctions, 'disabledFunctions');
     console.log(integration, 'integration');
     console.log(integration.constructor.name, 'integration.constructor.name');
+    console.log(integrationKey, 'integration key');
     console.log(disabledFunctions[integration.constructor.name], 'disabledFunctions[integration.constructor.name]');
     console.log(func, 'func');
-    if (
-      disabledFunctions &&
-      disabledFunctions[integration.constructor.name] &&
-      disabledFunctions[integration.constructor.name].includes(func)
-    ) {
-      integration[func] = () => null;
+    if (disabledFunctions && disabledFunctions[integrationKey] && disabledFunctions[integrationKey].includes(func)) {
       return;
     }
     // eslint-disable-next-line prefer-spread
@@ -74,9 +76,10 @@ export const _AnalyticsProvider: FC<IAnalyticsProvider> = ({ children, disabledF
       return false;
     });
 
-    return enabledIntegrations.map((integration) => {
-      return new supportedIntegrations[integration](props[integration]);
-    });
+    return enabledIntegrations.reduce((acc, integration) => {
+      acc[integration] = new supportedIntegrations[integration](props[integration]);
+      return acc;
+    }, {});
   }, []);
 
   const context = useMemo(() => {
